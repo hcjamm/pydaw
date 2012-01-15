fbf832c3277470cff8becf2d284f8111e90e800e
Jeff Hubbard <jhubbard651@users.sf.net>
2012-01-15 17:52:51 -0500

    More GUI work, although some of it is broken now.

--- libmodsynth/examples/synth_qt_gui.h
+++ libmodsynth/examples/synth_qt_gui.h
@@ -61,15 +61,24 @@ public:
     void setHostRequestedQuit(bool r) { m_hostRequestedQuit = r; }
         
 public slots:
+    /*GUI Step 2:  Event handlers for setting knob values*/
     void setAttack (float sec);
     void setDecay  (float sec);
-    void setSustain(float percent);
+    void setSustain(float val);
     void setRelease(float sec);
     void setTimbre (float val);
     void setRes (float val);
     void setDist (float val);
     void aboutToQuit();
+    
+    
+    void setAttack_f (float sec);
+    void setDecay_f  (float sec);
+    void setSustain_f(float val);
+    void setRelease_f(float sec);
+    
 protected slots:
+    /*GUI Step 3:  Event handlers for receiving changed knob values*/
     void attackChanged (int);
     void decayChanged  (int);
     void sustainChanged(int);
@@ -81,10 +90,10 @@ protected slots:
     void test_release();
     void oscRecv();
     
-    /*Slots for playing the QWERTY keyboard like a MIDI keyboard.
-     You probably want to remove this before distributing your plugin*/
-    void _keypress_event(QKeyEvent *_event);
-    void _keyrelease_event(QKeyEvent * _event);
+    void attack_fChanged (int);
+    void decay_fChanged  (int);
+    void sustain_fChanged(int);
+    void release_fChanged(int);
 
 protected:
     QDial *newQDial( int, int, int, int );
@@ -92,11 +101,12 @@ protected:
     QDial *_get_knob(_knob_type);
     
     //TODO:  update this to be more flexible about layout types
-    void _add_knob(QGridLayout * _layout, int position_x, int position_y, std::string _label_text, QDial * _knob,    
+    void _add_knob(QGridLayout * _layout, int position_x, int position_y, QString _label_text, QDial * _knob,    
     QLabel * _label);
     
     QGroupBox * _newGroupBox(QString, QWidget *);
     
+    QLabel * _newQLabel(QWidget *);
     
     QCheckBox * _get_checkbox(std::string _text);
     
@@ -104,23 +114,32 @@ protected:
     void _changed_pitch(int, QLabel *, int);
     void _changed_decibels(int, QLabel *, int);
     
-    //QDial *m_tuning;
+    /*GUI Step1:  Declare a QLabel and QDial for each control (or other controls) here*/
     QDial *m_attack;
-    QDial *m_decay;
-    QDial *m_sustain;
-    QDial *m_release;
-    QDial *m_timbre;
-    QDial *m_res;
-    QDial *m_dist;
-
-    //QLabel *m_tuningLabel;
     QLabel *m_attackLabel;
-    QLabel *m_decayLabel;
+    QDial *m_decay;
+    QLabel *m_decayLabel;    
+    QDial *m_sustain;
     QLabel *m_sustainLabel;
-    QLabel *m_releaseLabel;
-    QLabel *m_timbreLabel;
-    QLabel *m_resLabel;
+    QDial *m_release;
+    QLabel *m_releaseLabel;    
+    QDial *m_timbre;
+    QLabel *m_timbreLabel;    
+    QDial *m_res;
+    QLabel *m_resLabel;    
+    QDial *m_dist;
     QLabel *m_distLabel;
+    
+    
+    QDial *m_attack_f;
+    QLabel *m_attackLabel_f;
+    QDial *m_decay_f;
+    QLabel *m_decayLabel_f;    
+    QDial *m_sustain_f;
+    QLabel *m_sustainLabel_f;
+    QDial *m_release_f;
+    QLabel *m_releaseLabel_f;    
+    
 
     lo_address m_host;
     QByteArray m_controlPath;
