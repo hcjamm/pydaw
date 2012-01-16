9aa9102878a0a6bbf746fbe795b1258652b14d6c
PyDAW Team <pydawteam@users.sf.net>
2012-01-15 23:07:55 -0500

    Fixed the UI problems, apparently moc (qt header file.h) -o (qt header file.moc.cpp) must be run periodically to correct the Qt metadata

--- libmodsynth/examples/synth_qt_gui.moc.cpp
+++ libmodsynth/examples/synth_qt_gui.moc.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
-** Meta object code from reading C++ file 'less_trivial_synth_qt_gui.h'
+** Meta object code from reading C++ file 'synth_qt_gui.h'
 **
-** Created: Mon Jan 9 22:07:33 2012
+** Created: Sun Jan 15 23:04:44 2012
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.7.2)
 **
 ** WARNING! All changes made in this file will be lost!
@@ -9,7 +9,7 @@
 
 #include "synth_qt_gui.h"
 #if !defined(Q_MOC_OUTPUT_REVISION)
-#error "The header file 'less_trivial_synth_qt_gui.h' doesn't include <QObject>."
+#error "The header file 'synth_qt_gui.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 62
 #error "This file was generated using the moc from 4.7.2. It"
 #error "cannot be used with the include files from this version of Qt."
@@ -23,7 +23,7 @@ static const uint qt_meta_data_SynthGUI[] = {
        5,       // revision
        0,       // classname
        0,    0, // classinfo
-      20,   14, // methods
+      28,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -31,41 +31,55 @@ static const uint qt_meta_data_SynthGUI[] = {
        0,       // signalCount
 
  // slots: signature, parameters, type, tag, flags
-      13,   10,    9,    9, 0x0a,
-      34,   30,    9,    9, 0x0a,
-      51,   30,    9,    9, 0x0a,
-      75,   67,    9,    9, 0x0a,
-      93,   30,    9,    9, 0x0a,
-     115,  111,    9,    9, 0x0a,
-     132,  111,    9,    9, 0x0a,
-     146,  111,    9,    9, 0x0a,
-     161,    9,    9,    9, 0x0a,
-     175,    9,    9,    9, 0x09,
-     194,    9,    9,    9, 0x09,
-     213,    9,    9,    9, 0x09,
-     231,    9,    9,    9, 0x09,
-     251,    9,    9,    9, 0x09,
-     271,    9,    9,    9, 0x09,
-     290,    9,    9,    9, 0x09,
-     306,    9,    9,    9, 0x09,
-     323,    9,    9,    9, 0x09,
+      14,   10,    9,    9, 0x0a,
+      31,   10,    9,    9, 0x0a,
+      51,   47,    9,    9, 0x0a,
+      69,   10,    9,    9, 0x0a,
+      87,   47,    9,    9, 0x0a,
+     104,   47,    9,    9, 0x0a,
+     118,   47,    9,    9, 0x0a,
+     133,   10,    9,    9, 0x0a,
+     156,   10,    9,    9, 0x0a,
+     178,   47,    9,    9, 0x0a,
+     202,   10,    9,    9, 0x0a,
+     226,   47,    9,    9, 0x0a,
+     245,    9,    9,    9, 0x0a,
+     259,    9,    9,    9, 0x09,
+     278,    9,    9,    9, 0x09,
+     296,    9,    9,    9, 0x09,
+     316,    9,    9,    9, 0x09,
      336,    9,    9,    9, 0x09,
-     351,    9,    9,    9, 0x09,
+     355,    9,    9,    9, 0x09,
+     371,    9,    9,    9, 0x09,
+     388,    9,    9,    9, 0x09,
+     413,    9,    9,    9, 0x09,
+     437,    9,    9,    9, 0x09,
+     463,    9,    9,    9, 0x09,
+     489,    9,    9,    9, 0x09,
+     510,    9,    9,    9, 0x09,
+     523,    9,    9,    9, 0x09,
+     538,    9,    9,    9, 0x09,
 
        0        // eod
 };
 
 static const char qt_meta_stringdata_SynthGUI[] = {
-    "SynthGUI\0\0hz\0setTuning(float)\0sec\0"
-    "setAttack(float)\0setDecay(float)\0"
-    "percent\0setSustain(float)\0setRelease(float)\0"
-    "val\0setTimbre(float)\0setRes(float)\0"
-    "setDist(float)\0aboutToQuit()\0"
-    "tuningChanged(int)\0attackChanged(int)\0"
-    "decayChanged(int)\0sustainChanged(int)\0"
-    "releaseChanged(int)\0timbreChanged(int)\0"
-    "resChanged(int)\0distChanged(int)\0"
-    "test_press()\0test_release()\0oscRecv()\0"
+    "SynthGUI\0\0sec\0setAttack(float)\0"
+    "setDecay(float)\0val\0setSustain(float)\0"
+    "setRelease(float)\0setTimbre(float)\0"
+    "setRes(float)\0setDist(float)\0"
+    "setFilterAttack(float)\0setFilterDecay(float)\0"
+    "setFilterSustain(float)\0setFilterRelease(float)\0"
+    "setNoiseAmp(float)\0aboutToQuit()\0"
+    "attackChanged(int)\0decayChanged(int)\0"
+    "sustainChanged(int)\0releaseChanged(int)\0"
+    "timbreChanged(int)\0resChanged(int)\0"
+    "distChanged(int)\0filterAttackChanged(int)\0"
+    "filterDecayChanged(int)\0"
+    "filterSustainChanged(int)\0"
+    "filterReleaseChanged(int)\0"
+    "noiseAmpChanged(int)\0test_press()\0"
+    "test_release()\0oscRecv()\0"
 };
 
 const QMetaObject SynthGUI::staticMetaObject = {
@@ -97,29 +111,37 @@ int SynthGUI::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
         switch (_id) {
-        //case 0: setTuning((*reinterpret_cast< float(*)>(_a[1]))); break;
-        case 1: setAttack((*reinterpret_cast< float(*)>(_a[1]))); break;
-        case 2: setDecay((*reinterpret_cast< float(*)>(_a[1]))); break;
-        case 3: setSustain((*reinterpret_cast< float(*)>(_a[1]))); break;
-        case 4: setRelease((*reinterpret_cast< float(*)>(_a[1]))); break;
-        case 5: setTimbre((*reinterpret_cast< float(*)>(_a[1]))); break;
-        case 6: setRes((*reinterpret_cast< float(*)>(_a[1]))); break;
-        case 7: setDist((*reinterpret_cast< float(*)>(_a[1]))); break;
-        case 8: aboutToQuit(); break;
-        //case 9: tuningChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 10: attackChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 11: decayChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 12: sustainChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 13: releaseChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 14: timbreChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 15: resChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 16: distChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 17: test_press(); break;
-        case 18: test_release(); break;
-        case 19: oscRecv(); break;
+        case 0: setAttack((*reinterpret_cast< float(*)>(_a[1]))); break;
+        case 1: setDecay((*reinterpret_cast< float(*)>(_a[1]))); break;
+        case 2: setSustain((*reinterpret_cast< float(*)>(_a[1]))); break;
+        case 3: setRelease((*reinterpret_cast< float(*)>(_a[1]))); break;
+        case 4: setTimbre((*reinterpret_cast< float(*)>(_a[1]))); break;
+        case 5: setRes((*reinterpret_cast< float(*)>(_a[1]))); break;
+        case 6: setDist((*reinterpret_cast< float(*)>(_a[1]))); break;
+        case 7: setFilterAttack((*reinterpret_cast< float(*)>(_a[1]))); break;
+        case 8: setFilterDecay((*reinterpret_cast< float(*)>(_a[1]))); break;
+        case 9: setFilterSustain((*reinterpret_cast< float(*)>(_a[1]))); break;
+        case 10: setFilterRelease((*reinterpret_cast< float(*)>(_a[1]))); break;
+        case 11: setNoiseAmp((*reinterpret_cast< float(*)>(_a[1]))); break;
+        case 12: aboutToQuit(); break;
+        case 13: attackChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 14: decayChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 15: sustainChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 16: releaseChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 17: timbreChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 18: resChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 19: distChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 20: filterAttackChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 21: filterDecayChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 22: filterSustainChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 23: filterReleaseChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 24: noiseAmpChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 25: test_press(); break;
+        case 26: test_release(); break;
+        case 27: oscRecv(); break;
         default: ;
         }
-        _id -= 20;
+        _id -= 28;
     }
     return _id;
 }
