dee066f8864033590d1eb401eaad70d593b75020
Jeff Hubbard <jhubbard651@users.sf.net>
2012-01-26 19:18:55 -0500

    Added another interpolate-linear function that accepts an array as input instead of expecting you to have the values ready

--- libmodsynth/examples/libmodsynth/lib/interpolate-linear.h
+++ libmodsynth/examples/libmodsynth/lib/interpolate-linear.h
@@ -12,14 +12,29 @@
 extern "C" {
 #endif
 
-inline float f_linear_interpolate(float, float, float);
     
+inline float f_linear_interpolate(float, float, float);
+inline float f_linear_interpolate_arr(float[],float);
+//TODO a function that accepts a number of table elements and checks the length of sample + 1 ??? 
+
+
 inline float f_linear_interpolate(float a_a, float a_b, float a_position)
 {    
     return (((a_a - a_b) * a_position) + a_a);
 }
 
+//Accepts an array as input
+inline float f_linear_interpolate_array(float a_table[], float a_ptr)
+{        
+    int f_int_pos = (int)a_ptr;
+    int f_int_pos_plus_1 = f_int_pos + 1;
+    float f_pos = a_ptr - f_int_pos;
+    
+    return ((a_table[f_int_pos] - a_table[f_int_pos_plus_1]) * f_pos) + a_table[f_int_pos_plus_1];
+}
 
+
+    
 #ifdef	__cplusplus
 }
 #endif
