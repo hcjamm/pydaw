6e7fb1a8dbeba9bbb667df4760e58cb692956ece
PyDAW Team <pydawteam@users.sf.net>
2012-01-28 17:51:37 -0500

    Added some more of the files that will eventually make the library feature-complete

---
+++ libmodsynth/examples/libmodsynth/modules/delay/convolution_reverb.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   convolution_reverb.h
+ * Author: vm-user
+ *
+ * Created on January 28, 2012, 1:41 PM
+ */
+
+#ifndef CONVOLUTION_REVERB_H
+#define	CONVOLUTION_REVERB_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* CONVOLUTION_REVERB_H */
+
