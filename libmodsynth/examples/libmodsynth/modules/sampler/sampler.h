c6ff5723a95fc1c1dfc07a8b2a3d993ee4b735ea
Jeff Hubbard <jhubbard651@users.sf.net>
2012-01-28 17:51:37 -0500

    Added some more of the files that will eventually make the library feature-complete

---
+++ libmodsynth/examples/libmodsynth/modules/sampler/sampler.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   sampler.h
+ * Author: vm-user
+ *
+ * Created on January 28, 2012, 1:37 PM
+ */
+
+#ifndef SAMPLER_H
+#define	SAMPLER_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* SAMPLER_H */
+
