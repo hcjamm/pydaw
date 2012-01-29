36efc05b6875bc4a151530f649d13eaf749253e8
PyDAW Team <pydawteam@users.sf.net>
2012-01-28 20:59:44 -0500

    I forgot to save this file from 2 commits ago, some of the fixes weren't committed.

--- libmodsynth/rebuild.sh
+++ libmodsynth/rebuild.sh
@@ -3,6 +3,9 @@
 #Run this script to rebuild everything.  There seems to be a problem with either Qt or GCC
 #that results in only a popping noise coming out when you play a note due to an unexpected null value somewhere.
 #This should fix it, unless there really is an error in your code.
+./configure --enable-libtool-lock
+echo -e "\nconfigure finished\n"
+sleep 15
 make clean
 echo -e "\nmake clean finished\n"
 sleep 15
@@ -12,9 +15,6 @@ moc synth_qt_gui.h -o synth_qt_gui.moc.cpp
 echo -e "\nmoc finished\n"
 sleep 15
 cd ..
-./configure --enable-libtool-lock
-echo -e "\nconfigure finished\n"
-sleep 15
 make
 echo -e "\nmake finished\n"
 sleep 15
