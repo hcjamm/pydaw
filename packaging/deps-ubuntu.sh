811f7402627de8b2812b7e2c75df96ce88d562c2
Jeff Hubbard <jhubbard651@users.sf.net>
2012-01-31 19:57:58 -0500

    Updated the dependencies script to include php5-cli for running the png2base64 script

--- packaging/deps-ubuntu.sh
+++ packaging/deps-ubuntu.sh
@@ -6,7 +6,7 @@
 #I am open to dependency scripts for other distros if anybody wants to contribute one, please test it thoroughly before sending it to me
 
 #dependencies, current as of Ubuntu 12.04 alpha
-gksudo apt-get install liblo-dev dssi-dev ladspa-sdk libasound2-dev g++ qtractor qjackctl qt4-designer libjack-jackd2-dev libsndfile1-dev libsamplerate0-dev libtool autoconf openjdk-7-jre libsm-dev uuid-dev cmake liblscp-dev checkinstall
+gksudo apt-get install liblo-dev dssi-dev ladspa-sdk libasound2-dev g++ qtractor qjackctl qt4-designer libjack-jackd2-dev libsndfile1-dev libsamplerate0-dev libtool autoconf openjdk-7-jre libsm-dev uuid-dev cmake liblscp-dev checkinstall php5-cli
 #add your user to the audio group to allow proper access to Qjackctl
 gksudo usermod -g audio $USER
 
