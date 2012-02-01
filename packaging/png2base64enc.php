bea525fe0dfd8e264d5d81db4b78d473301356ab
Jeff Hubbard <jhubbard651@users.sf.net>
2012-01-31 19:55:59 -0500

    Added a PHP CLI  script to convert PNG images to base64 encoded strings

---
+++ packaging/png2base64enc.php
@@ -0,0 +1,11 @@
+<?php
+function base64_encode_image ($filename=string,$filetype=string) {
+    if ($filename) {
+        $imgbinary = fread(fopen($filename, "r"), filesize($filename));
+        return 'data:image/' . $filetype . ';base64,' . base64_encode($imgbinary);
+    }
+}
+
+echo base64_encode_image ($argv[1],'png');
+?>
+
