7948d450ea1d758fb5f75e8555f5ac4777ae75a5
PyDAW Team <pydawteam@users.sf.net>
2012-02-01 20:49:53 -0500

    fixed the script, it had a typo in it

--- packaging/presets_to_qstring.pl
+++ packaging/presets_to_qstring.pl
@@ -1,11 +1,11 @@
 #!/usr/bin/perl
 
-#Script to read a preset file into a C header file
+#Script to convert a preset file into a C header file, for packaging a default set of presets into a plugin
 #usage:
-#perl presets_to_qstring.pl ~/dssi/(name_of_synth.tsv) > qt_synth_gui_presets.h
+#perl presets_to_qstring.pl ~/dssi/(name_of_synth.tsv) > q_preset_file.h
 
 local $/ = undef;
-open FILE, @argv[0] or die "Couldn't open file: $!";
+open FILE, $ARGV[0] or die "Couldn't open file: " .  $ARGV[0];
 binmode FILE;
 $string = <FILE>;
 close FILE;
@@ -14,8 +14,9 @@ close FILE;
 $string =~ s/\n/\\n/g;
 $string =~ s/\t/\\t/g;
 
-printf "#include <QString>\n\nQString q_preset_string = QString(\"" 
-. $string . 
-"\");";
+printf "#ifndef Q_PRESET_FILE_H\n#define	Q_PRESET_FILE_H\n" . 
+"#include <QString>\n\nQString q_preset_string = QString(\"" .
+$string . 
+"\");\n#endif";
 
 
