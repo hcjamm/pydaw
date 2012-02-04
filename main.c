dce175d5e7b1ecb86d4aac999abac591625f1f1a
Jeff Hubbard <jhubbard651@users.sf.net>
2012-02-03 19:34:41 -0500

    Reorganized the library to something approximating the finished product it will eventually be.  The DSSI API is now essentially gutted out to the bits I actually want for LibModSynth, and the rest is gone.

---
+++ main.c
@@ -0,0 +1,8 @@
+/*This file doesn't do anything, it's only here so that Netbeans will open the entire thing as a project
+Each subfolder of the trunk has it's own makefile, and is to be compiled separately, there is no reason to compile
+it all together*/
+
+int main(void)
+{
+	return 0;
+}
