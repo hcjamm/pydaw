f73939f17755a4edc8697e65b58336cd649fabac
Jeff Hubbard <jhubbard651@users.sf.net>
2012-02-05 10:30:37 -0500

    Fixed a bug where closing and re-opening the GUI in a host would cause it to reload the first preset.

--- ray-v/synth_qt_gui.moc.cpp
+++ ray-v/synth_qt_gui.moc.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'synth_qt_gui.h'
 **
-** Created: Tue Jan 31 19:31:17 2012
+** Created: Sun Feb 5 10:12:19 2012
 **      by: The Qt Meta Object Compiler version 63 (Qt 4.8.0)
 **
 ** WARNING! All changes made in this file will be lost!
