03df7b6bbea95894039f86e8e98b73ad41a3a4f3
Jeff Hubbard <jhubbard651@users.sf.net>
2012-02-08 19:10:08 -0500

    Work around a bug in gksudo in Ubuntu

--- packaging/deps-ubuntu.sh
+++ packaging/deps-ubuntu.sh
@@ -6,8 +6,8 @@
 #I am open to dependency scripts for other distros if anybody wants to contribute one, please test it thoroughly before sending it to me
 
 #dependencies, current as of Ubuntu 12.04 alpha
-gksudo apt-get install liblo-dev dssi-dev ladspa-sdk libasound2-dev g++ qtractor qjackctl qt4-designer libjack-jackd2-dev libsndfile1-dev libsamplerate0-dev libtool autoconf openjdk-7-jre libsm-dev uuid-dev cmake liblscp-dev checkinstall php5-cli
+sudo apt-get install liblo-dev dssi-dev ladspa-sdk libasound2-dev g++ qtractor qjackctl qt4-designer libjack-jackd2-dev libsndfile1-dev libsamplerate0-dev libtool autoconf openjdk-7-jre libsm-dev uuid-dev cmake liblscp-dev checkinstall
 #add your user to the audio group to allow proper access to Qjackctl
-gksudo usermod -g audio $USER
+sudo usermod -g audio $USER
 
 #See rebuild.sh for how to build and run the example plugin
