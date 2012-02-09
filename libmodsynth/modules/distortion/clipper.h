0fa89348f651e89f2693396c8116307ca2832265
Jeff Hubbard <jhubbard651@users.sf.net>
2012-02-08 21:59:11 -0500

    Switch db2a functions to the fast table lookup

--- libmodsynth/modules/distortion/clipper.h
+++ libmodsynth/modules/distortion/clipper.h
@@ -34,7 +34,7 @@ void v_clp_set_clip_sym(t_clipper * a_clp, float a_db)
     
     a_clp->clip_db = a_db;
     
-    float f_value = f_db_to_linear(a_db);
+    float f_value = f_db_to_linear_fast(a_db);
     
 #ifdef LMS_DEBUG_MODE
         printf("Clipper value == %f", f_value);
