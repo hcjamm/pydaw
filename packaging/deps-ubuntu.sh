2c0b4d8d3a61081d167fb7e4a269ef8020b30524
PyDAW Team <pydawteam@users.sf.net>
2012-02-14 23:08:57 -0500

    Remove qtractor as a dependency, the older versions in the repos don't work properly

--- packaging/deps-ubuntu.sh
+++ packaging/deps-ubuntu.sh
@@ -6,7 +6,7 @@
 #I am open to dependency scripts for other distros if anybody wants to contribute one, please test it thoroughly before sending it to me
 
 #dependencies, current as of Ubuntu 12.04 alpha
-sudo apt-get install liblo-dev dssi-dev ladspa-sdk libasound2-dev g++ qtractor qjackctl qt4-designer libjack-jackd2-dev libsndfile1-dev libsamplerate0-dev libtool autoconf openjdk-7-jre libsm-dev uuid-dev cmake liblscp-dev checkinstall libmad0-dev
+sudo apt-get install liblo-dev dssi-dev ladspa-sdk libasound2-dev g++  qjackctl qt4-designer libjack-jackd2-dev libsndfile1-dev libsamplerate0-dev libtool autoconf openjdk-7-jre libsm-dev uuid-dev cmake liblscp-dev checkinstall libmad0-dev
 #add your user to the audio group to allow proper access to Qjackctl
 sudo usermod -g audio $USER
 
