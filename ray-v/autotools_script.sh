2cac4014001c2b5b92996a6e20f0c42d6a0cb4db
PyDAW Team <pydawteam@users.sf.net>
2012-02-19 13:13:07 -0500

    Added sleep to the autotools script, because it seems not to compile consistently without it on my machine, which may be due to kernel bugs in handling SSDs

--- ray-v/autotools_script.sh
+++ ray-v/autotools_script.sh
@@ -1,9 +1,18 @@
+make clean
+sleep 15
 aclocal
+sleep 15
 libtoolize --force --copy
+sleep 15
 autoheader
+sleep 15
 automake --add-missing --foreign
+sleep 15
 autoconf
+sleep 15
 moc synth_qt_gui.h -o synth_qt_gui.moc.cpp
+sleep 15
 ./configure
+sleep 15
 make
 #sudo checkinstall
