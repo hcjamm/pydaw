2ea5d3b36d58359b0620d300f9132f3a1f13154e
PyDAW Team <pydawteam@users.sf.net>
2012-03-01 19:10:37 -0500

    Added a parent script to prevent duplicating effort between build.pl scripts in different folders.

---
+++ build-lib.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+#This script provides functions for the build.pl scripts in each plugin directory.  This prevents the need 
+#to duplicate changes to each build.pl script when making changes.
+
+#This script should not be invoked by itself, it is merely a library for the other scripts.
+
+sub run_script
+{
+
+}
