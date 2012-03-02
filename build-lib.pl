4e09f859e20238e4271339c22bd11a529a0bcab8
Jeff Hubbard <jhubbard651@users.sf.net>
2012-03-01 22:30:07 -0500

    Centralized the main parts of the build.pl scripts into a master script that is maintained in a central location

--- build-lib.pl
+++ build-lib.pl
@@ -5,7 +5,215 @@
 
 #This script should not be invoked by itself, it is merely a library for the other scripts.
 
+#TODO:  An option to cleanly fork a plugin that will autogenerate the required changes
+#fork add:  git add dssi.h libmodsynth.h Makefile.am synth.c synth.h synth_qt_gui.cpp synth_qt_gui.h
+#fork add:  git add build.pl Makefile.am configure.ac
+#fork:  Change Makefile.am, replace ray_v with XXXXX
+$help_text = "
+The LibModSynth build helper script.  http://libmodsynth.sourceforge.net
+
+Usage:
+
+perl build.pl [-f (first build)] || [-b (build)] [-i (install)] || [-s (run standalone)] || 
+[-d (build release .deb packages)]  || [-u (install Ubuntu dependencies)] [compile options]
+
+-f :  A clean build, rebuilding all autotools files, does not install.
+-b :  A quick build, does not install.
+-i :  Install using make install
+-s :  Compile, install and run standalone from the terminal.  Use this to test changes to your code.
+-d :  Compile and package the plugin into a .deb file
+-u :  Install all Ubuntu dependencies
+
+Additional options for compiling arguments
+
+--native  :  Compile using -march=native .  This optimizes for the machine being compiled on, but the binaries will not be usable on a different machine.  This can give you greater performance if compiling your own plugins, but may introduce bugs.
+
+--sse3    :  Compile for SSE, SSE2 and SSE3.  This is the default option, requires a later Pentium4, Athlon64 or newer machine.
+
+--user-cflags [CFLAGS]  :  Specify your own additional CFLAGS
+
+--no-opt  :  Compile with no optimizations.  Not recommended unless compiling for a non-x86/x64 architecture.
+
+There should be one of these scripts in each plugin directory.
+
+";
+
+
+$plugin_path = "/usr/local/lib/dssi";
+$jack_host = "../jack-dssi-host/jack-dssi-host";
+$sleep = "sleep 10";
+
+$makefile = "Makefile";
+
+$deps_ubuntu = "sudo apt-get install liblo-dev dssi-dev ladspa-sdk libasound2-dev g++ qjackctl qt4-designer libjack-jackd2-dev libsndfile1-dev libsamplerate0-dev libtool autoconf openjdk-7-jre libsm-dev uuid-dev cmake liblscp-dev checkinstall libmad0-dev ; sudo usermod -g audio \$USER";
+
+#TODO:  Check for dependencies when running the other arguments, place a file when installed
+#TODO:  Place a file in the plugin directory once the first build has been run
 sub run_script
 {
+	if($ARGV[0] eq "-f")
+	{
+		notify_wait();
+		first_build();
+		notify_done();
+	}
+	elsif($ARGV[0] eq "-s")
+	{
+		notify_wait();
+		unless(-e $jack_host)
+		{
+			`cd ../jack-dssi-host ; sh ./autotools_script.sh`;
+		}
 
+		if(-e $makefile)
+		{
+			clean();
+			build();
+		}
+		else
+		{
+			first_build();
+		}
+		make_install();
+		exec("$jack_host $plugin_path/$plugin_name");
+	}
+	elsif($ARGV[0] eq "-b")
+	{
+		notify_wait();
+		if(-e $makefile)
+		{
+			clean();
+			build();
+		}
+		else
+		{
+			first_build();
+		}
+		notify_done();
+	}
+	elsif($ARGV[0] eq "-d")
+	{
+
+		deb_package();
+		notify_done();
+	}
+	elsif($ARGV[0] eq "-i")
+	{
+		`sudo make install`;
+	}
+	elsif($ARGV[0] eq "-u")
+	{
+		install_deps_ubuntu();
+	}
+	else
+	{
+		print $help_text . 'Invalid argument:  $ARGV[0] eq "' . $ARGV[0] . "\"\n\n";
+	}
+
+}
+sub notify_wait
+{
+print "\nPlease wait, this could take a few minutes...\n";
+}
+
+sub notify_done
+{
+print "\n\nFinished.\n\n";
+}
+
+sub first_build
+{
+check_deps();
+clean();
+`aclocal`;
+`$sleep`;
+`libtoolize --force --copy`;
+`$sleep`;
+`autoheader`;
+`$sleep`;
+`automake --add-missing --foreign`;
+`$sleep`;
+`autoconf`;
+`$sleep`;
+`moc ./src/synth_qt_gui.h -o ./src/synth_qt_gui.moc.cpp`;
+`$sleep`;
+`./configure`;
+`$sleep`;
+build();
+}
+
+sub clean
+{
+`$clean`;
+`make clean`;
+`$sleep`;
+}
+
+sub build
+{
+#TODO:  test -ffast-math CFLAG
+#TODO:  Remove the extra cflags from Makefile.am in ray-v
+
+if($ARGV[1] eq "--native")
+{
+$make_result = `make CFLAGS+="-O3 -pipe -march=native -mtune=native"`;
+}
+elsif($ARGV[1] eq "--user-cflags")
+{
+$user_flags = $ARGV[2];
+$make_result = `make CFLAGS+="$user_flags"`;
+}
+elsif($ARGV[1] eq "--no-opt")
+{
+$make_result = `make`;
+}
+else
+{
+$make_result = `make CFLAGS+="-O3 -msse -msse2 -msse3 -mmmx -pipe"`;
+}
+
+#TODO:  Check make result
+#TODO:  Properly parse the args at the beginning of the script instead of relying on index
+
+`$sleep`;
+}
+
+sub make_install
+{
+$install_result = system("sudo make install");
+#TODO:  Check install result
+}
+
+sub deb_package
+{
+first_build();
+`sudo checkinstall --type=debian --install=no`;
+}
+
+#This isn't a real check, it only tests to see if the script attempted to install the dependencies
+sub check_deps
+{
+	unless(-e "../deps_installed.txt")
+	{
+		install_deps_ubuntu();
+	}
+}
+
+sub install_deps_ubuntu
+{
+$check_ubuntu = `uname -v`;
+if($check_ubuntu =~ m/ubuntu/i)
+{
+	`$deps_ubuntu`;
+}
+else
+{	
+print("Did not detect a Ubuntu system, dependencies not installed.
+If you are running a non-Ubuntu system, please ensure that you have the 
+equivalent dependencies on your system:
+
+$deps_ubuntu
+");
+}
+`echo 'This file is created when build.pl attempts to install the dependencies' > ../deps_installed.txt`;
 }
