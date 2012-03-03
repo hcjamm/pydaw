6bc78a9b158e4d9c60bd9e15e9c51a56615d0262
Jeff Hubbard <jhubbard651@users.sf.net>
2012-03-03 15:57:36 -0500

    More cleanup and fixes to the build/packaging system.

---
+++ jack-dssi-host/build.pl
@@ -0,0 +1,10 @@
+
+#!/usr/bin/perl
+
+require "../build-lib.pl";
+
+#change $plugin_name and $clean when you fork a LibModSynth plugin
+$plugin_name = "jack-dssi-host";
+$clean = "sudo rm -R $plugin_path/jack-dssi-host*";
+
+run_script();
