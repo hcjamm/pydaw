815d00c5cb0e650657dd8a836511b13fa450be64
Jeff Hubbard <jhubbard651@users.sf.net>
2012-03-05 22:33:54 -0500

    LMS Delay is now working, albeit no GUI, and not feature-complete.

--- libmodsynth/modules/delay/delay.h
+++ libmodsynth/modules/delay/delay.h
@@ -14,31 +14,59 @@
 extern "C" {
 #endif
     
-#define LMS_DELAY_DEBUG_MODE
-    
-typedef struct st_delay_simple
-{    
+#define DLY_DEBUG_MODE
+
+/* A tap is used to read from a delay.  You can have as many taps as you want per
+ * delay line.
+ * This type is suitable for ping-pong delays, etc...
+ */
+typedef struct st_delay_tap
+{
     int read_head;
-    int write_head;
     int delay_samples;
     float delay_seconds;
     float delay_beats;
+    float output;
+}t_delay_tap;
+
+/* A tap for reading from a delay using linear interpolation.
+ * This type is suitable for chorus, flanger, etc...
+ */
+typedef struct st_delay_tap_lin
+{
+    int read_head;
+    int read_head_p1;
+    float delay_samples;
+    float delay_seconds;
+    float delay_beats;
+    float output;
+}t_delay_tap_lin;
+    
+/* A delay is just a buffer to write audio to, that also maintains information like tempo,
+ * sample rate, etc...  Taps are used to read from the delay line, either 
+ * non-interpolated (CPU friendly, not suitable for modulation) or interpolated (suitable for modulation)
+ */
+typedef struct st_delay_simple
+{   
+    int write_head;    
     float sample_rate;
     float tempo;
-    float tempo_recip;
-    float output;
-    int count;
+    float tempo_recip;    
+    int sample_count;    
     float* buffer;
-#ifdef LMS_DELAY_DEBUG_MODE
+#ifdef DLY_DEBUG_MODE
     int debug_counter;
 #endif
 }t_delay_simple;
 
+
 t_delay_simple * g_dly_get_delay(float, float);
 t_delay_simple * g_dly_get_delay_tempo(float,float,float);
-inline void v_dly_set_delay_seconds(t_delay_simple*,float);
-inline void v_dly_set_delay_tempo(t_delay_simple*,float);
+t_delay_tap * g_dly_get_tap();
+inline void v_dly_set_delay_seconds(t_delay_simple*,t_delay_tap*,float);
+inline void v_dly_set_delay_tempo(t_delay_simple*,t_delay_tap*,float);
 inline void v_dly_run_delay(t_delay_simple*,float);
+inline void v_dly_run_tap(t_delay_simple*,t_delay_tap*);
 
 
 /*inline void v_dly_set_delay(
@@ -46,25 +74,26 @@ inline void v_dly_run_delay(t_delay_simple*,float);
  * float a_seconds //delay in seconds, typically .1 to 1
  * )
  */
-inline void v_dly_set_delay_seconds(t_delay_simple* a_dly,float a_seconds)
+inline void v_dly_set_delay_seconds(t_delay_simple* a_dly, t_delay_tap* a_tap,float a_seconds)
 {
-    if((a_dly->delay_seconds) != a_seconds)
+    if((a_tap->delay_seconds) != a_seconds)
     {
-        a_dly->delay_seconds = a_seconds;
-        a_dly->delay_samples = (a_dly->sample_rate) * a_seconds;
+        a_tap->delay_seconds = a_seconds;
+        a_tap->delay_samples = (a_dly->sample_rate) * a_seconds;
     }
 }
 /*inline void v_dly_set_delay_tempo(
  * t_delay_simple* a_dly, 
+ * t_delay_tap* a_tap, 
  * float a_beats //Delay time in beats.  Typical value:  .25, .5, 1, 2....
  * )
  */
-inline void v_dly_set_delay_tempo(t_delay_simple* a_dly, float a_beats)
+inline void v_dly_set_delay_tempo(t_delay_simple* a_dly, t_delay_tap* a_tap, float a_beats)
 {
-    if((a_dly->delay_beats) != a_beats)
+    if((a_tap->delay_beats) != a_beats)
     {
-        a_dly->delay_beats = a_beats;
-        a_dly->delay_samples = (a_dly->tempo_recip) * a_beats * (a_dly->sample_rate);
+        a_tap->delay_beats = a_beats;
+        a_tap->delay_samples = (a_dly->tempo_recip) * a_beats * (a_dly->sample_rate);
     }
 }
 
@@ -79,23 +108,15 @@ inline void v_dly_set_delay_tempo(t_delay_simple* a_dly, float a_beats)
 inline void v_dly_run_delay(t_delay_simple* a_dly,float a_input)
 {
     
-    a_dly->read_head = (a_dly->write_head) - (a_dly->delay_samples);    
-    if((a_dly->read_head) < 0)
-    {
-        a_dly->read_head = (a_dly->read_head) + (a_dly->count);
-    }
-    
-    a_dly->output = a_dly->buffer[(a_dly->read_head)];
-    
     a_dly->write_head = (a_dly->write_head) + 1;
-    if((a_dly->write_head) >= (a_dly->count))
+    if((a_dly->write_head) >= (a_dly->sample_count))
     {
         a_dly->write_head = 0;
     }
     
     a_dly->buffer[(a_dly->write_head)] = a_input;
     
-#ifdef LMS_DELAY_DEBUG_MODE
+#ifdef DLY_DEBUG_MODE
     a_dly->debug_counter = (a_dly->debug_counter) + 1;
     
     if((a_dly->debug_counter) >= 100000)
@@ -103,12 +124,7 @@ inline void v_dly_run_delay(t_delay_simple* a_dly,float a_input)
         a_dly->debug_counter = 0;
         
         printf("\n\nDelay debug info:\n");
-        printf("a_dly->count == %i\n", (a_dly->count));
-        printf("a_dly->delay_beats == %f\n", (a_dly->delay_beats));
-        printf("a_dly->delay_samples == %i\n", (a_dly->delay_samples));
-        printf("a_dly->delay_seconds == %f\n", (a_dly->delay_seconds));
-        printf("a_dly->output == %f\n", (a_dly->output));
-        printf("a_dly->read_head == %i\n", (a_dly->read_head));
+        printf("a_dly->count == %i\n", (a_dly->sample_count));        
         printf("a_dly->sample_rate == %f\n", (a_dly->sample_rate));
         printf("a_dly->tempo == %f\n", (a_dly->tempo));
         printf("a_dly->tempo_recip == %f\n", (a_dly->tempo_recip));
@@ -118,6 +134,30 @@ inline void v_dly_run_delay(t_delay_simple* a_dly,float a_input)
 #endif
 }
 
+inline void v_dly_run_tap(t_delay_simple* a_dly,t_delay_tap* a_tap)
+{
+    a_tap->read_head = (a_dly->write_head) - (a_tap->delay_samples);    
+    if((a_tap->read_head) < 0)
+    {
+        a_tap->read_head = (a_tap->read_head) + (a_dly->sample_count);
+    }
+    
+    a_tap->output = a_dly->buffer[(a_tap->read_head)];
+    
+#ifdef DLY_DEBUG_MODE
+    if((a_dly->debug_counter) == 50000)
+    {
+        printf("\n\nTap debug info:\n");
+        printf("a_tap->delay_beats == %f\n", (a_tap->delay_beats));
+        printf("a_tap->delay_samples == %i\n", (a_tap->delay_samples));
+        printf("a_tap->delay_seconds == %f\n", (a_tap->delay_seconds));
+        printf("a_tap->output == %f\n", (a_tap->output));
+        printf("a_tap->read_head == %i\n", (a_tap->read_head));
+    }
+#endif
+}
+
+
 /*t_delay_simple * g_dly_get_delay
  * (float a_max_size, //max size in seconds
  * float a_sr //sample rate
@@ -126,19 +166,14 @@ inline void v_dly_run_delay(t_delay_simple* a_dly,float a_input)
 t_delay_simple * g_dly_get_delay(float a_max_size, float a_sr)
 {
     t_delay_simple * f_result = (t_delay_simple*)malloc(sizeof(t_delay_simple));
-         
-    f_result->read_head = 0;
+    
     f_result->write_head = 0;
-    f_result->delay_samples = 0;
-    f_result->delay_seconds  = 0;
-    f_result->delay_beats = 0;
     f_result->sample_rate = a_sr;
     f_result->tempo = 999;
     f_result->tempo_recip = 999;
-    f_result->output = 0;
-    
-    f_result->count = (int)((a_max_size * a_sr) + 2400);   //add 2400 samples to ensure we don't overrun our buffer
-    f_result->buffer = (float*)malloc(sizeof(float) * (f_result->count));
+        
+    f_result->sample_count = (int)((a_max_size * a_sr) + 2400);   //add 2400 samples to ensure we don't overrun our buffer
+    f_result->buffer = (float*)malloc(sizeof(float) * (f_result->sample_count));
     
     return f_result;
 }
@@ -153,18 +188,27 @@ t_delay_simple * g_dly_get_delay_tempo(float a_tempo, float a_max_size,float a_s
 {
     t_delay_simple * f_result = (t_delay_simple*)malloc(sizeof(t_delay_simple));
          
-    f_result->read_head = 0;
-    f_result->write_head = 0;
-    f_result->delay_samples = 0;
-    f_result->delay_seconds  = 0;
-    f_result->delay_beats = 0;
+    
+    f_result->write_head = 0;    
     f_result->sample_rate = a_sr;
     f_result->tempo = a_tempo;
     f_result->tempo_recip = (60.0f/a_tempo);  //convert from beats per minute to seconds per beat
-    f_result->output = 0;
+        
+    f_result->sample_count = (int)((a_max_size * (f_result->tempo_recip) * a_sr) + 2400);  //add 2400 samples to ensure we don't overrun our buffer
+    f_result->buffer = (float*)malloc(sizeof(float) * (f_result->sample_count));
     
-    f_result->count = (int)((a_max_size * (f_result->tempo_recip) * a_sr) + 2400);  //add 2400 samples to ensure we don't overrun our buffer
-    f_result->buffer = (float*)malloc(sizeof(float) * (f_result->count));
+    return f_result;
+}
+
+t_delay_tap * g_dly_get_tap()
+{
+    t_delay_tap * f_result = (t_delay_tap*)malloc(sizeof(t_delay_tap));
+    
+    f_result->read_head = 0;
+    f_result->delay_samples = 0;
+    f_result->delay_seconds  = 0;
+    f_result->delay_beats = 0;
+    f_result->output = 0;
     
     return f_result;
 }
---
+++ libmodsynth/modules/delay/lms_delay.h
@@ -0,0 +1,113 @@
+/* 
+ * File:   lms_delay.h
+ * Author: Jeff Hubbard
+ * 
+ * Created on March 5, 2012, 9:16 PM
+ */
+
+#ifndef LMS_DELAY_H
+#define	LMS_DELAY_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#define LMS_DELAY_DEBUG_MODE
+    
+#include "delay.h"
+#include "../../lib/amp.h"
+    
+/* A multi-mode delay module.  This is a complete delay with stereo, ping-pong, etc... modes
+ * feeback can be routed out and back into the module.
+ * 
+ * The delay in delay.h is for building your own custom delays.  This is an implementation of that.
+*/
+typedef struct st_lms_delay
+{
+    t_delay_simple * delay0;
+    t_delay_simple * delay1;
+    t_delay_tap * tap0;
+    t_delay_tap * tap1;
+    float output0;
+    float output1;
+    float feedback0;
+    float feedback1;
+    float feedback_db;
+    float feedback_linear;
+    int is_ducking;
+}t_lms_delay;
+
+//Used to switch between delay types, uses much less CPU than a switch statement
+typedef float (*fp_ldl_run_ptr)(t_lms_delay*,float,float);
+
+t_lms_delay * g_ldl_get_delay(float,float);
+
+inline void v_ldl_set_delay(t_lms_delay*,float,float,int);
+
+inline void v_ldl_run_delay_ping_pong(t_lms_delay*,float,float,float,float);
+inline void v_ldl_run_delay_stereo(t_lms_delay*,float,float,float,float);
+
+/*t_lms_delay * g_ldl_get_delay(
+ * float a_tempo, //tempo in BPM
+ * float a_sr  //sample rate
+ * )
+ */
+t_lms_delay * g_ldl_get_delay(float a_tempo, float a_sr)
+{
+    t_lms_delay* f_result = (t_lms_delay*)malloc(sizeof(t_lms_delay));
+    
+    f_result->delay0 = g_dly_get_delay_tempo(a_tempo, 4, a_sr);
+    f_result->delay1 = g_dly_get_delay_tempo(a_tempo, 4, a_sr);
+    f_result->tap0 = g_dly_get_tap();
+    f_result->tap1 = g_dly_get_tap();
+    f_result->output0 = 0;
+    f_result->output1 = 0;
+    f_result->feedback0 = 0;
+    f_result->feedback1 = 0;
+    f_result->feedback_db = -50;
+    f_result->feedback_linear = 0;
+    f_result->is_ducking = 0;
+    
+    return f_result;
+}
+
+/* inline void v_ldl_run_delay_ping_pong(
+ * t_lms_delay* a_dly, 
+ * float a_in0, //input 0  - audio input 0
+ * float a_in1, //input 1
+ * float a_fb0, //feedback 0 - This is for allowing external modules to modify the feedback, like a dampening filter
+ * float a_fb1) //feedback 1
+ */
+inline void v_ldl_run_delay_ping_pong(t_lms_delay* a_dly, float a_in0, float a_in1, float a_fb0, float a_fb1)
+{
+    v_dly_run_delay(a_dly->delay0, (a_in0 + a_in1 + ((a_fb0 + a_fb1) * (a_dly->feedback_linear) * 0.5f)));        
+    v_dly_run_tap(a_dly->delay0, a_dly->tap0);
+    
+    v_dly_run_delay(a_dly->delay1, (a_dly->tap0->output));        
+    v_dly_run_tap(a_dly->delay1, a_dly->tap1);    
+    
+    a_dly->output0 = (a_dly->tap0->output);
+    a_dly->output1 = (a_dly->tap1->output);
+}
+
+
+inline void v_ldl_set_delay(t_lms_delay* a_dly,float a_beats, float a_feeback_db, int a_is_ducking)
+{
+    v_dly_set_delay_tempo(a_dly->delay0, a_dly->tap0, a_beats);
+    v_dly_set_delay_tempo(a_dly->delay1, a_dly->tap1, a_beats);
+    
+    a_dly->is_ducking = a_is_ducking;
+    
+    if(a_feeback_db != (a_dly->feedback_db))
+    {
+        a_dly->feedback_db = a_feeback_db;
+        a_dly->feedback_linear = f_db_to_linear_fast(a_feeback_db);
+    }
+}
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* LMS_DELAY_H */
+
