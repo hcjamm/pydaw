375631c96322f270dcce903920e630b35b9d34f8
Jeff Hubbard <jhubbard651@users.sf.net>
2012-03-07 19:51:57 -0500

    Forked LMS Delay into LMS Reverb.

---
+++ lms_reverb/src/meta.h
@@ -0,0 +1,21 @@
+
+/* This file contains global identifier information for the plugin. */
+
+#ifndef META_H
+#define	META_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+/*These should be customized for each developer and plugin*/
+#define LMS_PLUGIN_NAME "lms_reverb"
+#define LMS_PLUGIN_LONG_NAME "LMS Reverb (Powered by LibModSynth)";  //Please keep the (Powered by LibModSynth) tag, it helps further the goal of promoting Linux DSSI plugins
+#define LMS_PLUGIN_DEV "Jeff Hubbard <jhubbard651@users.sf.net>";
+#define LMS_PLUGIN_UUID 595558
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* META_H */
