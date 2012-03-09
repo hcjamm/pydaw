1d93761cd7e796e9f38dff08c7c55205786495b8
Jeff Hubbard <jhubbard651@users.sf.net>
2012-03-09 18:48:47 -0500

    Experimental changes to interpolate-linear

--- libmodsynth/lib/interpolate-linear.h
+++ libmodsynth/lib/interpolate-linear.h
@@ -17,7 +17,12 @@ inline float f_linear_interpolate(float, float, float);
 inline float f_linear_interpolate_arr(float[],float);
 inline float f_linear_interpolate_arr_wrap(float[],int,float);
 
-
+/*These are meant to prevent allocating objects constantly, without having to immediately
+ re-write everything that uses them.  If LMS ever becomes multi-threaded, these will need
+ to be moved to their own struct and passed to the functions*/
+static int f_int_pos;
+static int f_int_pos_plus_1;
+static float f_pos;
 
 inline float f_linear_interpolate(float a_a, float a_b, float a_position)
 {    
@@ -27,9 +32,9 @@ inline float f_linear_interpolate(float a_a, float a_b, float a_position)
 //Accepts an array as input
 inline float f_linear_interpolate_arr(float a_table[], float a_ptr)
 {        
-    int f_int_pos = (int)a_ptr;
-    int f_int_pos_plus_1 = f_int_pos + 1;
-    float f_pos = a_ptr - f_int_pos;
+    f_int_pos = (int)a_ptr;
+    f_int_pos_plus_1 = f_int_pos + 1;
+    f_pos = a_ptr - f_int_pos;
     
     return ((a_table[f_int_pos] - a_table[f_int_pos_plus_1]) * f_pos) + a_table[f_int_pos_plus_1];
 }
@@ -37,13 +42,13 @@ inline float f_linear_interpolate_arr(float a_table[], float a_ptr)
 /*Wrap if the next sample is past the array boundary*/
 inline float f_linear_interpolate_arr_wrap(float a_table[], int a_table_size, float a_ptr)
 {        
-    int f_int_pos = (int)a_ptr;
-    int f_int_pos_plus_1 = f_int_pos + 1;
+    f_int_pos = (int)a_ptr;
+    f_int_pos_plus_1 = f_int_pos + 1;
     
     if(f_int_pos_plus_1 > a_table_size)
         f_int_pos_plus_1 = 0;
     
-    float f_pos = a_ptr - f_int_pos;
+    f_pos = a_ptr - f_int_pos;
     
     return ((a_table[f_int_pos] - a_table[f_int_pos_plus_1]) * f_pos) + a_table[f_int_pos_plus_1];
 }
