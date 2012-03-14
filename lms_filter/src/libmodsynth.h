a3b3efce441ff46c9c2246ee4346a4ef5d21d318
Jeff Hubbard <jhubbard651@users.sf.net>
2012-03-14 18:25:34 -0400

    Update LMS Filter for the new library

--- lms_filter/src/libmodsynth.h
+++ lms_filter/src/libmodsynth.h
@@ -22,16 +22,8 @@ extern "C" {
 #include "../libmodsynth/constants.h"
     
 /*includes for any libmodsynth modules you'll be using*/
-#include "../../libmodsynth/lib/osc_core.h"
 #include "../../libmodsynth/lib/pitch_core.h"
-#include "../../libmodsynth/lib/smoother-linear.h"
-#include "../../libmodsynth/modules/oscillator/osc_simple.h"
-#include "../../libmodsynth/modules/oscillator/noise.h"
 #include "../../libmodsynth/modules/filter/svf.h"
-#include "../../libmodsynth/modules/distortion/clipper.h"
-#include "../../libmodsynth/modules/modulation/adsr.h"
-#include "../../libmodsynth/modules/signal_routing/audio_xfade.h"
-#include "../../libmodsynth/modules/modulation/ramp_env.h"
 #include "../../libmodsynth/lib/smoother-iir.h"
    
 /*A call to an audio function that requires no parameters.  Use this for GUI switches when possible, as it will
