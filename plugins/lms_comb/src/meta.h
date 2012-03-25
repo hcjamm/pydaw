286f3189cb5dc2e82dc5c4405d8304d4fbf63b8c
PyDAW Team <pydawteam@users.sf.net>
2012-03-24 21:01:17 -0400

    More improvements to building and packaging.
    A special thanks goes out to Glen MacArthur of AV Linux for finding
    a bug in LMS Comb.  Plugin names are limited to 32 characters in some
    host, so I had to remove the (powered by LibModSynth) tag from the
    names.

--- plugins/lms_comb/src/meta.h
+++ plugins/lms_comb/src/meta.h
@@ -9,7 +9,7 @@ extern "C" {
 
 /*These should be customized for each developer and plugin*/
 #define LMS_PLUGIN_NAME "lms-comb"
-#define LMS_PLUGIN_LONG_NAME "LMS Comb (Powered by LibModSynth)";  //Please keep the (Powered by LibModSynth) tag, it helps further the goal of promoting Linux DSSI plugins
+#define LMS_PLUGIN_LONG_NAME "LMS Comb";
 #define LMS_PLUGIN_DEV "Jeff Hubbard <jhubbard651@users.sf.net>";
 #define LMS_PLUGIN_UUID 1307797
 
