6ada96b43877fec3abf15b8e985e2d5a79358cea
Jeff Hubbard <jhubbard651@users.sf.net>
2012-04-01 18:45:50 -0400

    More work to the LMS custom widget framework

--- plugins/libmodsynth/widgets/group_box.h
+++ plugins/libmodsynth/widgets/group_box.h
@@ -10,7 +10,57 @@
 #ifndef GROUP_BOX_H
 #define	GROUP_BOX_H
 
+#include <QtGui/QApplication>
+#include <QGroupBox>
+#include <QGridLayout>
+#include "knob_regular.h"
 
+class LMS_group_box 
+{
+    public:
+        LMS_group_box(QWidget * a_parent, QString a_title, QString a_stylesheet)
+        {
+            lms_groupbox = new QGroupBox(a_parent);
+            lms_groupbox->setTitle(a_title);
+            lms_layout = new QGridLayout(lms_groupbox);
+            lms_groupbox->setStyleSheet(a_stylesheet);
+            lms_stylesheet = a_stylesheet;
+            x_index = 0;
+            y_index = 0;
+        }
+        void lms_add_h(LMS_knob_regular * a_knob)        
+        {
+            lms_add(a_knob);
+            x_index++;
+        }
+        
+        void lms_add_v(LMS_knob_regular * a_knob)        
+        {
+            /*Don't increment if nothing has been added yet
+            if((y_index != 0) && (x_index != 0))*/
+            
+            y_index++;
+            
+            x_index = 0;
+            
+            lms_add(a_knob);
+            
+            x_index++;
+        }
+        
+        QGroupBox * lms_groupbox;
+        QGridLayout * lms_layout;
+        QString lms_stylesheet;
+        
+    private:
+        int x_index;
+        int y_index;
+        
+        void lms_add(LMS_knob_regular * a_knob)        
+        {            
+            lms_layout->addLayout(a_knob->lms_layout, y_index, x_index, Qt::AlignCenter);
+        }
+};
 
 #endif	/* GROUP_BOX_H */
 
--- plugins/libmodsynth/widgets/knob_regular.h
+++ plugins/libmodsynth/widgets/knob_regular.h
@@ -31,6 +31,27 @@ extern "C"{
  */
 enum LMS_KNOB_CONVERSION{lmk_integer, lmk_pitch, lmk_decimal, lmk_off};
 
+
+/* This class passes global information for the plugin to new knobs
+ */
+class LMS_knob_info
+{
+    public:
+        LMS_knob_info(bool * a_suppressHostUpdate, 
+                lo_address a_host, QByteArray a_controlPath)
+        {
+            lms_controlPath = a_controlPath;
+            lms_host = a_host;
+            lms_suppressHostUpdate = a_suppressHostUpdate;
+        }
+        
+        bool * lms_suppressHostUpdate;        
+        lo_address lms_host;
+        QByteArray lms_controlPath;
+    
+};
+
+
 class LMS_knob_regular : QObject
 {    
     Q_OBJECT
@@ -50,8 +71,7 @@ class LMS_knob_regular : QObject
          * QByteArray a_controlPath)
          */
         LMS_knob_regular(QString a_label,int a_min, int a_max, int a_step_size, int a_value, 
-        QString a_label_value, LMS_KNOB_CONVERSION a_conv_type, QWidget *a_parent, bool * a_suppressHostUpdate, 
-                int a_lms_port, lo_address a_host, QByteArray a_controlPath)
+        QString a_label_value, LMS_KNOB_CONVERSION a_conv_type, QWidget *a_parent, int a_lms_port, LMS_knob_info * a_knob_info)
         {
             lms_layout = new QVBoxLayout(a_parent);
             lms_label = new QLabel(a_parent);
@@ -64,11 +84,12 @@ class LMS_knob_regular : QObject
             lms_value = new QLabel(a_parent);
             lms_value->setText(a_label_value);
             lms_conversion_type = a_conv_type;
-            lms_suppressHostUpdate = a_suppressHostUpdate;
             lms_port = a_lms_port;
-            lms_host = a_host;
-            lms_controlPath = a_controlPath;
-
+            
+            lms_suppressHostUpdate =  a_knob_info->lms_suppressHostUpdate;
+            lms_host = a_knob_info->lms_host;
+            lms_controlPath = a_knob_info->lms_controlPath;
+                        
             lms_layout->addWidget(lms_label, -1, Qt::AlignCenter);
             lms_layout->addWidget(lms_knob, -1, Qt::AlignCenter);
             lms_layout->addWidget(lms_value, -1, Qt::AlignCenter);
@@ -84,10 +105,12 @@ class LMS_knob_regular : QObject
         QLabel * lms_value;        
         int lms_port;
         LMS_KNOB_CONVERSION lms_conversion_type;
+        
+        
         bool * lms_suppressHostUpdate;
         lo_address lms_host;
         QByteArray lms_controlPath;
-    
+        
         ~LMS_knob_regular(){};
     public slots:
         void lms_value_changed(int a_value)
@@ -109,6 +132,7 @@ class LMS_knob_regular : QObject
                     //Do nothing
                     break;
             }
+           
             
             bool f_osc_send = *(lms_suppressHostUpdate);
             
@@ -116,6 +140,7 @@ class LMS_knob_regular : QObject
             {
                 lo_send(lms_host, lms_controlPath, "if", lms_port, a_value);
             }
+            
         };
 };
 
