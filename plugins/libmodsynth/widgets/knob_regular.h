ae6857bfb9828a4df9f8c821d897fb5ec442f4d5
Jeff Hubbard <jhubbard651@users.sf.net>
2012-04-02 18:23:53 -0400

    Major improvements to the new widget system, LMS Distortion is now working again.  Nearly ready to merge this branch back into master.

--- plugins/libmodsynth/widgets/knob_regular.h
+++ plugins/libmodsynth/widgets/knob_regular.h
@@ -15,6 +15,12 @@
 #include <QString>
 #include <QLabel>
 #include <QDial>
+#include <math.h>
+
+enum LMS_KNOB_CONVERSION
+{
+    lms_kc_integer, lms_kc_decimal, lms_kc_pitch, lms_kc_none
+};
 
 /* This class passes global information for the plugin to new knobs
  */
@@ -57,7 +63,7 @@ class LMS_knob_regular
          * LMS_knob_info * a_knob_info)
          */
         LMS_knob_regular(QString a_label,int a_min, int a_max, int a_step_size, int a_value, 
-        QString a_label_value, QWidget *a_parent, LMS_knob_info * a_knob_info)
+        QString a_label_value, QWidget *a_parent, LMS_knob_info * a_knob_info, LMS_KNOB_CONVERSION a_conv_type, int a_lms_port)
         {
             lms_layout = new QVBoxLayout(a_parent);
             lms_label = new QLabel(a_parent);
@@ -80,22 +86,47 @@ class LMS_knob_regular
                         
             lms_knob->setMinimumSize((a_knob_info->lms_knob_size),(a_knob_info->lms_knob_size));
             lms_knob->setMaximumSize((a_knob_info->lms_knob_size),(a_knob_info->lms_knob_size));
+            
+            lms_conv_type  = a_conv_type;
                                     
             lms_layout->addWidget(lms_label, -1, Qt::AlignCenter);
             lms_layout->addWidget(lms_knob, -1, Qt::AlignCenter);
             lms_layout->addWidget(lms_value, -1, Qt::AlignCenter);
             
-            //lms_value_changed(lms_knob->value());            
+            lms_port = a_lms_port;
+            
+            //valueChanged(lms_knob->value());            
         }
+        
+        void valueChanged(int a_value)
+        {
+                switch(lms_conv_type)
+                {
+                    case lms_kc_decimal:
+                        lms_value->setText(QString::number(((float)a_value) * .01 ));
+                        break;
+                    case lms_kc_integer:
+                        lms_value->setText(QString::number(a_value));
+                        break;
+                    case lms_kc_none:
+                        //Do nothing
+                        break;
+                    case lms_kc_pitch:
+                        lms_value->setText(QString::number(
+                        (440*pow(2,((float)(a_value-57))*.0833333))
+                        ));
+                        break;
+                }
+        }
+        
                 
         QVBoxLayout * lms_layout;
         QLabel * lms_label;
         QDial * lms_knob;        
-        QLabel * lms_value;        
-                        
-        bool * lms_suppressHostUpdate;
-        //lo_address lms_host;
-        QByteArray lms_controlPath;
+        QLabel * lms_value;
+        LMS_KNOB_CONVERSION lms_conv_type;
+        
+        int lms_port;
         
         ~LMS_knob_regular(){};
 };
