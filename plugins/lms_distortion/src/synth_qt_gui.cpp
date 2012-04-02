a58802325b174554ebd2f0106bd3e8969bf45028
PyDAW Team <pydawteam@users.sf.net>
2012-04-01 20:15:36 -0400

    Working on simplifying how the GUIs are assembled.

--- plugins/lms_distortion/src/synth_qt_gui.cpp
+++ plugins/lms_distortion/src/synth_qt_gui.cpp
@@ -23,23 +23,13 @@ GNU General Public License for more details.
 #include <iostream>
 #include <unistd.h>
 
-#include <qt4/QtGui/qgroupbox.h>
-#include <qt4/QtGui/qlayout.h>
-#include <qt4/QtGui/qlabel.h>
-#include <qt4/QtGui/qgridlayout.h>
-#include <QFormLayout>
-#include <qt4/QtGui/qboxlayout.h>
-#include <QGroupBox>
-#include <qt4/QtGui/qdial.h>
 #include <QPixmap>
 #include <QFile>
 #include <QDir>
 #include <QTextStream>
-#include <QMessageBox>
 
 #include <stdlib.h>
 #include "../../libmodsynth/lib/amp.h"
-#include "../../libmodsynth/lib/pitch_core.h"
 
 #include "synth.h"
 #include "meta.h"
@@ -49,6 +39,7 @@ GNU General Public License for more details.
 #include <X11/Xutil.h>
 #include <X11/Xatom.h>
 #include <X11/SM/SMlib.h>
+#include <qt4/QtGui/qwindowdefs.h>
 
 static int handle_x11_error(Display *dpy, XErrorEvent *err)
 {
@@ -101,44 +92,21 @@ SynthGUI::SynthGUI(const char * host, const char * port,
     layout->addLayout(layout_row0);
     layout->addLayout(layout_row1, -1);        
         
-    int f_row = 0;
-    int f_column = 0;
+    LMS_knob_info * f_info;
+    f_info->lms_controlPath = m_controlPath;
+    f_info->lms_host = m_host;
+    f_info->lms_suppressHostUpdate = &m_suppressHostUpdate;
     
-    int f_gb_layout_row = 0;
-    int f_gb_layout_column = 0;
-    /*Lay out the controls you declared in the first step*/
+    m_groupbox = new LMS_group_box(this, "Distortion", "QGroupBox {background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #E0E0E0, stop: 1 #FFFFFF); border: 2px solid gray;  border-radius: 10px;  margin-top: 1ex; } QGroupBox::title { subcontrol-origin: margin; subcontrol-position: top center; padding: 0 3px; background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #FFOECE, stop: 1 #FFFFFF); }");
+    m_ingain = new LMS_knob_regular(QString("In"), -6, 36, 1, 12, QString("0"), lmk_integer, m_groupbox->lms_groupbox, LMS_GAIN, f_info);
+    m_drywet = new LMS_knob_regular(QString("Wet"), 0, 100, 1, 0, QString("0"), lmk_off, m_groupbox->lms_groupbox, LMS_WET, f_info);
+    m_outgain = new LMS_knob_regular(QString("Out"), -24, 6, 1, -6, QString("0"), lmk_integer, m_groupbox->lms_groupbox, LMS_OUT_GAIN, f_info);
     
-#ifdef LMS_DEBUG_MODE_QT    
-    cerr << "Creating the Filter controls" << endl;    
-#endif    
+    m_groupbox->lms_add_h(m_ingain);
+    m_groupbox->lms_add_h(m_drywet);
+    m_groupbox->lms_add_h(m_outgain);
     
-    QGroupBox * f_gb_filter = newGroupBox("Distortion", this); 
-    QGridLayout *f_gb_filter_layout = new QGridLayout(f_gb_filter);
-    
-    m_gain  =   newQDial(-6, 36, 1, 15); //get_knob(pitch);
-    m_gainLabel  = newQLabel(this);
-    add_widget(f_gb_filter_layout, f_gb_layout_column, f_gb_layout_row, "In Gain",m_gain, m_gainLabel);
-    connect(m_gain,  SIGNAL(valueChanged(int)), this, SLOT(gainChanged(int)));
-        
-    f_gb_layout_column++;
-    
-    m_wet  =  get_knob(zero_to_one); 
-    add_widget_no_label(f_gb_filter_layout, f_gb_layout_column, f_gb_layout_row, "Wet", m_wet);
-    connect(m_wet,  SIGNAL(valueChanged(int)), this, SLOT(wetChanged(int)));
-        
-    f_gb_layout_column++;
-    
-    m_outGain  =   newQDial(-24, 6, 1, -6); //get_knob(pitch);
-    m_outGainLabel  = newQLabel(this);
-    add_widget(f_gb_filter_layout, f_gb_layout_column, f_gb_layout_row, "Out Gain",m_outGain, m_outGainLabel);
-    connect(m_outGain,  SIGNAL(valueChanged(int)), this, SLOT(outGainChanged(int)));
-        
-    f_gb_layout_column++;
-    
-    layout_row0->addWidget(f_gb_filter, -1, Qt::AlignLeft);
-    f_column++;
-    f_gb_layout_row = 0;
-    f_gb_layout_column = 0;
+    layout_row0->addWidget(m_groupbox->lms_groupbox, -1, Qt::AlignLeft);
         
     QLabel * f_logo_label = new QLabel("", this);    
     f_logo_label->setTextFormat(Qt::RichText);
@@ -148,8 +116,7 @@ SynthGUI::SynthGUI(const char * host, const char * port,
     f_logo_label->setText(f_logo_text);
     f_logo_label->setMinimumSize(90, 30);   
     f_logo_label->show();
-    layout_row1->addWidget(f_logo_label, -1, Qt::AlignRight);
-                
+    layout_row1->addWidget(f_logo_label, -1, Qt::AlignRight);                
         
     /*End test button code, DO NOT remove the code below this*/
 
@@ -159,270 +126,6 @@ SynthGUI::SynthGUI(const char * host, const char * port,
     myTimer->start(0);
     
     m_suppressHostUpdate = false;
-
-}
-
-/*I'm leaving this in here for now, but at the present it doesn't work*/
-void SynthGUI::v_add_knob_to_layout(QDial * a_knob, e_knob_type a_knob_type, int a_default_value, QLabel * a_label, QGridLayout * a_layout, QString a_title,
-int a_gb_layout_column, int a_gb_layout_row, const char * a_signal, const char * a_slot)
-{
-    a_knob = get_knob(a_knob_type, a_default_value);
-    a_label  = newQLabel(this);
-    add_widget(a_layout, a_gb_layout_column, a_gb_layout_row, a_title,a_knob, a_label);
-    connect(a_knob,  a_signal, this, a_slot);    
-}
-
-
-void SynthGUI::add_widget(QGridLayout * a_layout, int a_position_x, int a_position_y, QString a_label_text,  QWidget * a_widget,
-    QLabel * _label)
-{   
-    QLabel * f_knob_title = new QLabel(a_label_text,  this);
-    f_knob_title->setMinimumWidth(60);  //TODO:  make this a constant
-    f_knob_title->setAlignment(Qt::AlignCenter);
-    f_knob_title->setStyleSheet("background-color: white; border: 1px solid black;  border-radius: 6px;");  //TODO:  make this a constant string for all knobs
-    
-    a_layout->addWidget(f_knob_title, a_position_y, a_position_x, Qt::AlignCenter);    
-    a_layout->addWidget(a_widget,  (a_position_y + 1), a_position_x);
-    a_layout->addWidget(_label,  (a_position_y + 2), a_position_x, Qt::AlignCenter);     
-}
-
-void SynthGUI::add_widget_no_label(QGridLayout * a_layout, int a_position_x, int a_position_y, QString a_label_text, QWidget * a_widget)
-{
-    QLabel * f_knob_title = new QLabel(a_label_text,  this);
-    f_knob_title->setMinimumWidth(60);  //TODO:  make this a constant
-    f_knob_title->setAlignment(Qt::AlignCenter);
-    f_knob_title->setStyleSheet("background-color: white; border: 1px solid black;  border-radius: 6px;");    //TODO:  make this a constant string for all knobs
-    
-    a_layout->addWidget(f_knob_title, a_position_y, a_position_x, Qt::AlignCenter);    
-    a_layout->addWidget(a_widget,  (a_position_y + 1), a_position_x);    
-}
-
-QGroupBox * SynthGUI::newGroupBox(QString a_title, QWidget * a_parent)
-{
-    QGroupBox * f_result = new QGroupBox(a_parent);
-    
-    f_result->setTitle(a_title);
-    f_result->setAlignment(Qt::AlignHCenter);
-    return f_result;
-}
-
-QLabel * SynthGUI::newQLabel(QWidget * a_parent)
-{
-    QLabel * f_result = new QLabel(a_parent);
-    //_result->setStyleSheet("background-color: white; border: 2px solid black;  border-radius: 6px;");
-    return f_result;
-}
-
-QDial * SynthGUI::get_knob(e_knob_type a_ktype, int a_default_value)
-{
-    int f_min, f_max, f_step, f_value;
-    
-    switch(a_ktype)
-    {
-        case decibels_0:
-                f_min = -60; f_max = 0; f_step = 1; f_value = -6; 
-                break;
-        case decibels_plus_12:
-            f_min = -60; f_max = 12; f_step = 1; f_value = -6;            
-            break;
-        case decibels_plus_24:
-            f_min = -60; f_max = 24; f_step = 1; f_value = -6;            
-            break;
-        case decibels_plus_6:            
-            f_min = -60; f_max = 6; f_step = 1; f_value = -6;            
-            break;
-        case decibels_30_to_0:
-            f_min = -30; f_max = 0; f_step = 1; f_value = -9;
-            break;
-        case decibels_20_to_0:
-            f_min = -20; f_max = 0; f_step = 1; f_value = -9;
-            break;
-        case pitch:
-            f_min = 20; f_max = 124; f_step = 1; f_value = 105;            
-            break;
-        case zero_to_four:
-            f_min = 1; f_max = 400; f_step = 4; f_value = 75;            
-            break;
-        case zero_to_one:
-            f_min = 1; f_max = 100; f_step = 1; f_value = 15;            
-            break;
-        case zero_to_two:
-            f_min = 1; f_max = 200; f_step = 2; f_value = 25;            
-            break;
-        case minus1_to_1:
-            f_min = -100; f_max = 100; f_step = 1; f_value = 0;            
-            break;
-        case minus12_to_12:
-            f_min = -12; f_max = 12; f_step = 1; f_value = 0;            
-            break;
-        case minus24_to_24:
-            f_min = -24; f_max = 24; f_step = 1; f_value = 0;            
-            break;
-        case minus36_to_36:
-            f_min = -36; f_max = 36; f_step = 1; f_value = 0;            
-            break;
-    }
-    
-    if(a_default_value != 333)  //This makes the assumption that we will never pick 333 as a default value
-    {
-        f_value = a_default_value;
-    }
-    
-     return newQDial(f_min, f_max, f_step, f_value);
-    
-}
-
-QCheckBox * SynthGUI::get_checkbox(std::string a_text)
-{
-    QCheckBox * f_checkbox = new QCheckBox(this);
-    
-    f_checkbox->setText(QString::fromStdString(a_text));
-    
-    //TODO:  add a skin to make it look like a toggle-switch
-        
-    return f_checkbox;
-}
-
-/*newQDial(
- * int minValue,
- * int maxValue,
- * int pageStep,
- * int value
- * );
- */
-QDial * SynthGUI::newQDial( int minValue, int maxValue, int pageStep, int value )
-{
-    QDial *dial = new QDial( this );
-    dial->setMinimum( minValue );
-    dial->setMaximum( maxValue );
-    dial->setPageStep( pageStep );
-    dial->setValue( value );
-    dial->setNotchesVisible(false); 
-    //TODO:  Make this a constant value
-    dial->setMaximumHeight(66);
-    dial->setMaximumWidth(66);
-    dial->setMinimumHeight(66);
-    dial->setMinimumWidth(66);
-    
-    //dial->setFocusPolicy(Qt::NoFocus);
-    
-    return dial;
-}
-
-QComboBox * SynthGUI::get_combobox(QString a_choices [], int a_count,  QWidget * a_parent)
-{
-    QComboBox * f_result = new QComboBox(a_parent);
-    QStringList f_items;
-    
-    for(int i = 0; i < a_count; i++)
-    {
-        f_items << a_choices[i];
-    }
-    
-    f_result->addItems(f_items);
-    
-    return f_result;
-}
-
-void SynthGUI::setGain(float val)
-{
-    m_suppressHostUpdate = true;
-    m_gain->setValue(int(val));
-    m_suppressHostUpdate = false;
-}
-
-void SynthGUI::setWet(float val)
-{
-    m_suppressHostUpdate = true;
-    m_wet->setValue(int(val));
-    m_suppressHostUpdate = false;
-}
-
-void SynthGUI::setOutGain(float val)
-{
-    m_suppressHostUpdate = true;
-    m_outGain->setValue(int(val));
-    m_suppressHostUpdate = false;
-}
-
-/*Standard handlers for the audio slots, these perform manipulations of knob values
- that are common in audio applications*/
-
-void SynthGUI::changed_zero_to_x(int a_value, QLabel * a_label, int a_port)
-{
-    float val = float(a_value) * .01;
-    a_label->setText(QString("%1").arg(val));
-    
-    if (!m_suppressHostUpdate) {
-	lo_send(m_host, m_controlPath, "if", a_port, float(a_value));     
-    }
-}
-
-void SynthGUI::changed_integer(int a_value, QLabel * a_label, int a_port)
-{
-    float val = float(a_value);
-    a_label->setText(QString("%1").arg(val));
-    
-    if (!m_suppressHostUpdate) {
-	lo_send(m_host, m_controlPath, "if", a_port, val);
-    }
-}
-
-void SynthGUI::changed_seconds(int a_value, QLabel * a_label, int a_port)
-{
-    float sec = float(a_value) * .01;
-    a_label->setText(QString("%1").arg(sec));
-    
-    if (!m_suppressHostUpdate) {
-	lo_send(m_host, m_controlPath, "if", a_port, float(a_value));
-    }
-}
-
-void SynthGUI::changed_pitch(int a_value, QLabel * a_label, int a_port)
-{
-    /*We need to send midi note number to the synth, as it probably still needs to process it as
-     midi_note number.  We use this to display hz to the user*/
-    
-    float f_value = float(a_value);
-    float f_hz = f_pit_midi_note_to_hz(f_value);
-    
-    a_label->setText(QString("%1 hz").arg((int)f_hz));
-    
-    if (!m_suppressHostUpdate) {
-	lo_send(m_host, m_controlPath, "if", a_port, f_value);     
-    }    
-}
-
-void SynthGUI::changed_decibels(int a_value, QLabel * a_label, int a_port)
-{
-    /*Decibels is a reasonable way to display this to the user, so just use it as it is*/
-    a_label->setText(QString("%1").arg(a_value));
-    
-    if (!m_suppressHostUpdate) {
-	lo_send(m_host, m_controlPath, "if", a_port, float(a_value));
-    }
-}
-
-
-
-
-/*Implement the event handlers from step 3.*/
-
-void SynthGUI::gainChanged(int value)
-{
-    changed_decibels(value, m_gainLabel, LMS_GAIN);    
-}
-
-void SynthGUI::wetChanged(int value)
-{
-        if (!m_suppressHostUpdate) {
-	lo_send(m_host, m_controlPath, "if", LMS_WET, float(value));
-    }  
-}
-
-void SynthGUI::outGainChanged(int value)
-{
-    changed_decibels(value, m_outGainLabel, LMS_OUT_GAIN);    
 }
 
 void SynthGUI::v_print_port_name_to_cerr(int a_port)
@@ -459,13 +162,13 @@ void SynthGUI::v_set_control(int a_port, float a_value)
     switch (a_port) 
     {
         case LMS_GAIN:
-            setGain(a_value);
+            m_ingain->setValue(a_value);
             break;
         case LMS_WET:
-            setWet(a_value);
+            m_drywet->setValue(a_value);
             break;
         case LMS_OUT_GAIN:
-            setOutGain(a_value);
+            m_outgain->setValue(a_value);
             break;
     }
 }
@@ -485,13 +188,13 @@ void SynthGUI::v_control_changed(int a_port, int a_value, bool a_suppress_host_u
     
     switch (a_port) {
     case LMS_GAIN:
-	gainChanged(a_value);
+	m_ingain->lms_value_changed(a_value);
 	break;
     case LMS_WET:
-	wetChanged(a_value);
+	m_drywet->lms_value_changed(a_value);
 	break;
     case LMS_OUT_GAIN:
-	outGainChanged(a_value);
+	m_outgain->lms_value_changed(a_value);
 	break;
     default:
 #ifdef LMS_DEBUG_MODE_QT
@@ -511,11 +214,11 @@ int SynthGUI::i_get_control(int a_port)
 {        
     switch (a_port) {
     case LMS_GAIN:
-        return m_gain->value();
+        return m_ingain->lms_knob->value();
     case LMS_WET:
-        return m_wet->value();
+        return m_drywet->lms_knob->value();
     case LMS_OUT_GAIN:
-        return m_gain->value();
+        return m_outgain->lms_knob->value();
     default:
 #ifdef LMS_DEBUG_MODE_QT
 	cerr << "Warning: received request to get nonexistent port " << a_port << endl;
--- plugins/lms_distortion/src/synth_qt_gui.h
+++ plugins/lms_distortion/src/synth_qt_gui.h
@@ -21,41 +21,18 @@ GNU General Public License for more details.
 #include <QDial>
 #include <QLabel>
 #include <QLayout>
-#include <QCheckBox>
 #include <QGroupBox>
 #include <QComboBox>
-#include <QPushButton>
-#include <qt4/QtXml/QDomDocument>
 #include <QPixmap>
-#include <QFile>
-#include <QDir>
 
 #include <string>
 #include <stdlib.h>
-
+#include "../../libmodsynth/widgets/group_box.h"
+#include "../../libmodsynth/widgets/knob_regular.h"
 
 extern "C" {
 #include <lo/lo.h>
 }
-
-/*Defines ranges and other sane defaults depending on what a knob controls*/
-enum e_knob_type
-{
-    decibels_0,
-    decibels_plus_6,
-    decibels_plus_12,
-    decibels_plus_24,
-    decibels_20_to_0,
-    decibels_30_to_0,
-    pitch,
-    zero_to_one,
-    zero_to_two,
-    zero_to_four,     
-    minus1_to_1,
-    minus12_to_12,
-    minus24_to_24,
-    minus36_to_36
-};
 
 class SynthGUI : public QFrame
 {
@@ -77,61 +54,19 @@ public:
     void v_control_changed(int, int, bool);
     int i_get_control(int);
     
-    void v_add_knob_to_layout(QDial *, e_knob_type, int, QLabel *, QGridLayout *, QString, int, int, const char *, const char *);
-    
     void v_print_port_name_to_cerr(int);
         
-public slots:
-    /*Event handlers for setting knob values*/
-    void setGain (float val);
-    void setWet (float val);
-    void setOutGain (float val);
-    
+public slots:    
     void aboutToQuit();
     
 protected slots:
-    /*Event handlers for receiving changed knob values*/
-    void gainChanged (int);
-    void wetChanged (int);
-    void outGainChanged (int);
     
     void oscRecv();
 protected:
-    QDial *newQDial( int, int, int, int );
-    
-    QDial *get_knob(e_knob_type, int _default_value = 333);
-    
-    //TODO:  update this to be more flexible about layout types
-    void add_widget(QGridLayout * _layout, int position_x, int position_y, QString _label_text,  QWidget * _widget,    
-    QLabel * _label);  
-    
-    void add_widget_no_label(QGridLayout * _layout, int position_x, int position_y, QString _label_text, QWidget * _widget);
-    
-    QGroupBox * newGroupBox(QString, QWidget *);
-    
-    QLabel * newQLabel(QWidget *);
-    
-    QCheckBox * get_checkbox(std::string _text);
-    
-    QComboBox * get_combobox(QString _choices [], int _count, QWidget * _parent);
-    
-    void changed_seconds(int, QLabel *, int);
-    void changed_pitch(int, QLabel *, int);
-    void changed_decibels(int, QLabel *, int);
-    void changed_zero_to_x(int, QLabel *, int);
-    void changed_integer(int value, QLabel * _label, int _port);
-       
-    
-    QString presets_tab_delimited [128];
-    
-    
-    /*Declare a QLabel and QDial for each knob.  Also declare any other controls that set/receive values here*/
-    QDial *m_gain;
-    QLabel *m_gainLabel;    
-    QDial *m_wet;
-    QDial *m_outGain;
-    QLabel *m_outGainLabel;
-    
+    LMS_group_box * m_groupbox;
+    LMS_knob_regular * m_ingain;
+    LMS_knob_regular * m_outgain;
+    LMS_knob_regular * m_drywet;
     
     lo_address m_host;
     QByteArray m_controlPath;
