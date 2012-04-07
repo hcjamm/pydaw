9f364b21f18f30d38c890b101575d7f2132cdf8c
Jeff Hubbard <jhubbard651@users.sf.net>
2012-04-06 20:58:01 -0400

    More work to the sample viewer

--- plugins/libmodsynth/widgets/sample_graph.h
+++ plugins/libmodsynth/widgets/sample_graph.h
@@ -12,6 +12,7 @@
 
 #include <QPixmap>
 #include <QPainter>
+#include <QMessageBox>
 #include <QList>
 #include <sndfile.h>
 #include <math.h>
@@ -19,11 +20,13 @@
 class LMS_sample_graph
 {
 public:
-    LMS_sample_graph(int a_count, int a_graph_height, int a_graph_width, QWidget * a_parent)
+    LMS_sample_graph(int a_count, int a_graph_height, int a_graph_width, QWidget * a_parent, int a_max_sample_size)
     {
         lms_graph_height = a_graph_height;
         lms_graph_width = a_graph_width;
         lms_graph_count = a_count;
+        lms_max_sample_size = a_max_sample_size;
+        lms_parent = a_parent;
                 
         m_sample_graph = new QLabel(a_parent);
         m_sample_graph->setObjectName(QString::fromUtf8("m_sample_graph"));
@@ -41,6 +44,7 @@ public:
         }
     }
         
+    QWidget * lms_parent;
     QList <QPixmap> lms_graphs;
     QList <int> lms_durations;   //TODO: get rid of this
     QList <int> m_sample_counts;
@@ -48,6 +52,7 @@ public:
     int lms_graph_count;
     int lms_graph_height;
     int lms_graph_width;
+    int lms_max_sample_size;
        
 
     void generatePreview(QString path, int a_index)
@@ -57,86 +62,114 @@ public:
             //std::cout << "a_index is greater than lms_graph_count, NOT generating a sample graph\n";
         }
         
-        SF_INFO info;
-        SNDFILE *file;
-        
         lms_graphs[a_index].fill();
 
-        printf("set sample index\n");
+        if (!path.isEmpty()) {
 
-        info.format = 0;
-        file = sf_open(path.toLocal8Bit(), SFM_READ, &info);
-        printf("Opened SNDFILE\n");
-        if (file && info.frames > 0) {
+            SF_INFO info;
+            SNDFILE *file;
 
-            float binSize = (float)info.frames / lms_graph_width;
-            float peak[2] = { 0.0f, 0.0f }, mean[2] = { 0.0f, 0.0f };
-            float *frame = (float *)malloc(info.channels * sizeof(float));
-            int bin = 0;
+            info.format = 0;
+            file = sf_open(path.toLocal8Bit(), SFM_READ, &info);
 
-            QPainter paint(&(lms_graphs[a_index]));
-
-            for (size_t i = 0; i < info.frames; ++i) {
-
-                sf_readf_float(file, frame, 1);
-
-                if (fabs(frame[0]) > peak[0]) peak[0] = fabs(frame[0]);
-                mean[0] += fabs(frame[0]);
-
-                if (info.channels > 1) {
-                    if (fabs(frame[1]) > peak[1]) peak[1] = fabs(frame[1]);
-                    mean[1] += fabs(frame[1]);
-                }
-
-                if (i == size_t((bin + 1) * binSize)) {
-
-                    float silent = 1.0 / float(lms_graph_height);
-
-                    if (info.channels == 1) {
-                        mean[1] = mean[0];
-                        peak[1] = peak[0];
-                    }
-
-                    mean[0] /= binSize;
-                    mean[1] /= binSize;
-
-                    int m = lms_graph_height / 2;
-
-                    paint.setPen(Qt::black);
-                    paint.drawLine(bin, m, bin, int(m - m * peak[0]));
-                    if (peak[0] > silent && peak[1] > silent) {
-                        paint.drawLine(bin, m, bin, int(m + m * peak[1]));
-                    }
-
-                    paint.setPen(Qt::gray);
-                    paint.drawLine(bin, m, bin, int(m - m * mean[0]));
-                    if (mean[0] > silent && mean[1] > silent) {
-                        paint.drawLine(bin, m, bin, int(m + m * mean[1]));
-                    }
-
-                    paint.setPen(Qt::black);
-                    paint.drawPoint(bin, int(m - m * peak[0]));
-                    if (peak[0] > silent && peak[1] > silent) {
-                        paint.drawPoint(bin, int(m + m * peak[1]));
-                    }
-
-                    mean[0] = mean[1] = 0.0f;
-                    peak[0] = peak[1] = 0.0f;
-
-                    ++bin;
-                }
+            if (!file) {
+                QMessageBox::warning
+                    (lms_parent, "Couldn't load audio file",
+                    QString("Couldn't load audio sample file '%1'").arg(path),
+                    QMessageBox::Ok, 0);
+                return;
             }
 
-            lms_durations[a_index] = int(100.0 * float(info.frames) / float(info.samplerate));
+            if (info.frames > lms_max_sample_size) {
+                QMessageBox::warning
+                    (lms_parent, "Couldn't use audio file",
+                    QString("Audio sample file '%1' is too large (%2 frames, maximum is %3)").arg(path).arg((int)info.frames).arg(lms_max_sample_size),
+                    QMessageBox::Ok, 0);
+                sf_close(file);
+                return;
+            } else {  /*Success*/
+                //sf_close(file);
+                info.format = 0;
+                file = sf_open(path.toLocal8Bit(), SFM_READ, &info);
 
-            m_sample_counts[a_index] = info.frames;
-        } 
-        
-        if (file) sf_close(file);
-        
-        m_sample_graph->setPixmap(lms_graphs[a_index]);
+                if (file && info.frames > 0) {
+
+                    float binSize = (float)info.frames / lms_graph_width;
+                    float peak[2] = { 0.0f, 0.0f }, mean[2] = { 0.0f, 0.0f };
+                    float *frame = (float *)malloc(info.channels * sizeof(float));
+                    int bin = 0;
+
+                    QPainter paint(&(lms_graphs[a_index]));
+
+                    for (size_t i = 0; i < info.frames; ++i) {
+
+                        sf_readf_float(file, frame, 1);
+
+                        if (fabs(frame[0]) > peak[0]) peak[0] = fabs(frame[0]);
+                        mean[0] += fabs(frame[0]);
+
+                        if (info.channels > 1) {
+                            if (fabs(frame[1]) > peak[1]) peak[1] = fabs(frame[1]);
+                            mean[1] += fabs(frame[1]);
+                        }
+
+                        if (i == size_t((bin + 1) * binSize)) {
+
+                            float silent = 1.0 / float(lms_graph_height);
+
+                            if (info.channels == 1) {
+                                mean[1] = mean[0];
+                                peak[1] = peak[0];
+                            }
+
+                            mean[0] /= binSize;
+                            mean[1] /= binSize;
+
+                            int m = lms_graph_height / 2;
+
+                            paint.setPen(Qt::black);
+                            paint.drawLine(bin, m, bin, int(m - m * peak[0]));
+                            if (peak[0] > silent && peak[1] > silent) {
+                                paint.drawLine(bin, m, bin, int(m + m * peak[1]));
+                            }
+
+                            paint.setPen(Qt::gray);
+                            paint.drawLine(bin, m, bin, int(m - m * mean[0]));
+                            if (mean[0] > silent && mean[1] > silent) {
+                                paint.drawLine(bin, m, bin, int(m + m * mean[1]));
+                            }
+
+                            paint.setPen(Qt::black);
+                            paint.drawPoint(bin, int(m - m * peak[0]));
+                            if (peak[0] > silent && peak[1] > silent) {
+                                paint.drawPoint(bin, int(m + m * peak[1]));
+                            }
+
+                            mean[0] = mean[1] = 0.0f;
+                            peak[0] = peak[1] = 0.0f;
+
+                            ++bin;
+                        }
+                    }
+
+                    lms_durations[a_index] = int(100.0 * float(info.frames) / float(info.samplerate));
+
+                    m_sample_counts[a_index] = info.frames;
+                }
+
+                m_sample_graph->setPixmap(lms_graphs[a_index]);
+
+                if (file) sf_close(file);
+
+                }
+        }        
     }
 
+    void indexChanged(int a_index)
+    {
+        m_sample_graph->setPixmap(lms_graphs[a_index]);
+    }
+    
     
 };
 
