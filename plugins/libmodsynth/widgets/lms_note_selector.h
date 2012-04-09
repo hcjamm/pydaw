f8939d17df972eedfbd5cbb93d4e49fdd1b852a1
PyDAW Team <pydawteam@users.sf.net>
2012-04-09 15:50:42 -0400

    Fixixed the adjustment of the sample's base pitch, it was being directed to the wrong OSC port

--- plugins/libmodsynth/widgets/lms_note_selector.h
+++ plugins/libmodsynth/widgets/lms_note_selector.h
@@ -58,7 +58,7 @@ class LMS_note_selector : public LMS_control
             lms_octave->setValue((int)(a_value / 12));
         }
         void lms_value_changed(int a_value)
-        {
+        {            
             lms_selected_note = (lms_note->currentIndex()) + (((lms_octave->value()) + 2) * 12);
         }
         
