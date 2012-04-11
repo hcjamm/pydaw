83cda0c4ed258edc6151cb443ab91bad5b3ebbc9
PyDAW Team <pydawteam@users.sf.net>
2012-04-11 18:04:10 -0400

    Forked LMS Distortion into LMS Mixer8

---
+++ plugins/lms_mixer8/src/libmodsynth.h
@@ -0,0 +1,91 @@
+/* 
+ * File:   libmodsynth.h
+ This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+ */
+
+#ifndef LIBMODSYNTH_H
+#define	LIBMODSYNTH_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#include <stdio.h>
+#include "../../libmodsynth/constants.h"
+    
+/*includes for any libmodsynth modules you'll be using*/
+#include "../../libmodsynth/lib/pitch_core.h"
+#include "../../libmodsynth/modules/distortion/clipper.h"
+#include "../../libmodsynth/modules/signal_routing/audio_xfade.h"
+#include "../../libmodsynth/lib/amp.h"
+   
+/*A call to an audio function that requires no parameters.  Use this for GUI switches when possible, as it will
+ require less CPU time than running through if or switch statements.
+ Functions from the library that have their own parameters (such as a pointer to 
+ their associated struct type as a parameter) should declare their own function pointer types*/
+typedef float (*fp_funcptr_audio_generic)();
+    
+/*Declare any static variables that should be used globally in LibModSynth
+ Note that any constants not requiring dynamically generated data should be declared in constants.h
+ */
+static float va_sr_recip;
+static float va_sample_rate;
+
+void v_init_lms(float f_sr);
+
+void v_init_lms(float f_sr)
+{
+    va_sample_rate = f_sr;
+    va_sr_recip = 1/f_sr;    
+}
+
+/*Define any modules here that will be used monophonically, ie:  NOT per voice here.  If you are making an effect plugin instead
+ of an instrument, you will most likely want to define all of your modules here*/
+
+typedef struct st_mono_modules
+{
+    t_clipper * clipper0;
+    t_clipper * clipper1;
+    t_audio_xfade * dry_wet;
+    t_amp * amp_ptr;
+    float outgain;
+}t_mono_modules;
+    
+
+t_mono_modules * v_mono_init(float);
+
+
+/*Initialize any modules that will be run monophonically*/
+t_mono_modules * v_mono_init(float a_sr)
+{
+    t_mono_modules * a_mono = (t_mono_modules*)malloc(sizeof(t_mono_modules));
+    a_mono->clipper0 = g_clp_get_clipper();
+    a_mono->clipper1 = g_clp_get_clipper();
+    a_mono->dry_wet = g_axf_get_audio_xfade(-3.0f);
+    a_mono->amp_ptr = g_amp_get();
+    a_mono->outgain = 1.0f;
+    
+    v_clp_set_clip_sym(a_mono->clipper0, -3.0);
+    v_clp_set_clip_sym(a_mono->clipper1, -3.0);
+    
+    return a_mono;
+}
+
+/*Define any custom functions here*/
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* LIBMODSYNTH_H */
+
