b5d41a6001e1b53dec26d14cb9728ea2fda00ff0
Jeff Hubbard <jhubbard651@users.sf.net>
2012-04-11 18:04:10 -0400

    Forked LMS Distortion into LMS Mixer8

---
+++ plugins/lms_mixer8/src/meta.h
@@ -0,0 +1,21 @@
+
+/* This file contains global identifier information for the plugin. */
+
+#ifndef META_H
+#define	META_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+/*These should be customized for each developer and plugin*/
+#define LMS_PLUGIN_NAME "lms_mixer8"
+#define LMS_PLUGIN_LONG_NAME "LMS Mixer8";
+#define LMS_PLUGIN_DEV "Jeff Hubbard <jhubbard651@users.sf.net>";
+#define LMS_PLUGIN_UUID 939937
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* META_H */
