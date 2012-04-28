ba620d96c42989a4ee1df43f76dee66f493f039f
bob <bob@bobdozer.(none)>
2012-04-28 19:42:40 -0400

    Big improvements to the voice architecture, should reduce the probability of a hung note in the event that a host misbehaves.

--- plugins/libmodsynth/lib/voice.h
+++ plugins/libmodsynth/lib/voice.h
@@ -18,8 +18,13 @@ extern "C" {
 
 typedef enum 
 {
-    off = 0,    
-    running
+    /**/
+    note_state_off = 0,
+    /**/
+    note_state_running,
+    /*Synths should iterate voices looking for any voice note_state that is set to releasing, and 
+     trigger a release event in it's amplitude envelope*/
+    note_state_releasing
 } note_state;
     
 typedef struct st_voc_voice
@@ -35,16 +40,16 @@ t_voc_single_voice g_voc_get_single_voice(int a_voice_number)
     t_voc_single_voice * f_result = (t_voc_single_voice*)malloc(sizeof(t_voc_single_voice));
     
     f_result->note = a_voice_number;
-    f_result->n_state = off;
+    f_result->n_state = note_state_off;
     
     return *f_result;
 }
 
 typedef struct st_voc_voices
 {
-    int note2voice[MIDI_NOTES];
     t_voc_single_voice * voices;
     int count;
+    int iterator;
 }t_voc_voices;
 
 t_voc_voices * g_voc_get_voices(int);
@@ -57,6 +62,8 @@ t_voc_voices * g_voc_get_voices(int a_count)
     
     f_result->voices = (t_voc_single_voice*)malloc(sizeof(t_voc_single_voice*) * a_count);
     
+    f_result->iterator = 0;
+    
     int f_i = 0;
     
     while(f_i < a_count)
@@ -64,72 +71,91 @@ t_voc_voices * g_voc_get_voices(int a_count)
         f_result->voices[f_i] = g_voc_get_single_voice(f_i);   
         f_i++;
     }
-    
-    f_i = 0;
-    
-    while(f_i < MIDI_NOTES)
-    {
-        f_result->note2voice[f_i] = 0;
-        f_i++;
-    }
-    
+        
     return f_result;
 }
 
-/* Original comment:  find the voice that is least relevant (low note priority)*/
 /*libmodsynth comment:  *data is an array of voices in an LMS struct,
  iterate through them for a free voice, or if one cannot be found,
  pick the highest voice*/
 int i_pick_voice(t_voc_voices *data, int a_current_note)
-{
-    unsigned int f_i;
+{   
+    data->iterator = 0;
+    /* Look for an inactive voice */
+    while (data->iterator<data->count) {
+	if ((data->voices[(data->iterator)].note == a_current_note) &&
+                (data->voices[(data->iterator)].n_state == note_state_running)) {
+                /*Kill the note if already being used, this is to prevent hung
+                 notes in hosts that might not handle MIDI events properly*/
+                data->voices[(data->iterator)].n_state == note_state_releasing;
+	}
+        
+        data->iterator = (data->iterator) + 1;
+    }
+    
+    data->iterator = 0;
+    /* Look for an inactive voice */
+    while (data->iterator<data->count) {
+	if (data->voices[(data->iterator)].n_state == note_state_off) {
+        
+        data->voices[(data->iterator)].note = a_current_note;            
+        return (data->iterator);
+	}
+        
+        data->iterator = (data->iterator) + 1;
+    }
+    
     int highest_note = 0;
     int highest_note_voice = 0;
     
-    /*Look for the voice being played by the current note.
-     It's more musical to kill the same note than to let it play twice,
-     guitars, pianos, etc... work that way.  It also helps to prevent hung notes*/    
-    for (f_i=0; f_i<data->count; f_i++) {
-	if (data->voices[f_i].note == a_current_note) {
-#ifdef LMS_DEBUG_VOICES            
-            printf("pick_voice found current_note:  %i\n", f_i);
-#endif
-            /*TODO:  Re-add this part*/
-            //v_adsr_set_fast_release(data[f_i].p_voice->adsr_amp);
-            break;	    
-	}
-    }
-    
-    /* Look for an inactive voice */
-    for (f_i=0; f_i<data->count; f_i++) {
-	if (data->voices[f_i].n_state == off) {
-#ifdef LMS_DEBUG_VOICES            
-            printf("pick_voice found inactive voice:  %i\n", f_i);
-#endif
-            data->note2voice[a_current_note] = f_i;
-            data->voices[f_i].note = a_current_note;
-            
-	    return f_i;
-	}
-    }
-
+    data->iterator = 0;
     /* otherwise find for the highest note and replace that */
-    for (f_i=0; f_i<data->count; f_i++) {
-	if (data->voices[f_i].note > highest_note) {
-	    highest_note = data->voices[f_i].note;
-	    highest_note_voice = f_i;
+    while ((data->iterator) < (data->count)) {
+	if (data->voices[(data->iterator)].note > highest_note) {
+	    highest_note = data->voices[(data->iterator)].note;
+	    highest_note_voice = (data->iterator);
 	}
+        data->iterator = (data->iterator) + 1;
     }
 
-#ifdef LMS_DEBUG_VOICES            
-            printf("pick_voice found highest voice:  %i\n", highest_note_voice);
-#endif
-    data->note2voice[a_current_note] = highest_note_voice;
     data->voices[highest_note_voice].note = a_current_note;
             
     return highest_note_voice;        
 }
 
+/* int i_voc_note_off(t_voc_voices * a_voc)
+ * 
+ * After running this event at note_off, you should run a loop like this:
+ * 
+ *         plugin_data->i_iterator = 0;
+        
+        while(plugin_data->i_iterator < (plugin_data->voices->count))
+        {
+            if((plugin_data->voices->voices[(plugin_data->i_iterator)].n_state) == note_state_releasing)
+            {
+                v_poly_note_off(plugin_data->data[(plugin_data->i_iterator)]);
+            }
+            
+            plugin_data->i_iterator = (plugin_data->i_iterator) + 1;
+        }
+ * 
+ * Where v_poly_note_off is a function that releases ADSR envelopes, etc...
+ */
+void v_voc_note_off(t_voc_voices * a_voc, int a_note)
+{
+    a_voc->iterator = 0;
+    
+    while((a_voc->iterator) < (a_voc->count))
+    {
+        if(((a_voc->voices[(a_voc->iterator)].note) == a_note) &&
+           ((a_voc->voices[(a_voc->iterator)].n_state) == note_state_running))
+        {
+            a_voc->voices[(a_voc->iterator)].n_state = note_state_releasing;            
+        }
+        
+        a_voc->iterator  = (a_voc->iterator) + 1;
+    }    
+}
 
 #ifdef	__cplusplus
 }
