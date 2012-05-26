83afe870e5bcf493edefa309d0bea87631ab55d1
bob <bob@ubuntustudio.(none)>
2012-05-26 10:19:13 -0400

    Make the curve knob optional, since it's not supported yet by the corresponding ramp_env module

--- plugins/libmodsynth/widgets/ui_modules/ramp_env.h
+++ plugins/libmodsynth/widgets/ui_modules/ramp_env.h
@@ -14,7 +14,7 @@
 class LMS_ramp_env
 {
 public:
-    LMS_ramp_env(QWidget * a_parent, LMS_style_info * a_style, int a_time_port, int a_amt_port, int a_curve_port, QString a_label)
+    LMS_ramp_env(QWidget * a_parent, LMS_style_info * a_style, int a_time_port, int a_amt_port, int a_curve_port, bool a_show_curve_knob, QString a_label)
     {
         lms_groupbox = new LMS_group_box(a_parent, QString("Pitch Envelope"), a_style);
         
@@ -22,8 +22,11 @@ public:
         lms_groupbox->lms_add_h(lms_amt_knob);
         lms_time_knob = new LMS_knob_regular(QString("Time"), 1, 200, 1, 1, QString("0"), lms_groupbox->lms_groupbox, a_style, lms_kc_decimal, a_time_port);
         lms_groupbox->lms_add_h(lms_time_knob);        
-        lms_curve_knob = new LMS_knob_regular(QString("Curve"), 1, 200, 1, 1, QString("0"), lms_groupbox->lms_groupbox, a_style, lms_kc_decimal, a_time_port);
-        lms_groupbox->lms_add_h(lms_curve_knob);
+        if(a_show_curve_knob)
+        {
+            lms_curve_knob = new LMS_knob_regular(QString("Curve"), 1, 200, 1, 1, QString("0"), lms_groupbox->lms_groupbox, a_style, lms_kc_decimal, a_time_port);
+            lms_groupbox->lms_add_h(lms_curve_knob);
+        }
     }
     
     LMS_group_box * lms_groupbox;
