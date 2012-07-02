e1007bc417bf0b6f25711a786c7073ba3ee007ea
PyDAW Team <pydawteam@users.sf.net>
2012-07-01 21:44:17 -0400

    Add initial files for my forthcoming multieffect

---
+++ plugins/lms_modulex/src/meta.h
@@ -0,0 +1,21 @@
+
+/* This file contains global identifier information for the plugin. */
+
+#ifndef META_H
+#define	META_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+/*These should be customized for each developer and plugin*/
+#define LMS_PLUGIN_NAME "LMS_MODULEX"
+#define LMS_PLUGIN_LONG_NAME "LMS Modulex";
+#define LMS_PLUGIN_DEV "Jeff Hubbard <jhubbard651@users.sf.net>";
+#define LMS_PLUGIN_UUID 402679
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* META_H */
