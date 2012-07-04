e064d34f57f5337afd6acff8cb5f6ba9f374fede
Jeff Hubbard <jhubbard651@users.sf.net>
2012-07-04 11:42:02 -0400

    Fix most(all?) of the UI bugs

--- plugins/libmodsynth/widgets/ui_modules/multieffect_basic.h
+++ plugins/libmodsynth/widgets/ui_modules/multieffect_basic.h
@@ -52,70 +52,90 @@ public:
                 lms_knob1->lms_conv_type = lms_kc_none;
                 lms_knob2->lms_conv_type = lms_kc_none;
                 lms_knob3->lms_conv_type = lms_kc_none;
+                lms_knob1->lms_value->setText(QString(""));
+                lms_knob2->lms_value->setText(QString(""));
+                lms_knob3->lms_value->setText(QString(""));
                 break;
             case 1: //LP2
                 lms_knob1->lms_label->setText(QString("Cutoff"));
                 lms_knob2->lms_label->setText(QString("Res"));
-                lms_knob3->lms_label->setText(QString("(unused)"));
-                lms_knob1->lms_conv_type = lms_kc_pitch;
-                lms_knob2->lms_conv_type = lms_kc_none;
+                lms_knob3->lms_label->setText(QString("unused"));
+                lms_knob1->lms_conv_type = lms_kc_127_pitch;
+                lms_knob2->lms_conv_type = lms_kc_127_zero_to_x;
+                lms_knob2->lms_set_127_min_max(-30.0f, 0.0f);
                 lms_knob3->lms_conv_type = lms_kc_none;
+                lms_knob3->lms_value->setText(QString(""));
                 break;
             case 2: //LP4
                 lms_knob1->lms_label->setText(QString("Cutoff"));
                 lms_knob2->lms_label->setText(QString("Res"));
-                lms_knob3->lms_label->setText(QString("(unused)"));
-                lms_knob1->lms_conv_type = lms_kc_pitch;
-                lms_knob2->lms_conv_type = lms_kc_none;
+                lms_knob3->lms_label->setText(QString("unused"));
+                lms_knob1->lms_conv_type = lms_kc_127_pitch;
+                lms_knob2->lms_conv_type = lms_kc_127_zero_to_x;
+                lms_knob2->lms_set_127_min_max(-30.0f, 0.0f);
                 lms_knob3->lms_conv_type = lms_kc_none;
+                lms_knob3->lms_value->setText(QString(""));
                 break;
             case 3: //HP2
                 lms_knob1->lms_label->setText(QString("Cutoff"));
                 lms_knob2->lms_label->setText(QString("Res"));
-                lms_knob3->lms_label->setText(QString("(unused)"));
-                lms_knob1->lms_conv_type = lms_kc_pitch;
-                lms_knob2->lms_conv_type = lms_kc_none;
+                lms_knob3->lms_label->setText(QString("unused"));
+                lms_knob1->lms_conv_type = lms_kc_127_pitch;
+                lms_knob2->lms_conv_type = lms_kc_127_zero_to_x;
+                lms_knob2->lms_set_127_min_max(-30.0f, 0.0f);
                 lms_knob3->lms_conv_type = lms_kc_none;
+                lms_knob3->lms_value->setText(QString(""));
                 break;
             case 4: //HP4
                 lms_knob1->lms_label->setText(QString("Cutoff"));
                 lms_knob2->lms_label->setText(QString("Res"));
-                lms_knob3->lms_label->setText(QString("(unused)"));
-                lms_knob1->lms_conv_type = lms_kc_pitch;
-                lms_knob2->lms_conv_type = lms_kc_none;
+                lms_knob3->lms_label->setText(QString("unused"));
+                lms_knob1->lms_conv_type = lms_kc_127_pitch;
+                lms_knob2->lms_conv_type = lms_kc_127_zero_to_x;
+                lms_knob2->lms_set_127_min_max(-30.0f, 0.0f);
                 lms_knob3->lms_conv_type = lms_kc_none;
+                lms_knob3->lms_value->setText(QString(""));
                 break;
             case 5: //BP2
                 lms_knob1->lms_label->setText(QString("Cutoff"));
                 lms_knob2->lms_label->setText(QString("Res"));
-                lms_knob3->lms_label->setText(QString("(unused)"));
-                lms_knob1->lms_conv_type = lms_kc_pitch;
-                lms_knob2->lms_conv_type = lms_kc_none;
+                lms_knob3->lms_label->setText(QString("unused"));
+                lms_knob1->lms_conv_type = lms_kc_127_pitch;
+                lms_knob2->lms_conv_type = lms_kc_127_zero_to_x;
+                lms_knob2->lms_set_127_min_max(-30.0f, 0.0f);
                 lms_knob3->lms_conv_type = lms_kc_none;
+                lms_knob3->lms_value->setText(QString(""));
                 break;
             case 6: //BP4
                 lms_knob1->lms_label->setText(QString("Cutoff"));
                 lms_knob2->lms_label->setText(QString("Res"));
-                lms_knob3->lms_label->setText(QString("(unused)"));
-                lms_knob1->lms_conv_type = lms_kc_pitch;
-                lms_knob2->lms_conv_type = lms_kc_none;
+                lms_knob3->lms_label->setText(QString("unused"));
+                lms_knob1->lms_conv_type = lms_kc_127_pitch;
+                lms_knob2->lms_conv_type = lms_kc_127_zero_to_x;
+                lms_knob2->lms_set_127_min_max(-30.0f, 0.0f);
                 lms_knob3->lms_conv_type = lms_kc_none;
+                lms_knob3->lms_value->setText(QString(""));
                 break;
             case 7: //EQ
                 lms_knob1->lms_label->setText(QString("Cutoff"));
                 lms_knob2->lms_label->setText(QString("Res"));
                 lms_knob3->lms_label->setText(QString("Gain"));
-                lms_knob1->lms_conv_type = lms_kc_pitch;
-                lms_knob2->lms_conv_type = lms_kc_none;
-                lms_knob3->lms_conv_type = lms_kc_none;
+                lms_knob1->lms_conv_type = lms_kc_127_pitch;
+                lms_knob2->lms_conv_type = lms_kc_127_zero_to_x;
+                lms_knob2->lms_set_127_min_max(-30.0f, 0.0f);
+                lms_knob3->lms_conv_type = lms_kc_127_zero_to_x;
+                lms_knob3->lms_set_127_min_max(-24.0f, 24.0f);
                 break;
             case 8: //Distortion
                 lms_knob1->lms_label->setText(QString("Gain"));
                 lms_knob2->lms_label->setText(QString("Dry/Wet"));
-                lms_knob3->lms_label->setText(QString("(unused)"));
-                lms_knob1->lms_conv_type = lms_kc_none;
-                lms_knob2->lms_conv_type = lms_kc_decimal;
+                lms_knob3->lms_label->setText(QString("unused"));
+                lms_knob1->lms_conv_type = lms_kc_127_zero_to_x;
+                lms_knob1->lms_set_127_min_max(0.0f, 36.0f);
+                lms_knob2->lms_conv_type = lms_kc_none;
+                lms_knob2->lms_value->setText(QString(""));
                 lms_knob3->lms_conv_type = lms_kc_none;
+                lms_knob3->lms_value->setText(QString(""));
                 break;
         }
     }
