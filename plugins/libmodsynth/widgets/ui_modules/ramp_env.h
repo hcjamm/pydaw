2ef9a2621be49e11ab26fb67d618cfd1c11b2b9d
Jeff Hubbard <jhubbard651@users.sf.net>
2012-07-07 22:29:24 -0400

    Make the amount knob optional

--- plugins/libmodsynth/widgets/ui_modules/ramp_env.h
+++ plugins/libmodsynth/widgets/ui_modules/ramp_env.h
@@ -14,12 +14,16 @@
 class LMS_ramp_env
 {
 public:
-    LMS_ramp_env(QWidget * a_parent, LMS_style_info * a_style, int a_time_port, int a_amt_port, int a_curve_port, bool a_show_curve_knob, QString a_label)
+    LMS_ramp_env(QWidget * a_parent, LMS_style_info * a_style, int a_time_port, int a_amt_port, int a_curve_port, bool a_show_curve_knob, QString a_label, bool a_show_amt_knob)
     {
-        lms_groupbox = new LMS_group_box(a_parent, QString("Pitch Envelope"), a_style);
+        lms_groupbox = new LMS_group_box(a_parent, a_label, a_style);
         
-        lms_amt_knob = new LMS_knob_regular(QString("Amt"), -36, 36, 1, 0, QString(""), lms_groupbox->lms_groupbox, a_style, lms_kc_integer, a_amt_port);
-        lms_groupbox->lms_add_h(lms_amt_knob);
+        if(a_show_amt_knob)
+        {
+            lms_amt_knob = new LMS_knob_regular(QString("Amt"), -36, 36, 1, 0, QString(""), lms_groupbox->lms_groupbox, a_style, lms_kc_integer, a_amt_port);
+            lms_groupbox->lms_add_h(lms_amt_knob);
+        }
+        
         lms_time_knob = new LMS_knob_regular(QString("Time"), 1, 200, 1, 1, QString("0"), lms_groupbox->lms_groupbox, a_style, lms_kc_decimal, a_time_port);
         lms_groupbox->lms_add_h(lms_time_knob);        
         if(a_show_curve_knob)
