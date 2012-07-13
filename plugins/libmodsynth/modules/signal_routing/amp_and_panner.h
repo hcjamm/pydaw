3f1bffab312b49e0bde93b7a984db8adad8049a2
PyDAW Team <pydawteam@users.sf.net>
2012-07-12 22:10:08 -0400

    Implement an amp/panner

--- plugins/libmodsynth/modules/signal_routing/amp_and_panner.h
+++ plugins/libmodsynth/modules/signal_routing/amp_and_panner.h
@@ -10,6 +10,7 @@
 
 #include "../../lib/amp.h"
 #include "../../lib/lms_math.h"
+#include "../../lib/fast_sine.h"
 
 #ifdef	__cplusplus
 extern "C" {
@@ -18,14 +19,14 @@ extern "C" {
 typedef struct st_amp_and_panner
 {
     float amp_db;
-    float pan_law;
-    float pan; //0 to 1
-    float pan_amt;
+    float amp_linear;    
+    float pan; //0 to 1    
     float amp_linear0;
     float amp_linear1;
     float output0;
     float output1;
     t_amp * amp_ptr;
+    t_lin_interpolater * linear_interpolator;
 }t_amp_and_panner;
 
 void v_app_set(t_amp_and_panner*,float,float);
@@ -37,13 +38,10 @@ void v_app_set(t_amp_and_panner* a_app,float a_db,float a_pan)
     a_app->amp_db = a_db;
     a_app->pan = a_pan;
     
-    a_app->amp_linear0 = f_db_to_linear_fast(
-            ((f_lms_ceiling(((a_pan * 2) - 1), 1) * (a_app->pan_amt)) + a_db)
-            , a_app->amp_ptr);
+    a_app->amp_linear = f_db_to_linear_fast(a_db , a_app->amp_ptr);
     
-    a_app->amp_linear1 = f_db_to_linear_fast(
-            ((f_lms_ceiling((1 - (a_pan * 2)), 1) * (a_app->pan_amt)) + a_db)
-            , a_app->amp_ptr);
+    a_app->amp_linear0 = (f_sine_fast_run(((a_pan * .5f) + 0.25f), a_app->linear_interpolator) * 0.5f + 1) * (a_app->amp_linear);
+    a_app->amp_linear1 = (f_sine_fast_run((0.75f - (a_pan * 0.5f)), a_app->linear_interpolator) * 0.5f + 1) * (a_app->amp_linear);    
 }
 
 void v_app_run(t_amp_and_panner* a_app, float a_in0, float a_in1)
@@ -56,14 +54,15 @@ t_amp_and_panner * g_app_get()
 {
     t_amp_and_panner * f_result = (t_amp_and_panner*)malloc(sizeof(t_amp_and_panner));
     f_result->amp_db = 0.0f;
-    f_result->pan_law = 0.0f;
     f_result->pan = 0.0f;
-    f_result->pan_amt = 0.0f;
     f_result->amp_linear0 = 1.0f;
     f_result->amp_linear1 = 1.0f;
     f_result->output0 = 0.0f;
     f_result->output1 = 0.0f;
     f_result->amp_ptr = g_amp_get();
+    f_result->linear_interpolator = g_lin_get();
+    
+    return f_result;
 }
 
 #ifdef	__cplusplus
