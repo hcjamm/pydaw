2e7421a027a19ad831fc0dbad8db7a4d0ecb819f
Jeff Hubbard <jhubbard651@users.sf.net>
2012-07-20 22:41:10 -0400

    Various fixes, still a W.I.P.

--- plugins/libmodsynth/widgets/lms_file_browser.h
+++ plugins/libmodsynth/widgets/lms_file_browser.h
@@ -26,99 +26,87 @@
 
 class LMS_file_browser
 {
-public:
-    QWidget *verticalLayoutWidget;
+public:    
     QVBoxLayout *m_file_browser_verticalLayout;
     QLabel *m_bookmarks_label;
     QListWidget *m_bookmarks_listWidget;
     QLabel *m_folders_label;
     QLabel *m_folder_path_label;
-    QListWidget *m_folders_listWidget;
-    QHBoxLayout *horizontalLayout;
+    QListWidget *m_folders_listWidget;    
     QPushButton *m_up_pushButton;
     QLabel *m_files_label;
     QListWidget *m_files_listWidget;
-    QHBoxLayout *m_files_buttons_horizontalLayout;
     QPushButton *m_preview_pushButton;
     QPushButton *m_load_pushButton; 
 
     LMS_file_browser(QWidget *a_parent)
-    {
-        
-         verticalLayoutWidget->setObjectName(QString::fromUtf8("verticalLayoutWidget"));
-        verticalLayoutWidget->setGeometry(QRect(60, 100, 201, 591));
-        m_file_browser_verticalLayout = new QVBoxLayout(verticalLayoutWidget);
+    {     
+        m_file_browser_verticalLayout = new QVBoxLayout(a_parent);
+                
         m_file_browser_verticalLayout->setObjectName(QString::fromUtf8("m_file_browser_verticalLayout"));
         m_file_browser_verticalLayout->setContentsMargins(0, 0, 0, 0);
-        m_bookmarks_label = new QLabel(verticalLayoutWidget);
+        m_bookmarks_label = new QLabel(a_parent);
         m_bookmarks_label->setObjectName(QString::fromUtf8("m_bookmarks_label"));
         m_bookmarks_label->setAlignment(Qt::AlignCenter);
 
         m_file_browser_verticalLayout->addWidget(m_bookmarks_label);
 
-        m_bookmarks_listWidget = new QListWidget(verticalLayoutWidget);
+        m_bookmarks_listWidget = new QListWidget(a_parent);
         m_bookmarks_listWidget->setObjectName(QString::fromUtf8("m_bookmarks_listWidget"));
         m_bookmarks_listWidget->setSelectionMode(QAbstractItemView::NoSelection);
+        m_bookmarks_listWidget->setMaximumWidth(240);
 
         m_file_browser_verticalLayout->addWidget(m_bookmarks_listWidget);
 
-        m_folders_label = new QLabel(verticalLayoutWidget);
+        m_folders_label = new QLabel(a_parent);
         m_folders_label->setObjectName(QString::fromUtf8("m_folders_label"));
         m_folders_label->setAlignment(Qt::AlignCenter);
 
         m_file_browser_verticalLayout->addWidget(m_folders_label);
 
-        m_folder_path_label = new QLabel(verticalLayoutWidget);
+        m_folder_path_label = new QLabel(a_parent);
         m_folder_path_label->setObjectName(QString::fromUtf8("m_folder_path_label"));
 
         m_file_browser_verticalLayout->addWidget(m_folder_path_label);
 
-        m_folders_listWidget = new QListWidget(verticalLayoutWidget);
+        m_folders_listWidget = new QListWidget(a_parent);
         m_folders_listWidget->setObjectName(QString::fromUtf8("m_folders_listWidget"));
         m_folders_listWidget->setSelectionMode(QAbstractItemView::NoSelection);
+        m_folders_listWidget->setMaximumWidth(240);
 
         m_file_browser_verticalLayout->addWidget(m_folders_listWidget);
-
-        horizontalLayout = new QHBoxLayout();
-        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
-        m_up_pushButton = new QPushButton(verticalLayoutWidget);
+        
+        m_up_pushButton = new QPushButton(a_parent);
         m_up_pushButton->setObjectName(QString::fromUtf8("m_up_pushButton"));
+        m_up_pushButton->setMaximumWidth(240);
 
-        horizontalLayout->addWidget(m_up_pushButton);
+        m_file_browser_verticalLayout->addWidget(m_up_pushButton);
 
-
-        m_file_browser_verticalLayout->addLayout(horizontalLayout);
-
-        m_files_label = new QLabel(verticalLayoutWidget);
+        m_files_label = new QLabel(a_parent);
         m_files_label->setObjectName(QString::fromUtf8("m_files_label"));
         m_files_label->setAlignment(Qt::AlignCenter);
 
         m_file_browser_verticalLayout->addWidget(m_files_label);
 
-        m_files_listWidget = new QListWidget(verticalLayoutWidget);
-        new QListWidgetItem(m_files_listWidget);
-        new QListWidgetItem(m_files_listWidget);
-        new QListWidgetItem(m_files_listWidget);
-        new QListWidgetItem(m_files_listWidget);
+        m_files_listWidget = new QListWidget(a_parent);
         m_files_listWidget->setObjectName(QString::fromUtf8("m_files_listWidget"));
         m_files_listWidget->setSelectionMode(QAbstractItemView::ExtendedSelection);
+        m_files_listWidget->setMaximumWidth(240);
 
         m_file_browser_verticalLayout->addWidget(m_files_listWidget);
-
-        m_files_buttons_horizontalLayout = new QHBoxLayout();
-        m_files_buttons_horizontalLayout->setObjectName(QString::fromUtf8("m_files_buttons_horizontalLayout"));
-        m_preview_pushButton = new QPushButton(verticalLayoutWidget);
-        m_preview_pushButton->setObjectName(QString::fromUtf8("m_preview_pushButton"));
-
-        m_files_buttons_horizontalLayout->addWidget(m_preview_pushButton);
-
-        m_load_pushButton = new QPushButton(verticalLayoutWidget);
+        
+        m_load_pushButton = new QPushButton(a_parent);
         m_load_pushButton->setObjectName(QString::fromUtf8("m_load_pushButton"));
+        m_load_pushButton->setMaximumWidth(240);
 
-        m_files_buttons_horizontalLayout->addWidget(m_load_pushButton);
+        m_file_browser_verticalLayout->addWidget(m_load_pushButton);
 
+        
+        m_preview_pushButton = new QPushButton(a_parent);
+        m_preview_pushButton->setObjectName(QString::fromUtf8("m_preview_pushButton"));
+        m_preview_pushButton->setMaximumWidth(240);
 
-        m_file_browser_verticalLayout->addLayout(m_files_buttons_horizontalLayout);
+        m_file_browser_verticalLayout->addWidget(m_preview_pushButton);
 
         m_bookmarks_label->setText(QApplication::translate("Form", "Bookmarks", 0, QApplication::UnicodeUTF8));
         m_folders_label->setText(QApplication::translate("Form", "Folders", 0, QApplication::UnicodeUTF8));
@@ -128,8 +116,8 @@ public:
 
         m_files_listWidget->setSortingEnabled(TRUE);
 
-        m_preview_pushButton->setText(QApplication::translate("Form", "Preview", 0, QApplication::UnicodeUTF8));
-        m_load_pushButton->setText(QApplication::translate("Form", "Load", 0, QApplication::UnicodeUTF8));
+        m_preview_pushButton->setText(QString("Preview"));
+        m_load_pushButton->setText(QString("Load"));
         
     }
     
@@ -159,7 +147,7 @@ public:
         m_files_listWidget->clear();
         m_folders_listWidget->clear();
         
-        m_folder_path_label = f_dir.path();
+        m_folder_path_label->setText(f_dir.path());
         
         foreach(QString f_entry, f_list)
         {
@@ -181,13 +169,13 @@ public:
     /* Returns a QStringList of the files that should be opened*/
     QStringList files_opened()
     {
-        QStringList f_result = QStringList();
+        QStringList f_result;
+                
+        QList<QListWidgetItem*> f_widget_items = m_files_listWidget->selectedItems();
         
-        QList<QListWidgetItem*> f_result = m_files_listWidget->selectedItems();
-        
-        for(int f_i = 0; f_i < f_result.count(); f_i++)
+        for(int f_i = 0; f_i < f_widget_items.count(); f_i++)
         {
-            f_result << f_result.at(f_i);
+            f_result << f_widget_items[f_i]->text();
         }
         
         return f_result;
