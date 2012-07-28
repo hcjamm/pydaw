35cddc5c25803f11ba69d1e0e5a7601768a0f114
PyDAW Team <pydawteam@users.sf.net>
2012-07-28 16:49:14 -0400

    Remove obsolete plugins that were merged into LMS Modulex

--- plugins/lms_comb/build.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-
-require "../build-lib.pl";
-
-#change $plugin_name and $clean when you fork a LibModSynth plugin
-$plugin_name = "lms_comb.so";
-$clean = "sudo rm -R $plugin_path/lms_comb*";
-
-run_script();
