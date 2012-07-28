32a597b6c987a402185e023bb14fdfec6180a030
Jeff Hubbard <jhubbard651@users.sf.net>
2012-07-28 19:11:05 -0400

    Revert not freeing memory as a fix to Protractor/Qtractor.  The amount of memory lost wasn't a big deal in the greater scheme of things, but it turns out that Qtractors current DSSI implementation just has more breakage than I care to fix.  It seems to be choking above a certain port count, I'm just going to revert to running these as standalone and connecting via jack/alsa.

--- plugins/euphoria/src/synth.c
+++ plugins/euphoria/src/synth.c
@@ -1292,8 +1292,7 @@ char *samplerLoad(Sampler *plugin_data, const char *path, int a_index)
     }
     
     pthread_mutex_unlock(&plugin_data->mutex);
-
-    /*
+    
     if (tmpOld[0]) 
     {
         free(tmpOld[0]);
@@ -1303,7 +1302,6 @@ char *samplerLoad(Sampler *plugin_data, const char *path, int a_index)
     {
         free(tmpOld[1]);
     }
-    */
     
     //printf("%s: loaded %s (%ld samples from original %ld channels resampled from %ld frames at %ld Hz)\n", Sampler_Stereo_LABEL, path, (long)samples, (long)info.channels, (long)info.frames, (long)info.samplerate);
 
@@ -1374,11 +1372,9 @@ char *samplerClear(Sampler *plugin_data, int a_index)
 
     pthread_mutex_unlock(&plugin_data->mutex);
 
-    /*
     if (tmpOld[0]) free(tmpOld[0]);
     if (tmpOld[1]) free(tmpOld[1]);
-    */
-    
+        
     return NULL;
 }
 
