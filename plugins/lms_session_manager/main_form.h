cc7511f7b4583befd15166d9fbfb22b69f210d62
Jeff Hubbard <jhubbard651@users.sf.net>
2012-07-31 22:49:55 -0400

    Initial UI mock-up of the session manager, nothing happening on the back-end yet...

--- plugins/lms_session_manager/main_form.h
+++ plugins/lms_session_manager/main_form.h
@@ -16,16 +16,44 @@
 #include "../libmodsynth/widgets/lms_main_layout.h"
 #include "../libmodsynth/widgets/mod_matrix.h"
 
+#define LMS_INSTRUMENT_COUNT 16
+
 class main_form : public QWidget
 {
     public:
     main_form()
     {
-        QRect f_rect(0, 0, 800, 600);
+        QRect f_rect(0, 0, 450, 600);
         this->setGeometry(f_rect);
-        this->setStyleSheet(QString("background-color: white; color:black;"));
+        //this->setStyleSheet(QString("background-color: white; color:black;"));
+        this->setWindowTitle(QString("LMS Session Manager"));
+                
+        
+        QList<LMS_mod_matrix_column*> f_columns;
+        f_columns << new LMS_mod_matrix_column(radiobutton, QString("MIDI Keyboard"), 0, 1,0);
+        f_columns << new LMS_mod_matrix_column(QStringList() << QString("None") << QString("Euphoria") << QString("Ray-V"), QString("Plugin"));
+        f_columns << new LMS_mod_matrix_column(QStringList(), QString("MIDI Input"));
+        f_columns << new LMS_mod_matrix_column(QStringList(), QString("Audio Output"));
+        
+        LMS_style_info * f_style = new LMS_style_info(45);
+        
+        main_layout = new LMS_main_layout(this);
+        
+        QLabel * f_midi_kbd_label = new QLabel(this);
+        f_midi_kbd_label->setText(QString("MIDI Keyboard:"));
+        midi_keyboard_combobox = new QComboBox(this);
+        
+        main_layout->lms_add_widget(f_midi_kbd_label);
+        main_layout->lms_add_widget(midi_keyboard_combobox);
+        
+        main_layout->lms_add_layout();
+        
+        mod_matrix = new LMS_mod_matrix(this, LMS_INSTRUMENT_COUNT, f_columns, 0, f_style);        
+        main_layout->lms_add_widget(mod_matrix->lms_mod_matrix);
+                
     }   
     
+    QComboBox * midi_keyboard_combobox;
     LMS_mod_matrix * mod_matrix;
     LMS_main_layout * main_layout;
     
