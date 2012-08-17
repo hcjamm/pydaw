3a8b077404659f830b1df9fa7b1d8bf6ca699108
Jeff Hubbard <jhubbard651@users.sf.net>
2012-08-16 20:41:34 -0400

    Remove deprecated functionality(although in hindsight, that might've been the way to do it...)

--- plugins/libmodsynth/widgets/lms_session_manager.h
+++ plugins/libmodsynth/widgets/lms_session_manager.h
@@ -15,11 +15,8 @@
 #include <QDir>
 
 #define LMS_SESSION_NOTIFICATION_DIRECTORY QString("/notify/")
-#define LMS_SESSION_NOTIFY_OPEN QString(".open")
 #define LMS_SESSION_NOTIFY_SAVE QString(".save")
 #define LMS_SESSION_NOTIFY_QUIT QString(".quit")
-
-#define LMS_SESSION_INSTRUMENT_FILE QString(".lis")
 
 /* These are called from the UI on the event handler of a timer.  They poll the project directory for files created by the session manager
  * notifying when to perform certain actions
@@ -34,13 +31,6 @@ public:
         
         return check_notification_file_exists(f_file_name);
     }
-    
-    static bool is_opening(QString a_project_directory, QString a_instance_name)
-    {
-        QString f_file_name = a_project_directory + LMS_SESSION_NOTIFICATION_DIRECTORY + a_instance_name + LMS_SESSION_NOTIFY_OPEN;
-        
-        return check_notification_file_exists(f_file_name);
-    }        
 
     static bool is_quitting(QString a_project_directory, QString a_instance_name)
     {
