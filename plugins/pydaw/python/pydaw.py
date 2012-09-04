d2016c53cb959f3b2fb6378555a46bdb1e866135
PyDAW Team <pydawteam@users.sf.net>
2012-09-04 18:02:51 -0400

    More PyDAW work

--- plugins/pydaw/python/pydaw.py
+++ plugins/pydaw/python/pydaw.py
@@ -1,7 +1,9 @@
-# a_test.py
-# Copyrighted (C) 2009 by Conrad "Lynx" Wong
-
-# Test dialog using the Kytten GUI
+# PyDAW - Part of the LibModSynth project
+#
+# A DAW using Python and OpenGL for the UI, and a high performance audio/MIDI
+# backend written in C
+#
+# Copyright 2012, Jeff Hubbard
 
 import os
 import pyglet
@@ -12,18 +14,18 @@ from pyglet import gl
 VERSION = '5.9'
 
 import kytten
-from background import Background
 
 # Default theme, gold-colored
 theme = kytten.Theme(os.path.join(os.getcwd(), 'theme'), override={
-    "gui_color": [64, 128, 255, 255],
-    "font_size": 14
+    #"gui_color": [64, 128, 255, 255],
+    "gui_color": [255, 240, 255, 255],
+    "font_size": 12
 })
 
 # Default theme, blue-colored
 theme2 = kytten.Theme(theme, override={
     "gui_color": [255, 235, 128, 255],
-    "font_size": 12
+    "font_size": 10
 })
 
 # Callback functions for dialogs which may be of interest
@@ -72,7 +74,7 @@ class seq_track:
         pass
     def on_name_changed(self, new_name):
         pass
-    def __init__(self, a_track_text="track"):
+    def __init__(self, a_track_text="track"):        
         self.dialog = kytten.Dialog(
         	kytten.Frame(
                  kytten.VerticalLayout([
@@ -91,28 +93,38 @@ class seq_track:
         	window=window, batch=batch, group=fg_group,
         	anchor=kytten.ANCHOR_CENTER,
         	theme=theme2, on_escape=on_escape, movable=True)
-         
+        
+
+        self.midi_items = []
+        self.midi_item_dialogs = []
+        
+        for i in range(0, 8):
+            self.midi_items.append(midi_item())
+            self.midi_item_dialogs.append(self.midi_items[i].dialog)        
+        
         self.midi_track = kytten.Dialog(
         	kytten.Frame(
-                 kytten.HorizontalLayout([
-                         
-                     ])
+                 kytten.HorizontalLayout(self.midi_item_dialogs)
         	),
         	window=window, batch=batch, group=fg_group,
         	anchor=kytten.ANCHOR_CENTER,
         	theme=theme2, on_escape=on_escape, movable=True)
+         
+        self.hlayout = kytten.HorizontalLayout([self.dialog, self.midi_track])
     
 
 class track_view:
     def __init__(self):
         
-        self.tracks = []
+        self.tracks = [
+                kytten.HorizontalLayout([transport().dialog, kytten.Slider(max_value=16)])
+            ]
 
-        for i in range(0, 15):        
-            self.tracks.append(seq_track(a_track_text="track" + str(i)).dialog)
+        for i in range(0, 16):        
+            self.tracks.append(seq_track(a_track_text="track" + str(i)).hlayout)
         
         self.layout = kytten.VerticalLayout(self.tracks)
-        self.scrollable = kytten.Scrollable(self.layout, width=1000, height=600, always_show_scrollbars=False)
+        self.scrollable = kytten.Scrollable(self.layout, width=1180, height=600, always_show_scrollbars=False)
         
         self.dialog = kytten.Dialog(
             	kytten.Frame(
@@ -124,7 +136,21 @@ class track_view:
     
 
 class midi_item:
-    pass
+    def on_mute(self, value):
+        pass
+    def __init__(self):
+        self.dialog =  kytten.Dialog(
+        	kytten.Frame(
+                 kytten.VerticalLayout([
+                         kytten.Button(text="Edit"),                         
+                         kytten.HorizontalLayout([
+                              kytten.Input(text="MIDI Item", length=15, max_length=20),                              
+                             ])
+                     ])
+        	),
+        	window=window, batch=batch, group=fg_group,
+        	anchor=kytten.ANCHOR_CENTER,
+        	theme=theme2, on_escape=on_escape, movable=True)
  
 
 if __name__ == '__main__':
@@ -147,11 +173,9 @@ if __name__ == '__main__':
     def update(dt):
 	window.dispatch_event('on_update', dt)
     pyglet.clock.schedule(update)
-
-    transport()
     
     track_view_test = track_view()
-             
+                         
     # Change this flag to run with profiling and dump top 20 cumulative times
     if True:
 	pyglet.app.run()
