0c097ecfa43390f88682b45abe0f36939a898648
PyDAW Team <pydawteam@users.sf.net>
2012-09-05 21:35:08 -0400

    Wrapping up some minor changes, I realized that I'm going to have to move some classes into a single class for them to be properly aware of each other without creating unmanageable code

--- plugins/pydaw/python/pydaw.py
+++ plugins/pydaw/python/pydaw.py
@@ -15,6 +15,8 @@ VERSION = '5.9'
 
 import kytten
 
+from lmssession import lms_session
+
 # Default theme, gold-colored
 theme = kytten.Theme(os.path.join(os.getcwd(), 'theme'), override={
     #"gui_color": [64, 128, 255, 255],
@@ -39,15 +41,28 @@ class transport:
         print("Stopping")
     def on_rec(self):
         print("Recording")
+    def on_new(self):
+        print("Saving")
+    def on_open(self):
+        print("Saving")
+    def on_save(self):
+        print("Saving")
     
     def __init__(self):
         self.dialog = kytten.Dialog(
         	kytten.Frame(
+             kytten.VerticalLayout([
         	    kytten.HorizontalLayout([
                      kytten.Button(text="Play",on_click=self.on_play),
                      kytten.Button(text="Stop",on_click=self.on_stop),
                      kytten.Button(text="Rec",on_click=self.on_rec),
-        	    ]),      
+        	    ]),
+        	    kytten.HorizontalLayout([
+                     kytten.Button(text="New",on_click=self.on_new),
+                     kytten.Button(text="Open",on_click=self.on_open),
+                     kytten.Button(text="Save",on_click=self.on_save),
+        	    ]),
+             ])
         	),
         	window=window, batch=batch, group=fg_group,
         	anchor=kytten.ANCHOR_CENTER,
@@ -74,7 +89,7 @@ class seq_track:
         pass
     def on_name_changed(self, new_name):
         pass
-    def __init__(self, a_track_text="track"):        
+    def __init__(self, a_track_num, a_track_text="track"):
         self.dialog = kytten.Dialog(
         	kytten.Frame(
                  kytten.VerticalLayout([
@@ -86,7 +101,7 @@ class seq_track:
                             ]),
                          kytten.HorizontalLayout([
                               kytten.Input(text=a_track_text),
-                              kytten.Dropdown(['Euphoria', 'Ray-V'],on_select=self.on_instrument_change),
+                              kytten.Dropdown(['None', 'Euphoria', 'Ray-V'],on_select=self.on_instrument_change),
                              ])
                      ])
         	),
@@ -94,6 +109,7 @@ class seq_track:
         	anchor=kytten.ANCHOR_CENTER,
         	theme=theme2, on_escape=on_escape, movable=True)
         
+        self.track_number = a_track_num
 
         self.midi_items = []
         self.midi_item_dialogs = []
@@ -115,13 +131,15 @@ class seq_track:
 
 class track_view:
     def __init__(self):
-        
+        self.session_mgr = lms_session('/home/bob/default.pss')
+        self.transport = transport()
+                
         self.tracks = [
-                kytten.HorizontalLayout([transport().dialog, kytten.Slider(max_value=16)])
+                kytten.HorizontalLayout([self.transport.dialog, kytten.Slider(max_value=16)])
             ]
 
         for i in range(0, 16):        
-            self.tracks.append(seq_track(a_track_text="track" + str(i)).hlayout)
+            self.tracks.append(seq_track(i, a_track_text="track" + str(i)).hlayout)            
         
         self.layout = kytten.VerticalLayout(self.tracks)
         self.scrollable = kytten.Scrollable(self.layout, width=1180, height=600, always_show_scrollbars=False)
@@ -136,8 +154,6 @@ class track_view:
     
 
 class midi_item:
-    def on_mute(self, value):
-        pass
     def __init__(self):
         self.dialog =  kytten.Dialog(
         	kytten.Frame(
