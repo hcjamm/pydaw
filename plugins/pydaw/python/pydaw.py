ef86234486939094cef64ca8267f43425873b0d9
Jeff Hubbard <jhubbard651@users.sf.net>
2012-09-12 17:54:27 -0400

    Move list editors and items into pydaw.py.  The project is still in the early, formative stages, and there's only 300-something lines of code in pydaw.py now...

--- plugins/pydaw/python/pydaw.py
+++ plugins/pydaw/python/pydaw.py
@@ -38,15 +38,188 @@ theme2 = kytten.Theme(theme, override={
 def on_escape(dialog):
     dialog.teardown()    
 
-for arg in argv:
-    print arg
+
+
+class seq_track:
+    def __init__(self):
+        self.regions = []
+        
+    def __str__(self):
+        return "" #TODO
+
+class list_region:
+    def __init__(self, a_string=None):
+        self.seq_items = []
+        
+        if not a_string is None:
+            pass            
     
-if(len(argv) >= 2):
-    this_dssi_gui = dssi_gui(argv[1])
+    def __str__(self):
+        return "" #TODO
+
+class list_item_seq:
+    def __init__(self, a_position, a_file_name, a_length=1, a_actual_length=1):
+        self.position = a_position
+        self.length = a_length
+        self.name = a_file_name
+        self.note_items = []
+        self.fq_file_path = os.getcwd() + a_file_name        
+        
+        file_handle = open(self.fq_file_path, 'r')        
+        line_arr = file_handle.readlines()        
+        file_handle.close()
+        
+        for line in line_arr:
+            item_arr = line.split('|')
+            self.note_items.append(list_item_content(item_arr[0], item_arr[1]))
+        
+    """
+    Rename the underlying file associated with the sequencer item
+    """
+    def rename(self, a_name):
+        pass
     
-user_home_folder = expanduser("~")
-print("user_home_folder = " + user_home_folder)
-session_mgr = lms_session(user_home_folder + '/default.pydaw')    
+    def __str__(self):
+        f_result = ""
+        for content_item in self.note_items:
+            f_result += content_item
+        
+        return f_result
+        
+class list_item_content:
+    """
+    Tentatively, for a_type:  0 == MIDI Note, 1 == MIDI CC, 2 == Pitchbend, 3 == Audio
+    """
+    def __init__(self, a_type, a_start, a_length):
+        self.type = a_type        
+        self.start = a_start
+        self.length = a_length
+            
+    def __str__(self):
+        return str(self.type) + '|' + str(self.start) + '|' + str(self.length) + "\n"
+    
+    def on_delete(self):
+        pass
+    
+    def on_mute(self):
+        pass
+    
+    """
+    Return a widget suitable for displaying in the list editor
+    """
+    def get_widget(self):
+        if self.type == 0:
+            return kytten.Dialog(
+            	kytten.Frame(
+                     kytten.HorizontalLayout([
+                         kytten.Input(text='0', length=4), #Start
+                         kytten.Input(text='1', length=4), #Length
+                         kytten.Dropdown(['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']),                            
+                         kytten.Dropdown(['-2', '-1', '0', '1', '2', '3', '4', '5', '6', '7', '8']),
+                         kytten.Checkbox(text="Mute",on_click=self.on_mute),
+                        ]),
+            	),
+            	window=window, batch=batch, group=fg_group,
+            	anchor=kytten.ANCHOR_CENTER,
+            	theme=theme2, movable=True)
+        elif self.type == 1:
+            return kytten.Dialog(
+            	kytten.Frame(
+                     kytten.HorizontalLayout([
+                         kytten.Input(text='0', length=4), #Start
+                         kytten.Input(text='1', length=4), #Length
+                         kytten.Input(text='64', length=4), #CC
+                         kytten.Slider(max_value=127.0), #Value
+                         kytten.Checkbox(text="Mute",on_click=self.on_mute),
+                        ]),
+            	),
+            	window=window, batch=batch, group=fg_group,
+            	anchor=kytten.ANCHOR_CENTER,
+            	theme=theme2, movable=True)
+        elif self.type == 2:
+            pass
+        elif self.type == 3:
+            return kytten.Dialog(
+            	kytten.Frame(
+                     kytten.HorizontalLayout([
+                         kytten.Input(text='0', length=4), #Start
+                         kytten.Input(text='1', length=4), #Length
+                         kytten.Dropdown(['test1.wav', 'test2.wav', 'test3.wav', 'test4.wav']),                            
+                         kytten.Dropdown(['One Shot', 'Loop']),
+                         kytten.Checkbox(text="Mute",on_click=self.on_mute),
+                        ]),
+             
+            	),
+            	window=window, batch=batch, group=fg_group,
+            	anchor=kytten.ANCHOR_CENTER,
+            	theme=theme2, movable=True)
+
+def note_number_to_string(a_note_number):
+    pass
+
+def string_to_note_number(a_note_string):
+    pass
+
+class region_list_editor:
+    #If a_new_file_name is set, a_file_name will be copied into a new file name with the name a_new_file_name
+    def __init__(self):
+        self.events = []        
+        self.layout = kytten.VerticalLayout(self.events)
+        self.scrollable = kytten.Scrollable(self.layout, width=1180, height=600, always_show_scrollbars=False)
+        
+        self.dialog = kytten.Dialog(
+            	kytten.Frame(
+                     kytten.VerticalLayout([
+                         self.scrollable
+                         ])
+        	),
+        	window=window, batch=batch, group=fg_group,
+        	anchor=kytten.ANCHOR_CENTER,
+        	theme=theme2)
+         
+    """
+    This should be called whenever the items have been changed, or when 
+    switching items
+    
+    a_items should be an array of 
+    """
+    def update_items(self, a_items=[]):
+         self.layout.delete()
+         
+         for item in a_items:
+             item_dialog = item.get_widget()
+             self.layout.add(item_dialog)
+    
+
+class item_list_editor:
+    #If a_new_file_name is set, a_file_name will be copied into a new file name with the name a_new_file_name
+    def __init__(self):
+        self.events = []        
+        self.layout = kytten.VerticalLayout(self.events)
+        self.scrollable = kytten.Scrollable(self.layout, width=1180, height=600, always_show_scrollbars=False)
+        
+        self.dialog = kytten.Dialog(
+            	kytten.Frame(
+                     kytten.VerticalLayout([
+                         self.scrollable
+                         ])
+        	),
+        	window=window, batch=batch, group=fg_group,
+        	anchor=kytten.ANCHOR_CENTER,
+        	theme=theme2)
+         
+    """
+    This should be called whenever the items have been changed, or when 
+    switching items
+    
+    a_items should be an array of 
+    """
+    def update_items(self, a_items=[]):
+         self.layout.delete()
+         
+         for item in a_items:
+             item_dialog = item.get_widget()
+             self.layout.content.append(item_dialog)
     
 class seq_track:
     def on_vol_change(self, value):
@@ -136,10 +309,17 @@ class track_view:
         
         self.layout = kytten.VerticalLayout(self.tracks)
         self.scrollable = kytten.Scrollable(self.layout, width=1180, height=600, always_show_scrollbars=False)
+
+        self.region_editor = region_list_editor()
+        self.item_editor = item_list_editor()        
         
         self.dialog = kytten.Dialog(
             	kytten.Frame(
-            	    self.scrollable
+                     kytten.HorizontalLayout([
+                        self.scrollable,
+                        self.region_editor.dialog,
+                        self.item_editor.dialog
+                     ])            	    
         	),
         	window=window, batch=batch, group=fg_group,
         	anchor=kytten.ANCHOR_CENTER,
@@ -185,7 +365,26 @@ if __name__ == '__main__':
 	window.dispatch_event('on_update', dt)
     pyglet.clock.schedule(update)
     
+    for arg in argv:
+        print arg
+    
+    if(len(argv) >= 2):
+        this_dssi_gui = dssi_gui(argv[1])
+        
+    user_home_folder = expanduser("~")
+    print("user_home_folder = " + user_home_folder)
+    session_mgr = lms_session(user_home_folder + '/default.pydaw')    
+
+    
     track_view_test = track_view()
+    
+    item_list = [        
+        list_item_content(0, 1, 1),
+        list_item_content(1, 1, 1),
+        list_item_content(3, 1, 1)    
+    ]
+    
+    track_view_test.item_editor.update_items(a_items=item_list)
                          
     # Change this flag to run with profiling and dump top 20 cumulative times
     if True:
