94491563345f4d9b9974decf6292b4aaa91ff652
Jeff Hubbard <jhubbard651@users.sf.net>
2012-09-18 18:18:07 -0400

    More incremental work, documentation and planning for PyDAW

--- plugins/pydaw/src/synth.c
+++ plugins/pydaw/src/synth.c
@@ -27,10 +27,10 @@ GNU General Public License for more details.
 
 #include "dssi.h"
 #include "ladspa.h"
+#include "sequencer.h"
 
 #include "libmodsynth.h"
 #include "../../libmodsynth/lib/amp.h"
-#include "../../libmodsynth/modules/filter/svf.h"
 
 #include "synth.h"
 #include "meta.h"
@@ -183,9 +183,6 @@ char *pydaw_configure(LADSPA_Handle instance, const char *key, const char *value
     return strdup("error: unrecognized configure key");
 }
 
-
-/*This returns MIDI CCs for the different knobs
- TODO:  Try it with non-hex numbers*/
 int getControllerLMS(LADSPA_Handle instance, unsigned long port)
 {    
     //t_pydaw_engine *plugin_data = (t_pydaw_engine *) instance;
@@ -193,167 +190,6 @@ int getControllerLMS(LADSPA_Handle instance, unsigned long port)
     return 0;
      
 }
-
-/*ALSA Sequencer Client Stuff*/
-
-#define MAX_MIDI_PORTS   4
-
-int open_seq(snd_seq_t **seq_handle, int in_ports[], int out_ports[], int num_in, int num_out);
-void midi_route(snd_seq_t *seq_handle, int out_ports[], int split_point);
-
-/* Open ALSA sequencer wit num_in writeable ports and num_out readable ports. */
-/* The sequencer handle and the port IDs are returned.                        */  
-int open_seq(snd_seq_t **seq_handle, int in_ports[], int out_ports[], int num_in, int num_out) {
-
-  int l1;
-  char portname[64];
-
-  if (snd_seq_open(seq_handle, "default", SND_SEQ_OPEN_DUPLEX, 0) < 0) {
-    fprintf(stderr, "Error opening ALSA sequencer.\n");
-    return(-1);
-  }
-  snd_seq_set_client_name(*seq_handle, "MIDI Router");
-  for (l1 = 0; l1 < num_in; l1++) {
-    sprintf(portname, "MIDI Router IN %d", l1);
-    if ((in_ports[l1] = snd_seq_create_simple_port(*seq_handle, portname,
-              SND_SEQ_PORT_CAP_WRITE|SND_SEQ_PORT_CAP_SUBS_WRITE,
-              SND_SEQ_PORT_TYPE_APPLICATION)) < 0) {
-      fprintf(stderr, "Error creating sequencer port.\n");
-      return(-1);
-    }
-  }
-  for (l1 = 0; l1 < num_out; l1++) {
-    sprintf(portname, "MIDI Router OUT %d", l1);
-    if ((out_ports[l1] = snd_seq_create_simple_port(*seq_handle, portname,
-              SND_SEQ_PORT_CAP_READ|SND_SEQ_PORT_CAP_SUBS_READ,
-              SND_SEQ_PORT_TYPE_APPLICATION)) < 0) {
-      fprintf(stderr, "Error creating sequencer port.\n");
-      return(-1);
-    }
-  }
-  return(0);
-}
-
-/* Read events from writeable port and route them to readable port 0  */
-/* if NOTEON / OFF event with note < split_point. NOTEON / OFF events */
-/* with note >= split_point are routed to readable port 1. All other  */
-/* events are routed to both readable ports.                          */
-void midi_route(snd_seq_t *seq_handle, int out_ports[], int split_point) {
-
-  snd_seq_event_t *ev;
-
-  do {
-    snd_seq_event_input(seq_handle, &ev);
-    snd_seq_ev_set_subs(ev);  
-    snd_seq_ev_set_direct(ev);
-    
-    if ((ev->type == SND_SEQ_EVENT_NOTEON)||(ev->type == SND_SEQ_EVENT_NOTEOFF)) 
-    {
-      if (ev->data.note.note < split_point) {
-        snd_seq_ev_set_source(ev, out_ports[0]);
-      } else {
-        snd_seq_ev_set_source(ev, out_ports[1]);
-      }
-      snd_seq_event_output_direct(seq_handle, ev);
-    } 
-    else 
-    {
-      snd_seq_ev_set_source(ev, out_ports[0]);
-      snd_seq_event_output_direct(seq_handle, ev);
-      snd_seq_ev_set_source(ev, out_ports[1]);
-      snd_seq_event_output_direct(seq_handle, ev);
-    }
-    snd_seq_free_event(ev);
-  } while (snd_seq_event_input_pending(seq_handle, 0) > 0);
-}
-
-int init_alsa_seq(int argc, char *argv[]) {
-
-  snd_seq_t *seq_handle;
-  int out_ports[MAX_MIDI_PORTS];
-  int npfd, split_point;
-  struct pollfd *pfd;
-  
-  if (argc < 2) {
-    fprintf(stderr, "\nmidiroute <split_point>\n\n");
-    exit(1);
-  } else {
-    split_point = atoi(argv[1]);  
-  }  
-  if (open_seq(&seq_handle, 0, out_ports, 1, 2) < 0) {
-    fprintf(stderr, "ALSA Error.\n");
-    exit(1);
-  }
-  npfd = snd_seq_poll_descriptors_count(seq_handle, POLLIN);
-  pfd = (struct pollfd *)alloca(npfd * sizeof(struct pollfd));
-  snd_seq_poll_descriptors(seq_handle, pfd, npfd, POLLIN);
-  while (1) {
-    if (poll(pfd, npfd, 100000) > 0) {
-      midi_route(seq_handle, out_ports, split_point);
-    }  
-  }
-}
-
-
-/*End ALSA Sequencer Client Stuff*/
-
-/*Stuff from midi-listen.c*/
-
-
-static snd_seq_t *seq_handle;
-static int in_port;
-
-#define CHK(stmt, msg) if((stmt) < 0) {puts("ERROR: "#msg); exit(1);}
-void midi_open(void)
-{
-    CHK(snd_seq_open(&seq_handle, "default", SND_SEQ_OPEN_INPUT, 0),
-            "Could not open sequencer");
-
-    CHK(snd_seq_set_client_name(seq_handle, "Midi Listener"),
-            "Could not set client name");
-    CHK(in_port = snd_seq_create_simple_port(seq_handle, "listen:in",
-                SND_SEQ_PORT_CAP_WRITE|SND_SEQ_PORT_CAP_SUBS_WRITE,
-                SND_SEQ_PORT_TYPE_APPLICATION),
-            "Could not open port");
-}
-
-snd_seq_event_t *midi_read(void)
-{
-    snd_seq_event_t *ev = NULL;
-    snd_seq_event_input(seq_handle, &ev);
-    return ev;
-}
-
-void midi_process(const snd_seq_event_t *ev)
-{
-    snd_seq_addr_t * test;
-    snd_seq_port_subscribe_t * test2;
-    
-    //snd_seq_subscribe_port(seq_handle, )
-    if((ev->type == SND_SEQ_EVENT_NOTEON)
-            ||(ev->type == SND_SEQ_EVENT_NOTEOFF)) {
-        const char *type = (ev->type==SND_SEQ_EVENT_NOTEON) ? "on " : "off";
-        printf("[%d] Note %s: %2x vel(%2x)\n", ev->time.tick, type,
-                                               ev->data.note.note,
-                                               ev->data.note.velocity);
-    }
-    else if(ev->type == SND_SEQ_EVENT_CONTROLLER)
-        printf("[%d] Control:  %2x val(%2x)\n", ev->time.tick,
-                                                ev->data.control.param,
-                                                ev->data.control.value);
-    else
-        printf("[%d] Unknown:  Unhandled Event Received\n", ev->time.tick);
-}
-
-int midi_listen_main()
-{
-    midi_open();
-    while(1)
-        midi_process(midi_read());
-    return -1;
-}
-
-/*End stuff from midi-listen.c*/
 
 #ifdef __GNUC__
 __attribute__((constructor)) void init()
