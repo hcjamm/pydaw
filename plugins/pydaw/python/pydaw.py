15eb5672fc36ae5fdaa55d01c76f92cd7c4e291c
Jeff Hubbard <jhubbard651@users.sf.net>
2012-09-20 22:31:49 -0400

    Saving of the various file types is now mostly working(albeit still with plenty of bugs)

--- plugins/pydaw/python/pydaw.py
+++ plugins/pydaw/python/pydaw.py
@@ -19,13 +19,16 @@ import sys, os
 from PyQt4 import QtGui, QtCore
 from sys import argv
 from os.path import expanduser
-from os import listdir
-from shutil import copyfile
-from lms_session import lms_session
 from dssi_gui import dssi_gui
 from connect import *
+from pydaw_project import pydaw_project
 
 class song_editor:
+    def open_song(self):
+        self.table_widget.clear()
+        f_song_string_arr = this_pydaw_project.get_song_string().split("|")
+        #TODO
+    
     def cell_clicked(self, x, y):
         f_cell = self.table_widget.item(x, y)
         if f_cell is None:
@@ -38,6 +41,7 @@ class song_editor:
                     
                 self.table_widget.setItem(x, y, f_new_cell)
                 this_region_editor.open_region(f_new_lineedit.text())
+                this_pydaw_project.save_song(self.__str__())
                 f_window.close()
                 
             def song_cancel_handler():            
@@ -81,9 +85,19 @@ class song_editor:
         self.table_widget.setRowCount(1)
         self.table_widget.cellClicked.connect(self.cell_clicked)
         self.main_vlayout.addWidget(self.table_widget)
+        
+    def __str__(self):
+        f_result = ""
+        for f_i in range(0, self.table_widget.columnCount()):
+            f_item = self.table_widget.item(f_i, 0)
+            if not f_item is None:
+                f_result += f_item.text()
+            f_result += "|"
+        return f_result
 
 class region_list_editor:
     def open_region(self, a_file_name):
+        self.table_widget.clear()
         self.region_name_lineedit.setText(a_file_name)
         f_region_string = this_pydaw_project.get_region_string(a_file_name)
         f_region_arr = f_region_string.split("\n")
@@ -103,16 +117,17 @@ class region_list_editor:
                 this_item_editor.open_item(f_item_name)
             else:
                 self.show_cell_dialog(x, y)
-    #Maybe use __str__ for this one?                
-    def get_region_string(self):
+             
+    def __str__(self):
         f_result = ""
-        for i in range(0, self.table_widget.rowCount()):
-            for i in range(0, self.table_widget.columnCount()):
+        for f_i in range(0, self.table_widget.rowCount()):
+            for f_i2 in range(0, self.table_widget.columnCount()):
                 f_item = self.table_widget.item(f_i, f_i2)
                 if not f_item is None:
                     f_result += f_item.text()
                 f_result += "|"
-            f_result += "\n"                    
+            f_result += "\n"
+        return f_result                
                 
     def show_cell_dialog(self, x, y):
         def note_ok_handler():            
@@ -124,7 +139,7 @@ class region_list_editor:
             this_item_editor.open_item(f_cell_text)
             f_new_cell = QtGui.QTableWidgetItem(f_cell_text)
             self.table_widget.setItem(x, y, f_new_cell)
-            #this_pydaw_project.save_region()
+            this_pydaw_project.save_region(str(self.region_name_lineedit.text()), self.__str__())
             f_window.close()
                 
         def note_cancel_handler():            
@@ -184,8 +199,7 @@ class region_list_editor:
          
 class item_list_editor:
     #If a_new_file_name is set, a_file_name will be copied into a new file name with the name a_new_file_name
-    def __init__(self):
-        self.events = []                
+    def __init__(self):           
         self.group_box = QtGui.QGroupBox()
         self.main_vlayout = QtGui.QVBoxLayout()
         self.group_box.setLayout(self.main_vlayout)
@@ -197,9 +211,27 @@ class item_list_editor:
         self.group_box.setMaximumWidth(270)
         self.main_vlayout.addWidget(self.table_widget)
         
+    def __str__(self):
+        f_result = ""
+        for f_i in range(0, self.table_widget.rowCount()):
+            for f_i2 in range(0, self.table_widget.columnCount()):
+                f_item = self.table_widget.item(f_i, f_i2)
+                if not f_item is None:
+                    f_result += f_item.text()
+                f_result += "\t"
+            f_result += "\n"
+        return f_result
+        
     def open_item(self, a_item_name):
-        f_this_item_string = this_pydaw_project.get_item_string(a_item_name)
-        #TODO
+        self.table_widget.clear()
+        self.item_name = a_item_name
+        f_items = this_pydaw_project.get_item_string(a_item_name).split("\n")
+        for f_i in range(0, len(f_items)):
+            f_item_arr = f_items[f_i].split("\t")
+            for f_i2 in range(0, len(f_item_arr)):
+                if f_item_arr[f_i2] != "":
+                    f_cell = QtGui.QTableWidgetItem(f_item_arr[f_i2])
+                    self.table_widget.setItem(f_i, f_i2, f_cell)
         
     def show_event_dialog(self, x, y):
         if y == 0:
@@ -224,6 +256,7 @@ class item_list_editor:
                     str(f_velocity.value()) + "|" + str(f_note.currentText()) + str(f_octave.value())
                     )
                 self.table_widget.setItem(x, y, f_new_cell)
+                this_pydaw_project.save_item(self.item_name, self.__str__())
                 f_window.close()
                 
             def note_cancel_handler():            
@@ -284,6 +317,7 @@ class item_list_editor:
                     str(f_cc.value()) + "|" + str(f_cc_value.value())
                     )
                 self.table_widget.setItem(x, y, f_new_cell)
+                this_pydaw_project.save_item(self.item_name, self.__str__())
                 f_window.close()
                 
             def cc_cancel_handler():            
@@ -443,6 +477,12 @@ class track_editor:
         self.tracks_tablewidget.resizeColumnsToContents()
         self.tracks_tablewidget.resizeRowsToContents()        
         self.tracks_tablewidget.setMaximumWidth(300)
+        
+    def __str__(self):
+        f_result = ""
+        for track in self.tracks:
+            pass #TODO
+        return f_result
 
 class pydaw_main_window(QtGui.QMainWindow):
     def on_new(self):
@@ -521,108 +561,7 @@ class pydaw_main_window(QtGui.QMainWindow):
         cp = QtGui.QDesktopWidget().availableGeometry().center()
         qr.moveCenter(cp)
         self.move(qr.topLeft())
-
-#Handles the various file IO for the project.  TODO:  Move to it's own file
-class pydaw_project:
-    def save_project(self):
-        self.session_mgr.save_session_file()
-    def save_project_as(self):
-        pass
-    def open_project(self):
-        pass
-    def new_project(self, a_project_folder=None, a_project_file=None):
-        #TODO:  Show a QFileDialog when either is None
-        self.project_folder = a_project_folder
-        self.project_file = a_project_file
-        self.instrument_folder = self.project_folder + "instruments"
-        self.regions_folder = self.project_folder + "regions"
-        self.items_folder = self.project_folder + "items"
-        
-        project_folders = [
-            self.project_folder,
-            self.instrument_folder,
-            self.regions_folder,
-            self.items_folder
-            ]
-                    
-        for project_dir in project_folders:
-            if not os.path.isdir(project_dir):
-                os.mkdir(project_dir)
-        
-        self.session_mgr = lms_session(self.instrument_folder + '/' + a_project_file + '.pyses')
-        this_main_window.setWindowTitle('PyDAW - ' + self.project_file)
-        
-    def get_region_string(self, a_region_name):
-        f_file = open(self.regions_folder + "/" + a_region_name + ".pyreg", "r")        
-        f_result = f_file.read()
-        f_file.close()
-        return f_result
-                
-    def get_item_string(self, a_item_name):
-        f_file = open(self.items_folder + "/" + a_item_name + ".pyitem", "r")        
-        f_result = f_file.read()
-        f_file.close()
-        return f_result
-        
-    def create_empty_region(self, a_region_name):
-        #TODO:  Check for uniqueness, from a pydaw_project.check_for_uniqueness method...
-        open(self.regions_folder + "/" + a_region_name + ".pyreg", 'w').close()        
-    
-    def create_empty_item(self, a_item_name):
-        #TODO:  Check for uniqueness, from a pydaw_project.check_for_uniqueness method...
-        open(self.items_folder + "/" + a_item_name + ".pyitem", 'w').close()        
-        
-    def copy_region(self, a_old_region, a_new_region):
-        copyfile(self.regions_folder + "/" + a_old_region + ".pyreg", self.regions_folder + "/" + a_new_region + ".pyreg")
-    
-    def copy_item(self, a_old_item, a_new_item):
-        copyfile(self.items_folder + "/" + a_old_item + ".pyitem", self.items_folder + "/" + a_new_item + ".pyitem")
-
-    def save_item(self, a_name, a_string):
-        f_file = open(self.items_folder + "/" + a_name + ".pyitem", 'w')
-        f_file.write(a_string)
-        f_file.close()
-    
-    def save_region(self, a_name, a_string):
-        f_file = open(self.regions_folder + "/" + a_name + ".pyreg", 'w')
-        f_file.write(a_string)
-        f_file.close()
-
-    def get_next_default_item_name(self):
-        for i in range(self.last_item_number, 10000):
-            f_result = self.items_folder + "/item-" + str(i) + ".pyitem"
-            if not os.path.isfile(f_result):
-                self.last_item_number = i
-                return "item-" + str(i)
-
-    def get_next_default_region_name(self):
-        for i in range(self.last_region_number, 10000):
-            f_result = self.regions_folder + "/region-" + str(i) + ".pyreg"
-            if not os.path.isfile(f_result):
-                self.last_item_number = i
-                return "region-" + str(i)
-            
-    def get_item_list(self):
-        f_result = []
-        for files in os.listdir(self.items_folder):
-            if files.endswith(".pyitem"):
-                f_result.append(files.split(".pyitem")[0])
-        f_result.sort()
-        return f_result
-    
-    def get_region_list(self):
-        f_result = []
-        for files in os.listdir(self.regions_folder):
-            if files.endswith(".pyreg"):
-                f_result.append(files.split(".pyreg")[0])
-        f_result.sort()
-        return f_result
-        
-    def __init__(self, a_project_folder=None, a_project_file=None):
-        self.last_item_number = 0
-        self.last_region_number = 0
-        self.new_project(a_project_folder, a_project_file)
-    
+   
 def about_to_quit():
     this_pydaw_project.session_mgr.quit_hander()
     this_dssi_gui.stop_server()
