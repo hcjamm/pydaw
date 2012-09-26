3d4f9ae2023b93e6bda00f1bacda7ebbc7d84e93
Jeff Hubbard <jhubbard651@users.sf.net>
2012-09-26 08:18:31 -0400

    Early rudimentary file reading, albeit needing some error checking and general polishing.

--- plugins/pydaw/src/pydaw_files.h
+++ plugins/pydaw/src/pydaw_files.h
@@ -28,11 +28,7 @@ char * get_string_from_file(char * a_file, int a_size)
 	
     f_file = fopen(a_file, "r");
 	
-    while (!feof(f_file))
-    {
-        fgets(f_buffer,a_size,f_file);
-        //printf("%s",f_buffer);
-    }
+    fread(f_buffer, sizeof(char), sizeof(char) * a_size, f_file);    
 	
     fclose(f_file);
     
@@ -47,9 +43,8 @@ typedef struct st_1d_char_array
 
 typedef struct st_2d_char_array
 {
-    char *** array;
-    int x_count;
-    int y_count;
+    char * array;  //allocate a continuous chunk of memory, otherwise we would need to free possibly thousands of pointers
+    int current_index;
 }t_2d_char_array;
 
 void g_free_1d_char_array(t_1d_char_array * a_array)
@@ -67,23 +62,7 @@ void g_free_1d_char_array(t_1d_char_array * a_array)
 }
 
 void g_free_2d_char_array(t_2d_char_array * a_array)
-{
-    int f_i, f_i2 = 0;
-    
-    while(f_i < (a_array->y_count))
-    {
-        f_i2 = 0;
-        
-        while(f_i2 < (a_array->x_count))
-        {
-            free(a_array->array[f_i][f_i2]);
-            f_i2++;
-        }
-        
-        free(a_array->array[f_i]);
-        f_i++;
-    }
-    
+{    
     free(a_array->array);    
     free(a_array);
 }
@@ -139,98 +118,46 @@ t_1d_char_array * c_split_str(char * a_input, char a_delim, int a_column_count, 
 /* Return a 2d array of strings from a file delimited by "|" and "\n" individual fields are 
  * limited to being the size of LMS_TINY_STRING */
 t_2d_char_array * g_get_2d_array_from_file(char * a_file, int a_size)
-{
-    char * f_file_chars = get_string_from_file(a_file, a_size);
-    
+{    
     t_2d_char_array * f_result = (t_2d_char_array*)malloc(sizeof(t_2d_char_array));
     
-    f_result->x_count = 0;
-    int f_i, f_i2 = 0;
+    f_result->array = get_string_from_file(a_file, a_size);
+    f_result->current_index = 0;
     
-    while(f_i < a_size)  //Count the width of the rows.  It's assumed that every row will have the same column count, if not, the file is malformed
-    {
-        if(f_file_chars[f_i] == '|')
-        {
-            f_result->x_count = (f_result->x_count) + 1;
-        }
-        else if((f_file_chars[f_i] == '\n') || (f_file_chars[f_i] == '\0'))
-        {
-            break;
-        }
+    return f_result;
+    
+}
+
+/* Return the next string from the array*/
+char * c_iterate_2d_char_array(t_2d_char_array* a_array)
+{
+    char * f_result = (char*)malloc(sizeof(char) * LMS_TINY_STRING);
+    int f_i = 0;    
         
-        f_i++;
-    }
-    
-    f_result->y_count = 0;
-    f_i = 0;
-    while(f_i < a_size)  //Count the width of the rows.  It's assumed that every row will have the same column count, if not, the file is malformed
-    {
-        if(f_file_chars[f_i] == '\n')
-        {
-            f_result->y_count = (f_result->y_count) + 1;
-        }
-        else if(f_file_chars[f_i] == '\0')
-        {
-            break;
-        }
-        
-        f_i++;
-    }
-    
-    f_result->array = (char***)malloc(sizeof(char**) * (f_result->y_count));
-    
-    f_i = 0;
-    f_i2 = 0;
-    
-    while(f_i < (f_result->y_count))
-    {
-        f_result->array[f_i] = (char**)malloc(sizeof(char*) * (f_result->x_count));
-        
-        while(f_i2 < (f_result->x_count))
-        {
-            f_result->array[f_i][f_i2] = (char*)malloc(sizeof(char) * LMS_TINY_STRING);
-            f_i2++;
-        }
-        
-        f_i++;
-    }    
-    
-    f_i = 0;
-    
-    int f_current_column = 0;
-    int f_current_row = 0;
-    int f_current_string_index = 0;
-    
     while(1)
     {
-        if(f_file_chars[f_i] == '|')
-        {
-            f_current_column++;
-            
-            f_current_string_index = 0;
-            
+        if((a_array->array[(a_array->current_index)] == '|') || (a_array->array[(a_array->current_index)] == '\n'))        
+        {   
+            f_result[f_i] = '\0';
+            a_array->current_index = (a_array->current_index) + 1;
+            break;
         }
-        else if(f_file_chars[f_i] == '\n')
+        else if(a_array->array[(a_array->current_index)] == '\0')
         {
-            f_current_string_index = 0;
-            f_current_column = 0;
-            f_current_row++;
-        }
-        else if(f_file_chars[f_i] == '\0')
-        {
+            f_result[f_i] = '\0';
+            a_array->current_index = (a_array->current_index) + 1;
             break;
         }
         else
         {
-            f_result->array[f_current_row][f_current_column][f_current_string_index] = f_file_chars[f_i];
-            f_current_string_index++;
+            f_result[f_i] = a_array->array[(a_array->current_index)];            
         }
         
+        a_array->current_index = (a_array->current_index) + 1;
         f_i++;
     }
     
     return f_result;
-    
 }
 
 #ifdef	__cplusplus
