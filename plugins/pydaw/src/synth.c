521296a397635791b2f7dc10ffe295ed3b4c3903
Jeff Hubbard <jhubbard651@users.sf.net>
2012-10-09 20:59:05 -0400

    Hack-ish fix to the problem of notes not firing from position 0.0 beats after the first bar.  The fix does slightly screw up the timing, but for now it fixes a much bigger problem until I can properly implement the main loop

--- plugins/pydaw/src/synth.c
+++ plugins/pydaw/src/synth.c
@@ -211,7 +211,8 @@ static void run_lms_pydaw(LADSPA_Handle instance, unsigned long sample_count,
         if((pydaw_data->playback_cursor) >= 1.0f)
         {
             //Calculate the remainder of this bar that occurs within the sample period
-            pydaw_data->playback_cursor = (pydaw_data->playback_cursor) - 1.0f;
+            //pydaw_data->playback_cursor = (pydaw_data->playback_cursor) - 1.0f;
+            pydaw_data->playback_cursor = 0.0f;
             f_next_period = (pydaw_data->playback_cursor) + ((pydaw_data->playback_inc) * ((double)(sample_count)));
             
             int f_i2 = 0;
