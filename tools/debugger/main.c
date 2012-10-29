95f93f6361ae8f52974cc2abcaf4ae6148af3c5c
PyDAW Team <pydawteam@users.sf.net>
2012-10-29 18:22:45 -0400

    Revamp the debugger project...

--- tools/debugger/main.c
+++ tools/debugger/main.c
@@ -15,85 +15,14 @@
 #include "../../plugins/pydaw/src/pydaw.h"
 #include "../../plugins/pydaw/src/pydaw_files.h"
 #include "../../plugins/pydaw/src/synth.c"
-
+#include <dssi.h>
 /* int main(
  * int argc, //ignored
  * char** argv) //ignored
  * 
  */
-int main(int argc, char** argv) {
-    
-    
-    /*t_osc_simple_unison * f_osc = g_osc_get_osc_simple_unison(44100.0f);
-    v_osc_set_simple_osc_unison_type(f_osc, 0);
-    
-    v_osc_set_unison_pitch(f_osc, 0.1f, 66.0f);
-    
-    v_lim_set(f_limiter, -6.0f, -9.0f, 200.0f);
-    
-    float * test_square; // = (float*)malloc(sizeof(float) * 50);
-    posix_memalign((void**)&test_square, 16, (sizeof(float) * 50));
-        
-    int i;
-    
-    for(i = 0; i < 10; i++)
-    {
-        test_square[i] = 0.0f;
-    }
-    
-    test_square[10] = 1.0f;
-    test_square[11] = 1.0f;
-    test_square[12] = 1.0f;
-    test_square[13] = 1.0f;
-    test_square[14] = 1.0f;
-    test_square[15] = 1.0f;
-    test_square[16] = 1.0f;
-    test_square[17] = 1.0f;
-    test_square[18] = 1.0f;
-    test_square[19] = -1.0f;
-    test_square[20] = -1.0f;
-    test_square[21] = -1.0f;
-    test_square[22] = -1.0f;
-    test_square[23] = -1.0f;
-    test_square[24] = -1.0f;
-    test_square[25] = -1.0f;
-    test_square[26] = -1.0f;
-    test_square[27] = -1.0f;
-    
-    for(i = 28; i < 50; i++)
-    {
-        test_square[i] = 0.0f;
-    }
-    
-    int f_i;
-    
-    for(f_i = 0; f_i < 500000; f_i++)
-    {
-        float f_sample = f_osc_run_unison_osc(f_osc);
-        
-        v_lim_run(f_limiter, f_sample, f_sample);
-    }
-    
-    
-    for(f_i = 0; f_i < 500000; f_i++)
-    {
-        float f_sample = f_osc_run_unison_osc(f_osc);
-        
-        v_lim_run(f_limiter, f_sample, f_sample);
-    }
-    
-    
-    for(f_i = 0; f_i < 500000; f_i++)
-    {
-        float f_sample = f_osc_run_unison_osc(f_osc);
-        
-        v_lim_run(f_limiter, f_sample, f_sample);
-    }
-    */
-
-    //midi_open();
-    //snd_seq_close(seq);
-    
+int main(int argc, char** argv) 
+{   
     /*
     t_pydaw_data * f_data = g_pydaw_data_get(44100);
     v_set_tempo(f_data, 140.0f);
