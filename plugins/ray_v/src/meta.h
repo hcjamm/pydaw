744271add62173846d07784a4a1d471c795302d9
PyDAW Team <pydawteam@users.sf.net>
2012-10-29 18:32:49 -0400

    Part2 of the previous commit...

--- plugins/ray_v/src/meta.h
+++ plugins/ray_v/src/meta.h
@@ -8,10 +8,10 @@ extern "C" {
 #endif
 
 /*These should be customized for each developer and plugin*/
-#define LMS_PLUGIN_NAME "LMS_RAYV"
-#define LMS_PLUGIN_LONG_NAME "Ray-V";
-#define LMS_PLUGIN_DEV "Jeff Hubbard <jhubbard651@users.sf.net>";
-#define LMS_PLUGIN_UUID 1337721
+#define RAYV_PLUGIN_NAME "LMS_RAYV"
+#define RAYV_PLUGIN_LONG_NAME "Ray-V";
+#define RAYV_PLUGIN_DEV "Jeff Hubbard <jhubbard651@users.sf.net>";
+#define RAYV_PLUGIN_UUID 1337721
 
 #ifdef	__cplusplus
 }
