d5ae83310879381e7abf561a11da8a40dc0c6040
PyDAW Team <pydawteam@users.sf.net>
2012-11-12 07:24:52 -0500

    Better handling of the plugin GUIs(including showing them when the UI button is pressed and the UI already open but below the main window...)

--- plugins/pydaw/src/pydaw_plugin.h
+++ plugins/pydaw/src/pydaw_plugin.h
@@ -138,9 +138,7 @@ typedef struct st_pydaw_plugin
     char euphoria_load[16384];
     int euphoria_load_set;
     //char euphoria_last_dir[512];
-    //int euphoria_last_dir_set;
-    
-    int ui_visible;
+    //int euphoria_last_dir_set;    
 }t_pydaw_plugin;
 
 #ifdef PYDAW_PLUGIN_MEMCHECK    
@@ -229,8 +227,6 @@ t_pydaw_plugin * g_pydaw_plugin_get(int a_sample_rate, int a_index)
             f_result->lib_handle = dlopen("/usr/lib/dssi/ray_v.so", RTLD_NOW | RTLD_LOCAL);                  
             break;
     }
-    
-    f_result->ui_visible = 0;
     
     //f_result->euphoria_last_dir_set = 0;
     f_result->euphoria_load_set = 0;
