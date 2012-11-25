d7e0e0524d81695ba1d06dd94f0468d616f6aa38
Jeff Hubbard <jhubbard651@users.sf.net>
2012-11-25 11:56:45 -0500

    Add FFADO dependencies to PyDAW, so as not to send Firewire users (like myself) through the process of having to learn CLI-foo to install them whenever it causes PyDAW not to work the first time they try it...

--- plugins/build-all.pl
+++ plugins/build-all.pl
@@ -13,7 +13,7 @@ perl build-all.pl
 
 ";
 
-$debian_deps = "liblo-dev, dssi-dev, ladspa-sdk, libasound2-dev, libqt4-dev, libsndfile1-dev, libsm-dev, qjackctl, alsa-utils, python-liblo, python-qt4, git, libsdl1.2-dev";
+$debian_deps = "liblo-dev, dssi-dev, ladspa-sdk, libasound2-dev, libqt4-dev, libsndfile1-dev, libsm-dev, qjackctl, alsa-utils, python-liblo, python-qt4, git, libsdl1.2-dev, ffado-mixer-qt4, ffado-tools, ffado-dbus-server";
 
 $prompt = 1;
 
