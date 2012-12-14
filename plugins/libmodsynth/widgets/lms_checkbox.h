86d0a6f5642e97b831fe01fdbce03281f439b9fd
Jeff Hubbard <jhubbard651@users.sf.net>
2012-12-13 20:08:38 -0500

    Various fixes

--- plugins/libmodsynth/widgets/lms_checkbox.h
+++ plugins/libmodsynth/widgets/lms_checkbox.h
@@ -14,68 +14,34 @@
 class LMS_checkbox : public LMS_control
 {   
     public:
-        /* LMS_checkbox(
-         * QString a_label,  //The label to display above the knob
-         * int a_min,  //The minimum value of the knob.
-         * int a_max, //The maximum value of the knob.
-         * int a_step_size, //Step size.  Usually 1
-         * int a_value, //The initial value        
-         * QWidget *a_parent, //The parent widget
-         * LMS_style_info * a_style_info, //A style_info object to provide information about theming         
-         * int a_lms_port) //The port defined in synth.h for this control
-         */
-        LMS_checkbox(QString a_label,int a_min, int a_max, int a_step_size, int a_value, 
-        QWidget *a_parent, LMS_style_info * a_style_info, int a_lms_port)
-        {
-            lms_layout = new QVBoxLayout();
-            lms_label = new QLabel(a_parent);
-            lms_label->setMinimumWidth(a_style_info->lms_label_width);
-            lms_label->setText(a_label);
-            lms_label->setAlignment(Qt::AlignCenter);
-            lms_checkbox = new  QCheckBox(a_parent);
-                        
-            if(a_style_info->lms_use_label_style)
-            {
-                lms_label->setStyleSheet((a_style_info->lms_label_style));
-            }
-
-            lms_layout->addWidget(lms_label, -1, Qt::AlignCenter);
-            lms_layout->addWidget(lms_checkbox, -1, Qt::AlignCenter);
-                        
-            lms_port = a_lms_port;            
-        }
-        
+                
         /* This constructor is meant to be used only with LMS_mod_matrix,
          * it does not initialize the layout or label.
          * 
-         * LMS_knob_regular(
-         * int a_min,  //The minimum value of the knob.
-         * int a_max, //The maximum value of the knob.
-         * int a_step_size, //Step size.  Usually 1
-         * int a_value, //The initial value        
+         * LMS_checkbox(
          * QWidget *a_parent, //The parent widget
          * LMS_style_info * a_style_info, //A style_info object to provide information about theming         
          * int a_lms_port) //The port defined in synth.h for this control
          */
-        LMS_checkbox(int a_min, int a_max, int a_step_size, int a_value, 
-        QWidget *a_parent, LMS_style_info * a_style_info, int a_lms_port)
+        LMS_checkbox(QWidget *a_parent, LMS_style_info * a_style_info, int a_lms_port, QString a_text)
         {            
-            lms_checkbox = new QSpinBox(a_parent);            
-            /*
-            lms_spinbox->setMinimumSize((a_style_info->lms_knob_size),(a_style_info->lms_knob_size));
-            lms_spinbox->setMaximumSize((a_style_info->lms_knob_size),(a_style_info->lms_knob_size));
-            */
-            lms_port = a_lms_port;            
+            lms_checkbox = new QCheckBox(a_parent);
+            lms_checkbox->setMaximumWidth(90);
+            lms_checkbox->setText(a_text);            
+            lms_port = a_lms_port;        
+            lms_layout = new QVBoxLayout();
+            lms_layout->addItem(new QSpacerItem(20, 20, QSizePolicy::Expanding, QSizePolicy::Minimum));
+            lms_layout->addWidget(lms_checkbox);
+            lms_layout->addItem(new QSpacerItem(20, 20, QSizePolicy::Expanding, QSizePolicy::Minimum));
         }
         
         void lms_value_changed(int a_value)
         {
                 //Do nothing?
         }        
-                
-        QVBoxLayout * lms_layout;
-        QLabel * lms_label;
+        
         QCheckBox * lms_checkbox;        
+        QVBoxLayout * lms_layout;
         
         void lms_set_value(int a_value)
         {
