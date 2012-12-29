7d0c0a942816ff980f5bb619990a41861ae2c3bb
Jeff Hubbard <jhubbard651@users.sf.net>
2012-12-29 00:08:26 -0500

    Begin implementing audio tracks in the back-end...

---
+++ plugins/pydaw/src/pydaw_audio_tracks.h
@@ -0,0 +1,35 @@
+/* 
+ * File:   pydaw_audio_tracks.h
+ * Author: JeffH
+ *
+ * Created on December 29, 2012, 12:03 AM
+ */
+
+#ifndef PYDAW_AUDIO_TRACKS_H
+#define	PYDAW_AUDIO_TRACKS_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+typedef struct 
+{
+    
+} pydaw_audio_track;
+
+pydaw_audio_track * g_pydaw_audio_track_get();
+
+
+
+pydaw_audio_track * g_pydaw_audio_track_get()
+{
+    
+    return 0;
+}
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* PYDAW_AUDIO_TRACKS_H */
+
