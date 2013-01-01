8aa672862b9c777ee188bc416169869be4a4401d
Jeff Hubbard <jhubbard651@users.sf.net>
2013-01-01 11:28:12 -0500

    Initial work on audio items in PyDAW's engine...  Nothing useful quite yet, I just wanted to save what I had before I make some improvements to Euphoria first so that PyDAW will automatically benefit from it when I adapt the code over...

--- plugins/pydaw/src/pydaw_audio_tracks.h
+++ plugins/pydaw/src/pydaw_audio_tracks.h
@@ -11,20 +11,61 @@
 #ifdef	__cplusplus
 extern "C" {
 #endif
+    
+#include <sndfile.h>
+
+#define PYDAW_MAX_AUDIO_ITEM_COUNT 32
+    
+typedef struct 
+{
+    float * samples[2];
+    int length;    
+} t_pydaw_audio_item;
 
 typedef struct 
 {
-    
-} pydaw_audio_track;
-
-pydaw_audio_track * g_pydaw_audio_track_get();
+    t_pydaw_audio_item * items;
+} t_pydaw_audio_items;
 
 
 
-pydaw_audio_track * g_pydaw_audio_track_get()
+t_pydaw_audio_item * g_pydaw_audio_item_get();
+t_pydaw_audio_item * g_pydaw_audio_items_get();
+
+t_pydaw_audio_item * g_pydaw_audio_item_get()
 {
+    t_pydaw_audio_item * f_result;
     
-    return 0;
+    if(posix_memalign((void**)&f_result, 16, sizeof(t_pydaw_audio_item)) != 0)
+    {     
+        return 0;
+    }
+    
+    f_result->length = 0;
+    f_result->samples[0] = 0;
+    f_result->samples[1] = 0;
+    
+    return f_result;
+}
+
+t_pydaw_audio_item * g_pydaw_audio_items_get()
+{
+    t_pydaw_audio_items * f_result;
+    
+    if(posix_memalign((void**)&f_result, 16, sizeof(t_pydaw_audio_items)) != 0)
+    {     
+        return 0;
+    }
+    
+    int f_i = 0;
+    
+    while(f_i < PYDAW_MAX_AUDIO_ITEM_COUNT)
+    {
+        f_result->items[f_i] = g_pydaw_audio_item_get();
+        f_i++;
+    }
+    
+    return f_result;
 }
 
 #ifdef	__cplusplus
