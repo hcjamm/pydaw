e78aa0ed2a7c6fb3841e9561a4e49cb4a96a7bda
Jeff Hubbard <jhubbard651@users.sf.net>
2013-01-05 11:23:23 -0500

    Fade samples in/out on starting/stopping playback...  Still some work to be done here...

--- plugins/pydaw/src/pydaw_audio_tracks.h
+++ plugins/pydaw/src/pydaw_audio_tracks.h
@@ -113,7 +113,8 @@ t_pydaw_audio_item * g_pydaw_audio_item_get(float a_sr)
     f_result->uid = -1;
     
     f_result->adsr = g_adsr_get_adsr(a_sr);
-    v_adsr_set_adsr(f_result->adsr, 0.01f, 0.2f, 1.0f, 0.1f);
+    v_adsr_set_adsr_db(f_result->adsr, 0.01f, 0.1f, 0.0f, 0.1f);
+    v_adsr_retrigger(f_result->adsr);
     f_result->sample_read_head = g_ifh_get();
         
     return f_result;
