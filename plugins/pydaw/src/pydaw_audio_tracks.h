9d7a21a27f2ac0b370ad764481acfe9ee1ce6eaa
PyDAW Team <pydawteam@users.sf.net>
2013-01-06 10:38:22 -0500

    Better default ADSR settings for audio items, to avoid that 'whoosh' sound on playback, or overl sudden stops...

--- plugins/pydaw/src/pydaw_audio_tracks.h
+++ plugins/pydaw/src/pydaw_audio_tracks.h
@@ -110,7 +110,7 @@ t_pydaw_audio_item * g_pydaw_audio_item_get(float a_sr)
     f_result->uid = -1;
     
     f_result->adsr = g_adsr_get_adsr(1.0f/a_sr);
-    v_adsr_set_adsr_db(f_result->adsr, 0.06f, 0.1f, 0.0f, 0.1f);
+    v_adsr_set_adsr_db(f_result->adsr, 0.003f, 0.1f, 0.0f, 0.2f);
     v_adsr_retrigger(f_result->adsr);
     f_result->sample_read_head = g_ifh_get();
         
