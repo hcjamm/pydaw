c8a93f9d7bb5c38afa11189164a50aae819c6ee4
Jeff Hubbard <jhubbard651@users.sf.net>
2013-01-11 13:06:51 -0500

    Added my new reverb module..  It's not a finished product yet, but I'm invested enough into it now that I wouldn't wish to lose it in a hard drive failure...

---
+++ plugins/libmodsynth/modules/delay/reverb.h
@@ -0,0 +1,142 @@
+/* 
+ * File:   reverb.h
+ * Author: JeffH
+ * 
+ * A simple digital reverb built with off-the-shelf parts from the rest of the library.
+ * The focus is on quality, CPU efficiency, and a minimal number of controls(targeting only time, wet and color knobs)
+ *
+ * Created on January 10, 2013, 8:29 AM
+ */
+
+#ifndef PYDAW_REVERB_H
+#define	PYDAW_REVERB_H
+
+#define PYDAW_REVERB_CHANNELS 2
+#define PYDAW_REVERB_DIFFUSER_COUNT 4
+#define PYDAW_REVERB_TAP_COUNT 8
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#include "../../lib/amp.h"
+#include "../../lib/pitch_core.h"
+#include "../filter/comb_filter.h"
+#include "../filter/svf.h"
+    
+typedef struct 
+{
+    int iter1;
+    float color;
+    float wet;
+    float wet_linear;
+    float time;
+    float output[PYDAW_REVERB_CHANNELS];    
+    t_state_variable_filter * diffusers[PYDAW_REVERB_CHANNELS][PYDAW_REVERB_DIFFUSER_COUNT];
+    t_state_variable_filter * hp[PYDAW_REVERB_CHANNELS];
+    t_state_variable_filter * lp[PYDAW_REVERB_CHANNELS]; //[PYDAW_REVERB_TAP_COUNT];
+    t_comb_filter * taps[PYDAW_REVERB_CHANNELS][PYDAW_REVERB_TAP_COUNT];
+    t_pit_pitch_core * pitch_core;
+    t_amp * amp;
+} t_rvb_reverb;
+
+t_rvb_reverb * g_rvb_reverb_get(float);
+void v_rvb_reverb_set(t_rvb_reverb *, float, float, float);
+inline void v_rvb_reverb_run(t_rvb_reverb *, float, float);
+
+void v_rvb_reverb_set(t_rvb_reverb * a_reverb, float a_time, float a_wet, float a_color)
+{
+    if(a_time != (a_reverb->time))
+    {
+        
+    }
+    
+    if(a_wet != (a_reverb->wet))
+    {
+        
+    }
+    
+    if(a_color != (a_reverb->color))
+    {
+        
+    }
+}
+
+inline void v_rvb_reverb_run(t_rvb_reverb * a_reverb, float a_input0, float a_input1)
+{
+    a_reverb->iter1 = 0;
+    
+    a_reverb->output[0] = 0.0f;
+    a_reverb->output[1] = 0.0f;
+    
+    while((a_reverb->iter1) < PYDAW_REVERB_TAP_COUNT)
+    {
+        v_cmb_set_input(a_reverb->taps[0][(a_reverb->iter1)], a_input0);
+        v_cmb_set_input(a_reverb->taps[1][(a_reverb->iter1)], a_input1);
+    
+        a_reverb->output[0] += (a_reverb->taps[0][(a_reverb->iter1)]->output_sample);
+        a_reverb->output[1] += (a_reverb->taps[1][(a_reverb->iter1)]->output_sample);
+        
+        a_reverb->iter1 = (a_reverb->iter1) + 1;
+    }
+    
+    a_reverb->iter1 = 0;
+    
+    while((a_reverb->iter1) < PYDAW_REVERB_TAP_COUNT)
+    {
+        a_reverb->output[0] = v_svf_run_2_pole_allpass(a_reverb->diffusers[0][(a_reverb->iter1)], a_reverb->output[0]);
+        a_reverb->output[1] = v_svf_run_2_pole_allpass(a_reverb->diffusers[1][(a_reverb->iter1)], a_reverb->output[1]);
+        
+        a_reverb->iter1 = (a_reverb->iter1) + 1;
+    }        
+}
+
+t_rvb_reverb * g_rvb_reverb_get(float a_sr)
+{
+    t_rvb_reverb * f_result;
+    
+    int f_i = 0;
+    int f_i2 = 0;
+    
+    f_result->color = 0.0f;
+    f_result->iter1 = 0;
+    f_result->time = 1.0f;
+    f_result->wet = -40.0f;
+    f_result->wet_linear = 0.0f;
+    
+    f_result->amp = g_amp_get();
+    f_result->pitch_core = g_pit_get();
+    
+    while(f_i < PYDAW_REVERB_CHANNELS)
+    {
+        f_result->output[f_i] = 0.0f;
+        
+        f_result->hp = g_svf_get(a_sr);
+        f_result->lp = g_svf_get(a_sr);
+        
+        while(f_i2 < PYDAW_REVERB_TAP_COUNT)
+        {
+            f_result->taps[f_i][f_i2] = g_cmb_get_comb_filter(a_sr);
+            f_i2++;
+        }
+        
+        f_i2 = 0;
+        
+        while(f_i2 < PYDAW_REVERB_DIFFUSER_COUNT)
+        {
+            f_result->diffusers[f_i][f_i2] = g_svf_get(a_sr);
+            f_i2++;
+        }
+        
+        f_i++;
+    }
+    
+    return f_result;    
+}
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* PYDAW_REVERB_H */
+
