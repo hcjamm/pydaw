d87229aaf6ff823ef43eb16f543a0e358d9e6608
PyDAW Team <pydawteam@users.sf.net>
2013-01-12 12:03:27 -0500

    More shell-script-ery for automating Live DVD/USB image creation...

---
+++ plugins/distro/mint/pydaw-live.sh
@@ -0,0 +1,8 @@
+#Run the dns-fix utility so that Linux Mint live use will have working internet
+dns-fix
+
+if [ -d /home/mint ]; then
+	cp /usr/share/applications/pydaw* /home/mint/Desktop
+else
+	echo "No /home/mint folder found, not copying shortcut(s)"
+fi
