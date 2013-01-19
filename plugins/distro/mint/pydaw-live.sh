e2d219630a44955034650d0cfe5f4cd3faaf6a32
PyDAW Team <pydawteam@users.sf.net>
2013-01-19 11:36:56 -0500

    Also place the Qjackctl and audacity shortcuts on the desktop...  Until which point I can have native replacements for them within PyDAW...

--- plugins/distro/mint/pydaw-live.sh
+++ plugins/distro/mint/pydaw-live.sh
@@ -3,6 +3,8 @@ dns-fix
 
 if [ -d /home/mint ]; then
 	cp /usr/share/applications/pydaw* /home/mint/Desktop
+	cp /usr/share/applications/qjackctl* /home/mint/Desktop
+	cp /usr/share/applications/audacity* /home/mint/Desktop
 else
 	echo "No /home/mint folder found, not copying shortcut(s)"
 fi
