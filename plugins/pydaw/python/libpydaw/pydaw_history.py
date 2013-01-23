03c35fd1b0ffdf8c9422169285ed9ac1e81b59f3
Jeff Hubbard <jhubbard651@users.sf.net>
2013-01-23 08:09:44 -0500

    More work to the new Git replacement for project history...

--- plugins/pydaw/python/libpydaw/pydaw_history.py
+++ plugins/pydaw/python/libpydaw/pydaw_history.py
@@ -23,30 +23,56 @@ class pydaw_history:
 
         if not os.path.isfile(self.db_file):
             pass  #TODO Create the db and tables, including the schemas...
-
-    def add(self, a_file_name=None, a_dir=None):  #Specifies that a_file_name should be tracked
-        if a_file_name is None and a_dir is not None:
-            pass
-        elif a_file_name is not None and a_dir is None:
-            pass
         else:
-            print("Invalid parameters to pydaw_history.add()")
+            pass  #TODO:  Load the existing data...
 
-    def commit(self):  #I guess this should always be like a git commit -a ....  for this purpose?
-        pass
+    def add_file(self, a_file_name):  #Specifies that a_file_name should be tracked
+        f_result = pydaw_history_file(a_file_name)
+        if not self.files_contains(f_result):
+            self.tracked_files.append(f_result)
+
+    def add_folder(self, a_dir, a_ext=None):
+        f_list = os.listdir(a_dir)
+        for f_file in f_list:
+            if os.path.isfile(f_file):
+                if a_ext is None or f_file.endswith(a_ext):
+                    self.add_file(f_file)
+
+    def commit(self, a_message):  #I guess this should always be like a git commit -a ....  for this purpose?
+        f_result = pydaw_history_commit()
+        for f_file in self.tracked_files:
+            if f_file.modified:
+                f_file_handle = open(f_file.file_name, "r")
+                f_file_text = f_file_handle.readlines()
+                f_file_handle.close()
+                f_diff_text = difflib.unified_diff(f_file.file_text.split("\n"), f_file_text)
+                #TODO:  Check that there is an actual difference
+                f_result.diffs.append(pydaw_history_diff(f_file.file_name, f_diff_text))
+        #TODO:  Flush to the database using variable binding...
+
+    def files_contains(self, a_file):
+        for f_file in self.tracked_files:
+            if a_file == a_file:
+                return True
+        return False
 
 
 class pydaw_history_file:
     def __init__(self, a_file_name):
         self.file_name = a_file_name
-        self.modified = False
+        self.modified = True
+        self.file_text = ""
 
     def __eq__(self, other):
         return self.file_name == other.file_name
 
 class pydaw_history_commit:
-    def __init__(self):
-        pass
+    def __init__(self, a_message):
+        self.message = a_message
+        self.diffs = []
+
+    def serialize(self):
+        pass  #TODO:  Figure otu whether to return as JSON or cPickle'd...
 
 class pydaw_history_diff:
     """ A diff for a single file """
