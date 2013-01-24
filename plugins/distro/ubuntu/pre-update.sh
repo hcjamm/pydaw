37ac3c37877a9e02d9053684d3f0a0cbf81672c1
PyDAW Team <pydawteam@users.sf.net>
2013-01-24 08:59:22 -0500

    Move the sources.list stuff to a separate script, since it has to be run before the build can happen, but the official live DVD policy is not to automatically risk breaking a working live DVD with updates at every build...

---
+++ plugins/distro/ubuntu/pre-update.sh
@@ -0,0 +1,3 @@
+#For whatever reason, Ubuntu doesn't enable the Universe and Multiverse repositories
+#by default on the live DVD image, hence we need to copy a list from an actual install
+cp ./sources.list /etc/apt/sources.list
