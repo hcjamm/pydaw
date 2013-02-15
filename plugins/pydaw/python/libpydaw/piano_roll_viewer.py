f5ead7086035063adb1dd18f0b0816bb8db76b11
PyDAW Team <pydawteam@users.sf.net>
2013-02-15 10:19:06 -0500

    Some pre-integration logistical and aesthetic improvements to Perry's piano-roll editor

--- plugins/pydaw/python/libpydaw/piano_roll_viewer.py
+++ plugins/pydaw/python/libpydaw/piano_roll_viewer.py
@@ -6,17 +6,26 @@ import sys
 from PyQt4 import QtGui, QtCore
 from pydaw_gradients import *
 
+global_piano_roll_snap = True
+global_piano_roll_snap_value = 1.0
+global_piano_keys_width = 34  #Width of the piano keys in px
+global_piano_roll_header_height = 20
+global_piano_roll_pixels_per_beat = 100000 #TODO:  the real value
+
+pydaw_note_gradient = QtGui.QLinearGradient(QtCore.QPointF(0, 0), QtCore.QPointF(0, 12))  # I might have the 10 and 0 switched up, LOL...
+#pydaw_note_gradient.setColorAt(0, QtGui.QColor(180, 150, 33))
+pydaw_note_gradient.setColorAt(0, QtGui.QColor(163, 136, 30))
+pydaw_note_gradient.setColorAt(1, QtGui.QColor(230, 221, 45))
+
 class note_item(QtGui.QGraphicsRectItem):
-    def __init__(self, a_length, a_note_height, a_value_snap, a_piano_width, a_header_height, a_padding):
+    def __init__(self, a_length, a_note_height):
         QtGui.QGraphicsRectItem.__init__(self, 0, 0, a_length, a_note_height)
         self.setFlag(QtGui.QGraphicsItem.ItemIsMovable)
         self.setFlag(QtGui.QGraphicsItem.ItemSendsGeometryChanges)
-        self.grid_snap = True
-        self.value_snap = a_value_snap
+        self.setBrush(pydaw_note_gradient)
         self.note_height = a_note_height
-        self.piano_width = a_piano_width + a_padding
-        self.header_height = a_header_height
-        
+        self.setToolTip("Double-click to edit note properties, click and drag to move")
+
     def mousePressEvent(self, a_event):
         QtGui.QGraphicsRectItem.mousePressEvent(self, a_event)
         self.o_brush = self.brush()
@@ -27,13 +36,13 @@ class note_item(QtGui.QGraphicsRectItem):
         QtGui.QGraphicsRectItem.mouseMoveEvent(self, a_event)
         f_pos_x = self.pos().x()
         f_pos_y = self.pos().y()
-        if f_pos_x < self.piano_width:
-            f_pos_x = self.piano_width
-        if f_pos_y < self.header_height:
-            f_pos_y = self.header_height
+        if f_pos_x < global_piano_keys_width:
+            f_pos_x = global_piano_keys_width
+        if f_pos_y < global_piano_roll_header_height:
+            f_pos_y = global_piano_roll_header_height
         f_pos_y = int((f_pos_y - self.o_pos.y())/self.note_height) * self.note_height
-        if self.grid_snap:
-            f_pos_x = int((f_pos_x - self.o_pos.x())/self.value_snap) * self.value_snap
+        if global_piano_roll_snap:
+            f_pos_x = int((f_pos_x - self.o_pos.x())/global_piano_roll_snap_value) * global_piano_roll_snap_value
         else:
             f_pos_x -= self.o_pos.x()
         self.setPos(self.o_pos.x()+f_pos_x, self.o_pos.y()+f_pos_y)
@@ -70,16 +79,16 @@ class piano_roll_viewer(QtGui.QGraphicsView):
         self.start_octave = -2
         self.notes_in_octave = 12
         self.total_notes = (self.end_octave - self.start_octave) * self.notes_in_octave + 1 #for C8
-        self.note_height = 12
+        self.note_height = 15
         self.octave_height = self.notes_in_octave * self.note_height
 
-        self.header_height = 20
+        self.header_height = global_piano_roll_header_height
 
         self.piano_height = self.note_height*self.total_notes
-        self.piano_width = 23
+        self.piano_width = 32
         self.padding = 2
         self.piano_height = self.note_height * self.total_notes
-        
+
         QtGui.QGraphicsView.__init__(self)
         self.scene = QtGui.QGraphicsScene(self)
         self.scene.setBackgroundBrush(QtGui.QColor(100,100,100))
@@ -124,28 +133,6 @@ class piano_roll_viewer(QtGui.QGraphicsView):
                     f_key.setBrush(QtGui.QColor(0,0,0))
                 else:
                     f_key.setBrush(QtGui.QColor(255,255,255))
-        #lesser implementation
-        #f_octave_number = 7
-        #f_key = piano_key_item(self.piano_width, self.note_height, self.piano)
-        #f_key.setBrush(QtGui.QColor(255,255,255))
-        #f_octave = QtGui.QGraphicsSimpleTextItem("C8", f_key)
-        #f_octave.setPos(3, 0)
-        #f_octave.setFont(f_piano_label)
-        #for i in range(1, self.total_notes+1):
-        #    f_key = piano_key_item(self.piano_width, self.note_height, self.piano)
-        #    f_key.setPos(0, self.note_height*(i-1))
-        #    if i in f_black_notes:
-        #        f_key.setBrush(QtGui.QColor(0,0,0))
-        #    else:
-        #        f_key.setBrush(QtGui.QColor(255,255,255))
-        #    if (i % 12) == 0:
-        #        f_octave = QtGui.QGraphicsSimpleTextItem("C%d" % (f_octave_number), f_key)
-        #        f_octave.setPos(3, 0)
-        #        f_octave.setFont(f_piano_label)
-        #        f_octave_number -= 1
-        #        if i:
-        #            for n in range(len(f_black_notes)):
-        #                f_black_notes[n] += 12
 
     def draw_grid(self):
         f_black_notes = [2, 4, 6, 9, 11]
@@ -154,7 +141,7 @@ class piano_roll_viewer(QtGui.QGraphicsView):
                 f_note_bar = QtGui.QGraphicsRectItem(0, 0, self.viewer_width, self.note_height, self.piano)
                 f_note_bar.setPos(self.piano_width + self.padding,  self.note_height*(j) + self.octave_height*(i-1))
                 #basic implementation of zth's "show scales", there are a few transparency issues
-                #if j not in f_black_notes: 
+                #if j not in f_black_notes:
                 #    f_note_bar.setBrush(QtGui.QColor(230,230,230,100))
         f_beat_pen = QtGui.QPen()
         f_beat_pen.setWidth(2)
@@ -169,8 +156,8 @@ class piano_roll_viewer(QtGui.QGraphicsView):
                 f_number.setPos(self.beat_width * i + 5, 2)
                 f_number.setBrush(QtCore.Qt.white)
                 for j in range(0, self.grid_div):
-                    f_line = QtGui.QGraphicsLineItem(0, 0, 0, self.piano_height, self.header) 
-                    if float(j) == self.grid_div / 2.0: 
+                    f_line = QtGui.QGraphicsLineItem(0, 0, 0, self.piano_height, self.header)
+                    if float(j) == self.grid_div / 2.0:
                         f_line.setLine(0, 0, 0, self.piano_height)
                         f_line.setPos((self.beat_width*i)+(self.value_width*j), self.header_height)
                     else:
@@ -178,22 +165,21 @@ class piano_roll_viewer(QtGui.QGraphicsView):
                         f_line.setPen(f_line_pen)
 
     def set_zoom(self, a_scale):
-        self.scale(a_scale, a_scale)
-        
+        self.scale(a_scale, 1.0)
+
     def clear_drawn_items(self):
         self.scene.clear()
         self.draw_header()
         self.draw_piano()
         self.draw_grid()
 
-    def draw_note(self, a_note, a_start, a_length, a_velocity, a_color):
+    def draw_note(self, a_note, a_start, a_length, a_velocity):
         '''a_note is the midi number, a_start is 1-4.99, a_length is in beats, a_velocity is 0-127'''
         f_start = self.piano_width + self.padding + self.beat_width*(a_start - 1)
         f_length = self.beat_width*(a_length*self.item_length)
         f_note = self.header_height + self.note_height * (self.total_notes - a_note - 1)
-        f_note_item = note_item(f_length, self.note_height, self.value_width, self.piano_width, self.header_height, self.padding)
+        f_note_item = note_item(f_length, self.note_height)
         f_note_item.setPos(f_start, f_note)
-        f_note_item.setBrush(a_color)
         f_vel_opacity = QtGui.QGraphicsOpacityEffect()
         f_vel_opacity.setOpacity(a_velocity/127.0)
         f_note_item.setGraphicsEffect(f_vel_opacity)
@@ -202,8 +188,7 @@ class piano_roll_viewer(QtGui.QGraphicsView):
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
     view = piano_roll_viewer()
-    color = QtGui.QColor(100, 100, 255)
-    view.draw_note(60, 2.0625, 0.25, 127, pydaw_track_gradients[4]) 
-    view.draw_note(120, 1, 0.5, 100, pydaw_track_gradients[4])
+    view.draw_note(60, 2.0625, 0.25, 127)
+    view.draw_note(120, 1, 0.5, 100)
     view.show()
     sys.exit(app.exec_())
