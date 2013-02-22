73812d5a09dedeecc9e34f314fe97b7bf70c928b
PyDAW Team <pydawteam@users.sf.net>
2013-02-22 18:14:55 -0500

    Update tooltip

--- plugins/pydaw/python/pydaw.py
+++ plugins/pydaw/python/pydaw.py
@@ -1872,7 +1872,10 @@ class piano_roll_note_item(QtGui.QGraphicsRectItem):
         self.setFlag(QtGui.QGraphicsItem.ItemSendsGeometryChanges)
         self.setBrush(pydaw_note_gradient)
         self.note_height = a_note_height
-        self.setToolTip("Double-click to edit note properties, click and drag to move,\nclick and drag the end to change length, and Shift+click to delete.\nYou can marquee-select multiple items by holding down CTRL, then clicking and dragging.")
+        self.setToolTip("Double-click to edit note properties, click and drag to move,\n" + \
+        "click and drag the end to change length, and Shift+click to delete.\n" + \
+        "You can marquee-select multiple items by holding down CTRL, then clicking and dragging.\n\n" + \
+        "To edit multiple items as one logical item, select multiple items in the region editor and right-click + 'Edit Selected Items as Group'")
         self.note_item = a_note_item
         self.setAcceptHoverEvents(True)
         self.resize_start_pos = 0.0
