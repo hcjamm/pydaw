b1e262b7f836f4170dda2a1b07ab99826b63e60d
PyDAW Team <pydawteam@users.sf.net>
2013-04-13 18:24:46 -0400

    Add a LoFi effect to Modulex.  It's just a bit-crusher for now, but I may add a sample-rate-reducer later...

---
+++ plugins/libmodsynth/modules/distortion/lofi.h
@@ -0,0 +1,75 @@
+/* 
+ * File:   lofi.h
+ * Author: JeffH
+ * 
+ * Just a bit-crusher for now, might include a sample-rate-reducer later...
+ *
+ * Created on April 13, 2013, 5:55 PM
+ */
+
+#ifndef PYDAW_LOFI_H
+#define	PYDAW_LOFI_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#include <math.h>
+    
+typedef struct 
+{
+    float bits, multiplier, recip;
+    int val0, val1;
+    float output0, output1;
+} t_lfi_lofi;
+
+t_lfi_lofi * g_lfi_lofi_get();
+void v_lfi_lofi_set(t_lfi_lofi*, float);
+void v_lfi_lofi_run(t_lfi_lofi*, float, float);
+
+t_lfi_lofi * g_lfi_lofi_get()
+{
+    t_lfi_lofi * f_result;
+    
+    if(posix_memalign((void**)&f_result, 16, (sizeof(t_lfi_lofi))) != 0)
+    {
+        return 0;
+    }
+    
+    f_result->output0 = 0.0f;
+    f_result->output1 = 0.0f;
+    f_result->bits = 163.654f;
+    f_result->multiplier = -443322.0f;
+    f_result->recip = 1.010101f;
+    f_result->val0 = 0;
+    f_result->val1 = 0;
+    
+    return f_result;
+}
+
+void v_lfi_lofi_set(t_lfi_lofi* a_lfi, float a_bits)
+{
+    if(a_lfi->bits != a_bits)
+    {
+        a_lfi->bits = a_bits;
+        a_lfi->multiplier = pow(2.0, a_bits);
+        a_lfi->recip = 1.0f / (a_lfi->multiplier);
+    }
+}
+
+void v_lfi_lofi_run(t_lfi_lofi* a_lfi, float a_in0, float a_in1)
+{
+    a_lfi->val0 = (int)((a_lfi->multiplier) * a_in0);
+    a_lfi->val1 = (int)((a_lfi->multiplier) * a_in1);
+    
+    a_lfi->output0 = ((float)(a_lfi->val0)) * (a_lfi->recip);
+    a_lfi->output1 = ((float)(a_lfi->val1)) * (a_lfi->recip);
+}
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* PYDAW_LOFI_H */
+
