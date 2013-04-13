c10068108deeca1ef95b2592d50580b7b14c1e56
PyDAW Team <pydawteam@users.sf.net>
2013-04-13 09:23:19 -0400

    Fixed the delay ducking to work again AND made it super awesome...

--- plugins/libmodsynth/modules/delay/lms_delay.h
+++ plugins/libmodsynth/modules/delay/lms_delay.h
@@ -23,6 +23,7 @@ extern "C" {
 #include "../../lib/denormal.h"
 #include "../signal_routing/audio_xfade.h"
 #include "../modulation/env_follower.h"
+#include "../dynamics/limiter.h"
 /* A multi-mode delay module.  This is a complete delay with stereo, ping-pong, etc... modes
  * feeback can be routed out and back into the module.
  * 
@@ -49,12 +50,14 @@ typedef struct st_lms_delay
     t_enf_env_follower * input_env_follower;  //Checks for overflow
     float wet_dry_diff;  //difference between wet and dry output volume
     float combined_inputs;  //Add output 0 and 1
+    t_lim_limiter * limiter;
+    float last_duck;
     
     t_amp * amp_ptr;    
 }t_lms_delay;
 
 t_lms_delay * g_ldl_get_delay(float,float);
-inline void v_ldl_set_delay(t_lms_delay*,float,float,float,float,float);
+inline void v_ldl_set_delay(t_lms_delay*,float,float,float,float,float,float);
 inline void v_ldl_run_delay(t_lms_delay*,float,float);
 
 /*t_lms_delay * g_ldl_get_delay(
@@ -86,7 +89,9 @@ t_lms_delay * g_ldl_get_delay(float a_seconds, float a_sr)
     f_result->combined_inputs = 0.0f;
     
     f_result->amp_ptr = g_amp_get();
-    
+    f_result->limiter = g_lim_get(a_sr);
+    f_result->last_duck = -99.999f;
+        
     return f_result;
 }
 
@@ -106,6 +111,8 @@ inline void v_ldl_run_delay(t_lms_delay* a_dly, float a_in0, float a_in1)
         a_dly->feedback_linear = f_db_to_linear(((-3.0f - (a_dly->feeback_env_follower->output_smoothed)) * 0.3f), a_dly->amp_ptr);
     }
     
+    v_lim_run(a_dly->limiter, a_in0, a_in1);
+    
     v_dly_run_delay(a_dly->delay0, 
             f_axf_run_xfade(a_dly->stereo_xfade0, 
             (a_in0 + ((a_dly->feedback0) * (a_dly->feedback_linear))),
@@ -124,8 +131,8 @@ inline void v_ldl_run_delay(t_lms_delay* a_dly, float a_in0, float a_in1)
     a_dly->feedback0 = f_remove_denormal((a_dly->tap0->output));
     a_dly->feedback1 = f_remove_denormal((a_dly->tap1->output));
     
-    v_dw_run_dry_wet(a_dly->dw0, a_in0, (a_dly->feedback0));
-    v_dw_run_dry_wet(a_dly->dw1, a_in1, (a_dly->feedback1));
+    v_dw_run_dry_wet(a_dly->dw0, a_in0, (a_dly->feedback0) * (a_dly->limiter->gain));
+    v_dw_run_dry_wet(a_dly->dw1, a_in1, (a_dly->feedback1) * (a_dly->limiter->gain));
     
     a_dly->output0 = (a_dly->dw0->output);
     a_dly->output1 = (a_dly->dw1->output);
@@ -141,7 +148,7 @@ inline void v_ldl_run_delay(t_lms_delay* a_dly, float a_in0, float a_in1)
  * float a_dry,
  * float a_stereo)  //Crossfading between dual-mono and stereo.  0 to 1
  */
-inline void v_ldl_set_delay(t_lms_delay* a_dly,float a_seconds, float a_feeback_db, float a_wet, float a_dry, float a_stereo)
+inline void v_ldl_set_delay(t_lms_delay* a_dly,float a_seconds, float a_feeback_db, float a_wet, float a_dry, float a_stereo, float a_duck)
 {
     v_dly_set_delay_seconds(a_dly->delay0, a_dly->tap0, a_seconds);
     v_dly_set_delay_seconds(a_dly->delay1, a_dly->tap1, a_seconds);
@@ -154,6 +161,12 @@ inline void v_ldl_set_delay(t_lms_delay* a_dly,float a_seconds, float a_feeback_
         a_dly->feedback_db = a_feeback_db;
         
         a_dly->feedback_linear = f_db_to_linear_fast(a_feeback_db, a_dly->amp_ptr);
+    }
+    
+    if(a_dly->last_duck != a_duck)
+    {
+        a_dly->last_duck = a_duck;
+        v_lim_set(a_dly->limiter, a_duck, 0.0f, 400.0f);
     }
     
     v_dw_set_dry_wet(a_dly->dw0, a_dry, a_wet);
