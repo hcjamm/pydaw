5ea7494dbaaf7a23514797cd87e94d3964a693bc
PyDAW Team <pydawteam@users.sf.net>
2013-06-07 23:35:34 -0400

    Add a script that automagically kills PyDAW with SIGSEGV so that it will dump a core...  Useful for inspecting the state of DSP modules that are misbehaving, but not in such a way as to cause a crash.

---
+++ plugins/pydaw/kill.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+#Force a core dump when something isn't quite right, but the program won't
+#crash on it's own.  This only works when debug.sh was used to run PyDAW
+
+
+import commands
+
+f_val = commands.getoutput('ps -ef | grep \./pydaw3 | grep -v grep | grep -v /bin/sh')
+
+f_arr = f_val.split()
+
+f_test = int(f_arr[1])
+
+f_kill = "/bin/kill -s SIGSEGV " + f_arr[1]
+
+print(f_kill)
+
+f_result = commands.getoutput(f_kill)
+
+print(f_result)
