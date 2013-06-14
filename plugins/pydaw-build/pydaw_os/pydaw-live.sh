1e04fe68587dc58c35366f2d26e898d7d43c3080
PyDAW Team <pydawteam@users.sf.net>
2013-06-13 23:08:58 -0400

    Another attempt at making Mixxx recall settings...

--- plugins/pydaw-build/pydaw_os/pydaw-live.sh
+++ plugins/pydaw-build/pydaw_os/pydaw-live.sh
@@ -4,18 +4,9 @@ if [ -d /home/ubuntu ]; then
 	cp /usr/share/applications/audacity* /home/ubuntu/Desktop
 	cp /usr/share/applications/mixxx* /home/ubuntu/Desktop
 	cp /usr/share/doc/pydaw3/readme.txt /home/ubuntu/Desktop
-	if [ -l /dev/disk/by-label/pydaw_data ]; then
-		if [ ! -d /media/pydaw_data ]; then
-			mkdir /media/pydaw_data
-			mount /dev/disk/by-label/pydaw_data /media/pydaw_data
-		fi
-		if [ ! -d /media/pydaw_data/mixxx ]; then
-			mkdir /media/pydaw_data/mixxx
-		fi
-		ln -s /media/pydaw_data/mixxx /home/ubuntu/.mixxx
-	fi
+	cp /usr/lib/pydaw3/mixxx/mixxx-launcher.py /usr/bin/mixxx-launcher.py
+	cp /usr/lib/pydaw3/mixxx/mixxx.desktop /home/ubuntu/Desktop
 else
 	echo "No /home/ubuntu folder found, not copying shortcut(s)"
 fi
-
 
