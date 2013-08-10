5a6e5c1a500db7407d44a1dd98a89c67a6acadd2
PyDAW Team <pydawteam@users.sf.net>
2013-08-10 14:28:54 -0400

    Remove all C++/Qt from the project now that the Python/PyQt port is nearing completion.

--- plugins/ray_v/build.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-
-require "../build-lib.pl";
-
-#change $plugin_name and $clean when you fork a LibModSynth plugin
-$plugin_name = "ray_v.so";
-$clean = "sudo rm -R $plugin_path/ray_v*";
-
-run_script();
