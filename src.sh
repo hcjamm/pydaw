1b3847bf74aa7e1e02487b1f70410ce0dfdb0c26
PyDAW Team <pydawteam@users.sf.net>
2013-09-20 22:52:48 -0400

    Forgot the -r switch...

--- src.sh
+++ src.sh
@@ -1,6 +1,6 @@
 git pull
 PYDAW_VERSION=pydaw3-$(cat src/pydaw3-version.txt)
-cp src $PYDAW_VERSION
+cp -r src $PYDAW_VERSION
 tar czf $PYDAW_VERSION-source-code.tar.gz $PYDAW_VERSION
 rm -rf $PYDAW_VERSION
 
