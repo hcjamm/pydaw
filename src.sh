6ba524ca2e1db2b2f6ecba58f1553c2e70bf3fdf
PyDAW Team <pydawteam@users.sf.net>
2013-09-20 22:41:19 -0400

    Have the source tarball-er Rename the folder to the release version...

--- src.sh
+++ src.sh
@@ -1,2 +1,6 @@
 git pull
-tar czf pydaw3-$(cat src/pydaw3-version.txt)-source-code.tar.gz src
+PYDAW_VERSION=pydaw3-$(cat src/pydaw3-version.txt)
+cp src $PYDAW_VERSION
+tar czf $PYDAW_VERSION-source-code.tar.gz $PYDAW_VERSION
+rm -rf $PYDAW_VERSION
+
