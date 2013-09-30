2efd6908fb8c54b8f17b6cb988947973ef701723
PyDAW Team <pydawteam@users.sf.net>
2013-09-30 11:10:22 -0400

    Rename the Fedora info file into an actual script for installing dependencies.

---
+++ src/fedora_deps.sh
@@ -0,0 +1,17 @@
+#dependencies
+sudo yum install PyQt4 pyliblo gcc alsa-lib-devel liblo-devel libsndfile-devel gcc-c++ audacity git libmad-devel numpy scipy ladspa-devel vamp-plugin-sdk-devel fftw-devel portaudio19 portmidi-217
+
+
+# Now run:
+#
+#   make clean
+#   make
+#
+# followed by this command as root
+#
+#   make install
+
+# or this command if packaging
+#
+#   make DESTDIR=/your/packaging/dir install
+#
