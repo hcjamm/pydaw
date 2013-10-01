6d1bec35dbf075240ae7c5d3ea7d9d6df0e4f542
PyDAW Team <pydawteam@users.sf.net>
2013-10-01 19:31:49 -0400

    Add a forgotten dependency.

--- src/ubuntu_deps.sh
+++ src/ubuntu_deps.sh
@@ -3,4 +3,4 @@ libportmidi-dev liblo-dev g++ libsndfile1-dev libtool gdb \
 debhelper dh-make build-essential automake autoconf \
 python3-pyqt4 python3 squashfs-tools genisoimage libmad0-dev \
 python3-scipy python3-numpy libsamplerate0-dev vamp-plugin-sdk \
-ladspa-sdk libfftw3-dev gcc
+ladspa-sdk libfftw3-dev gcc python3-dev
