fc0a52b19b84dc6563a880cca736e952822e1d78
PyDAW Team <pydawteam@users.sf.net>
2013-10-02 11:13:54 -0400

    The -devel package is required to build...

--- src/fedora_deps.sh
+++ src/fedora_deps.sh
@@ -1,5 +1,5 @@
 #dependencies
-sudo yum install python3-PyQt4 gcc alsa-lib-devel liblo-devel libsndfile-devel gcc-c++ audacity git libmad-devel python3-numpy python3-scipy ladspa-devel vamp-plugin-sdk-devel fftw-devel portmidi-217
+sudo yum install python3-PyQt4 gcc alsa-lib-devel liblo-devel libsndfile-devel gcc-c++ audacity git libmad-devel python3-numpy python3-scipy ladspa-devel vamp-plugin-sdk-devel fftw-devel portmidi-devel
 
 
 # Now run:
