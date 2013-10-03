ee06dd4a20f8a7684c5c61d773c66bbfbba0498c
PyDAW Team <pydawteam@users.sf.net>
2013-10-02 22:11:18 -0400

    Remove VAMP and LADSPA plugins from Rubberband, which has a configure script that does not allow removing them with a switch...  This allow removing Vamp-SDK and Ladspa-SDK as build dependencies for PyDAW...

--- src/ubuntu_deps.sh
+++ src/ubuntu_deps.sh
@@ -2,5 +2,5 @@ sudo apt-get install -y \
 libportmidi-dev liblo-dev g++ libsndfile1-dev libtool gdb \
 debhelper dh-make build-essential automake autoconf \
 python3-pyqt4 python3 squashfs-tools genisoimage \
-python3-scipy python3-numpy libsamplerate0-dev vamp-plugin-sdk \
-ladspa-sdk libfftw3-dev gcc python3-dev libsbsms-dev
+python3-scipy python3-numpy libsamplerate0-dev \
+libfftw3-dev gcc python3-dev libsbsms-dev
