7ed1cbe29000584521765871578182f3cba07580
PyDAW Team <pydawteam@users.sf.net>
2013-10-31 17:35:40 -0400

    Remove the core file from the src directory when doing a fresh build

--- src/install_deb.sh
+++ src/install_deb.sh
@@ -1,1 +1,7 @@
+if [ -e ./core ]; then
+	#Delete the core file if exists, sometimes the binary won't
+	#delete it and then replace with a new core
+	rm -f ./core
+fi
+
 sudo ./deb.py --default-version && sudo dpkg -i pydaw-build/pydaw*.deb
