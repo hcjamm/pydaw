081b80d4c5ef69618081a38954093415d2e9b8e9
PyDAW Team <pydawteam@users.sf.net>
2013-11-05 08:06:37 -0500

    Favor GCC 4.6 if it exists...  During the PyDAWv4 development cycle, I've been fighting with numerous compiler bugs, and even Valgrind is struggling against the binaries GCC 4.8 has been creating.
    I tried GCC 4.8.2 in Fedora 19 and I believe many/most/all of the issues
    may be fixed, so hopefully this kludge can go away at Ubuntu 14.04

--- src/ubuntu_deps.sh
+++ src/ubuntu_deps.sh
@@ -4,4 +4,17 @@ debhelper dh-make build-essential automake autoconf \
 python3-pyqt4 python3 squashfs-tools genisoimage \
 python3-scipy python3-numpy libsamplerate0-dev \
 libfftw3-dev gcc python3-dev libsbsms-dev libcpufreq-dev \
-ffmpeg lame
+ffmpeg lame 
+echo
+echo
+echo
+echo "Ubuntu 12.10, 13.04 and 13.10 users:"
+echo "It is highly recommended that you install gcc-4.6 with this command:"
+echo
+echo "sudo apt-get install gcc-4.6"
+echo
+echo "gcc-4.7 and gcc-4.8 had a number of regressions that can cause weird bugs in "
+
+echo  "PyDAW.  To the best of my knowledge, these will all be fixed in the gcc"
+echo " shipping with Ubuntu 14.04"
+echo
