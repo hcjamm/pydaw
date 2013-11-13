e8794c01ae82b9042487f8644ca66ba8883711eb
PyDAW Team <pydawteam@users.sf.net>
2013-11-13 17:51:13 -0500

    Tweak ADSR envelope parameters for that super-buttery-smooth sound. (plus Netbeans removed a ton of trailing whitespace from that file)

--- src/pydaw/libmodsynth/modules/modulation/adsr.h
+++ src/pydaw/libmodsynth/modules/modulation/adsr.h
@@ -17,39 +17,39 @@ GNU General Public License for more details.
 #ifdef	__cplusplus
 extern "C" {
 #endif
-    
+
 #include "../../constants.h"
 #include "../../lib/amp.h"
 
-#define ADSR_DB 24.0f    
-#define ADSR_DB_THRESHOLD -24.0f
-#define ADSR_DB_THRESHOLD_LINEAR 0.0625f
+#define ADSR_DB 18.0f
+#define ADSR_DB_THRESHOLD -18.0f
+#define ADSR_DB_THRESHOLD_LINEAR 0.125f
 
-#define ADSR_DB_RELEASE 36.0f    
-#define ADSR_DB_THRESHOLD_RELEASE -36.0f
-#define ADSR_DB_THRESHOLD_LINEAR_RELEASE  0.01575f
-    
+#define ADSR_DB_RELEASE 48.0f
+#define ADSR_DB_THRESHOLD_RELEASE -48.0f
+#define ADSR_DB_THRESHOLD_LINEAR_RELEASE  0.00390625f
+
 typedef struct st_adsr
 {
     float a_inc;
     float a_time;
     float d_inc;
     float d_time;
-    float s_value;    
+    float s_value;
     //float d_recip;
-    //float r_recip;    
+    //float r_recip;
     float r_inc;
     float r_time;
-    
-    float sr_recip;        
-    int stage;  //0=a,1=d,2=s,3=r,4=inactive    
+
+    float sr_recip;
+    int stage;  //0=a,1=d,2=s,3=r,4=inactive
     float output;
-    
+
     float output_db;
     float a_inc_db;
     float d_inc_db;
     float r_inc_db;
-    
+
     t_amp * amp_ptr;
 }t_adsr;
 
@@ -72,18 +72,18 @@ void v_adsr_kill(t_adsr *);
 t_adsr * g_adsr_get_adsr(float);
 
 /* void v_adsr_set_a_time(
- * t_adsr* a_adsr_ptr, 
+ * t_adsr* a_adsr_ptr,
  * float a_time)  //time in seconds
- * 
+ *
  * Sets the envelope attack time
  */
 void v_adsr_set_a_time(t_adsr*__restrict a_adsr_ptr, float a_time)
 {
     if((a_adsr_ptr->a_time) == a_time)
         return;
-    
+
     a_adsr_ptr->a_time = a_time;
-    
+
     if(a_time <= 0.0f)
     {
         a_adsr_ptr->a_inc = 1.0f;
@@ -92,63 +92,63 @@ void v_adsr_set_a_time(t_adsr*__restrict a_adsr_ptr, float a_time)
     {
         a_adsr_ptr->a_inc = (a_adsr_ptr->sr_recip) / (a_adsr_ptr->a_time);
     }
-        
+
 }
 
 /* void v_adsr_set_d_time(
- * t_adsr* a_adsr_ptr, 
+ * t_adsr* a_adsr_ptr,
  * float a_time) //time in seconds
- * 
+ *
  * Sets the envelope decay time
  */
 void v_adsr_set_d_time(t_adsr*__restrict a_adsr_ptr, float a_time)
 {
     if((a_adsr_ptr->d_time) == a_time)
         return;
-    
+
     if(a_time <= 0.0f)
     {
         a_adsr_ptr->d_time = .05;
     }
     else
     {
-        a_adsr_ptr->d_time = a_time;        
+        a_adsr_ptr->d_time = a_time;
     }
-    
+
     //printf("Setting D time to %f\n", (a_adsr_ptr->d_time));
-    
-    a_adsr_ptr->d_inc = ((a_adsr_ptr->sr_recip) / (a_adsr_ptr->d_time)) * -1.0f;    
+
+    a_adsr_ptr->d_inc = ((a_adsr_ptr->sr_recip) / (a_adsr_ptr->d_time)) * -1.0f;
 }
 
 /* void v_adsr_set_r_time(
- * t_adsr* a_adsr_ptr, 
+ * t_adsr* a_adsr_ptr,
  * float a_time) //time in seconds
- * 
+ *
  * Sets the envelope release time
  */
 void v_adsr_set_r_time(t_adsr*__restrict a_adsr_ptr, float a_time)
 {
     if((a_adsr_ptr->r_time) == a_time)
         return;
-    
+
     if(a_time <= 0.0f)
     {
         a_adsr_ptr->r_time = .05f;
     }
     else
     {
-        a_adsr_ptr->r_time = a_time;        
-    }    
-    
+        a_adsr_ptr->r_time = a_time;
+    }
+
     a_adsr_ptr->r_inc = ((a_adsr_ptr->sr_recip) / (a_adsr_ptr->r_time)) * -1.0f;
-    
+
     //printf("Setting R time to %f\n", (a_adsr_ptr->r_time));
-    
+
 }
 
 /* void v_adsr_set_fast_release(t_adsr* a_adsr_ptr)
- * 
- * This method is for killing voices by allowing a quick fade-out instead of directly stealing a voice, which should 
+ *
+ * This method is for killing voices by allowing a quick fade-out instead of directly stealing a voice, which should
  * allow a quick transition without a click
  * TODO:  The total time of the fadeout is not consistent between different sample rates.
  */
@@ -160,26 +160,26 @@ void v_adsr_set_fast_release(t_adsr*__restrict a_adsr_ptr)
 }
 
 /* void v_adsr_set_s_value(
- * t_adsr* a_adsr_ptr, 
+ * t_adsr* a_adsr_ptr,
  * float a_value) //The sustain value, range: 0 to 1
  */
 void v_adsr_set_s_value(t_adsr*__restrict a_adsr_ptr, float a_value)
 {
     a_adsr_ptr->s_value = a_value;
-    
+
     if((a_adsr_ptr->s_value) <= 0.0f)
     {
         a_adsr_ptr->s_value = .001f;
     }
-    
+
     //a_adsr_ptr->d_recip = (1.0f/(1.0f-(a_adsr_ptr->s_value)));
     //a_adsr_ptr->r_recip = (1.0f/(a_adsr_ptr->s_value));
-    
+
     //printf("Setting S value to %f\n", (a_adsr_ptr->s_value));
 }
 
 /* void v_adsr_set_s_value_db(
- * t_adsr* a_adsr_ptr, 
+ * t_adsr* a_adsr_ptr,
  * float a_value)  //The decibel value of sustain, typically -30 to 0
  */
 void v_adsr_set_s_value_db(t_adsr*__restrict a_adsr_ptr, float a_value)
@@ -188,12 +188,12 @@ void v_adsr_set_s_value_db(t_adsr*__restrict a_adsr_ptr, float a_value)
 }
 
 /* void v_adsr_set_adsr(
- * t_adsr* a_adsr_ptr, 
+ * t_adsr* a_adsr_ptr,
  * float a_a, //attack
  * float a_d, //decay
  * float a_s, //sustain
  * float a_r) //release
- * 
+ *
  * Set allADSR values, with a range of 0 to 1 for sustain
  */
 void v_adsr_set_adsr(t_adsr*__restrict a_adsr_ptr, float a_a, float a_d, float a_s, float a_r)
@@ -206,12 +206,12 @@ void v_adsr_set_adsr(t_adsr*__restrict a_adsr_ptr, float a_a, float a_d, float a
 
 
 /* void v_adsr_set_adsr(
- * t_adsr* a_adsr_ptr, 
+ * t_adsr* a_adsr_ptr,
  * float a_a, //attack
  * float a_d, //decay
  * float a_s, //sustain
  * float a_r) //release
- * 
+ *
  * Set all ADSR values, with a range of -30 to 0 for sustain
  */
 void v_adsr_set_adsr_db(t_adsr*__restrict a_adsr_ptr, float a_a, float a_d, float a_s, float a_r)
@@ -220,14 +220,14 @@ void v_adsr_set_adsr_db(t_adsr*__restrict a_adsr_ptr, float a_a, float a_d, floa
     v_adsr_set_d_time(a_adsr_ptr, a_d);
     v_adsr_set_s_value_db(a_adsr_ptr, a_s);
     v_adsr_set_r_time(a_adsr_ptr, a_r);
-    
+
     a_adsr_ptr->a_inc_db = (a_adsr_ptr->a_inc) * ADSR_DB;
     a_adsr_ptr->d_inc_db = (a_adsr_ptr->d_inc) * (ADSR_DB);
-    a_adsr_ptr->r_inc_db = (a_adsr_ptr->r_inc) * (ADSR_DB_RELEASE);    
+    a_adsr_ptr->r_inc_db = (a_adsr_ptr->r_inc) * (ADSR_DB_RELEASE);
 }
 
 /* void v_adsr_retrigger(t_adsr * a_adsr_ptr)
- * 
+ *
  * Reset the ADSR to the beginning of the attack phase
  */
 void v_adsr_retrigger(t_adsr *__restrict a_adsr_ptr)
@@ -245,7 +245,7 @@ void v_adsr_kill(t_adsr *__restrict a_adsr_ptr)
 }
 
 /* void v_adsr_release(t_adsr * a_adsr_ptr)
- * 
+ *
  * Set the ADSR to the release phase
  */
 void v_adsr_release(t_adsr *__restrict a_adsr_ptr)
@@ -258,18 +258,18 @@ void v_adsr_release(t_adsr *__restrict a_adsr_ptr)
 
 /* t_adsr * g_adsr_get_adsr(
  * float a_sr_recip) // 1.0f/sample_rate (TODO: use sample_rate instead)
- * 
+ *
  */
 t_adsr * g_adsr_get_adsr(float a_sr_recip)
 {
     t_adsr * f_result = (t_adsr*)malloc(sizeof(t_adsr));
-    
+
     f_result->sr_recip = a_sr_recip;
     f_result->amp_ptr = g_amp_get();
-        
+
     f_result->output = 0.0f;
     f_result->stage = 4;
-    
+
     //Set these to nonsensical values so that comparisons aren't happening with invalid numbers
     f_result->a_inc = -100.5f;
     f_result->a_time = -100.5f;
@@ -280,30 +280,30 @@ t_adsr * g_adsr_get_adsr(float a_sr_recip)
     //f_result->r_recip = -100.5f;
     f_result->r_time = -100.5f;
     f_result->s_value = -100.5f;
-    
+
     f_result->output_db = -100.5f;
     f_result->a_inc_db = 0.1f;
     f_result->d_inc_db = 0.1f;
     f_result->r_inc_db = 0.1f;
-    
+
     v_adsr_set_a_time(f_result, .05);
     v_adsr_set_d_time(f_result, .5);
     v_adsr_set_s_value_db(f_result, -12.0f);
     v_adsr_set_r_time(f_result, .5);
-    
+
     return f_result;
 }
 
 /* void v_adsr_run(t_adsr * a_adsr_ptr)
- * 
+ *
  * Run the ADSR envelope
  */
 void v_adsr_run(t_adsr *__restrict a_adsr_ptr)
 {
     if((a_adsr_ptr->stage) != 4)
-    {         
+    {
         switch(a_adsr_ptr->stage)
-        {            
+        {
             case 0:
                 a_adsr_ptr->output = (a_adsr_ptr->output) + (a_adsr_ptr->a_inc);
                 if((a_adsr_ptr->output) >= 1.0f)
@@ -341,9 +341,9 @@ void v_adsr_run(t_adsr *__restrict a_adsr_ptr)
 void v_adsr_run_db(t_adsr *__restrict a_adsr_ptr)
 {
     if((a_adsr_ptr->stage) != 4)
-    {         
+    {
         switch(a_adsr_ptr->stage)
-        {            
+        {
             case 0:
                 if((a_adsr_ptr->output) < ADSR_DB_THRESHOLD_LINEAR)
                 {
@@ -353,13 +353,13 @@ void v_adsr_run_db(t_adsr *__restrict a_adsr_ptr)
                 {
                     a_adsr_ptr->output_db = (a_adsr_ptr->output_db) + (a_adsr_ptr->a_inc_db);
                     a_adsr_ptr->output = f_db_to_linear((a_adsr_ptr->output_db), a_adsr_ptr->amp_ptr);
-                    
+
                     if((a_adsr_ptr->output) >= 1.0f)
                     {
-                        a_adsr_ptr->stage = 1;                    
+                        a_adsr_ptr->stage = 1;
                     }
                 }
-                
+
                 break;
             case 1:
                 if((a_adsr_ptr->output) < ADSR_DB_THRESHOLD_LINEAR)
@@ -371,10 +371,10 @@ void v_adsr_run_db(t_adsr *__restrict a_adsr_ptr)
                     a_adsr_ptr->output_db = (a_adsr_ptr->output_db) + (a_adsr_ptr->d_inc_db);
                     a_adsr_ptr->output = f_db_to_linear((a_adsr_ptr->output_db), a_adsr_ptr->amp_ptr);
                 }
-                
+
                 if((a_adsr_ptr->output) <= (a_adsr_ptr->s_value))
                 {
-                    a_adsr_ptr->stage = 2;                    
+                    a_adsr_ptr->stage = 2;
                 }
                 break;
             case 2:
@@ -384,7 +384,7 @@ void v_adsr_run_db(t_adsr *__restrict a_adsr_ptr)
                 if((a_adsr_ptr->output) < ADSR_DB_THRESHOLD_LINEAR_RELEASE)
                 {
                     a_adsr_ptr->output =  (a_adsr_ptr->output) + (a_adsr_ptr->r_inc);
-                    
+
                     if((a_adsr_ptr->output) <= 0.0f)
                     {
                         a_adsr_ptr->output = 0.0f;
@@ -397,7 +397,7 @@ void v_adsr_run_db(t_adsr *__restrict a_adsr_ptr)
                     a_adsr_ptr->output_db = (a_adsr_ptr->output_db) + (a_adsr_ptr->r_inc_db);
                     a_adsr_ptr->output = f_db_to_linear((a_adsr_ptr->output_db), a_adsr_ptr->amp_ptr);
                 }
-                
+
                 break;
         }
     }
