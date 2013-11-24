dc72e689479e91200987de5ba4ffeffa9a4f59a5
PyDAW Team <pydawteam@users.sf.net>
2013-11-24 18:10:08 -0500

    Switch to a X.X.X version format as that somewhat simplifies RPM package creation...

--- src.sh
+++ src.sh
@@ -1,5 +1,5 @@
 git pull
-PYDAW_VERSION=pydaw4-$(sed 's/-/\./g' src/pydaw4-version.txt)
+PYDAW_VERSION=pydaw4-$(cat src/pydaw4-version.txt)
 cp -r src $PYDAW_VERSION
 tar czf $PYDAW_VERSION-source-code.tar.gz $PYDAW_VERSION
 rm -rf $PYDAW_VERSION
