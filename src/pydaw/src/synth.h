ef9234bebb8a0f99e0931f63077298effca121d0
PyDAW Team <pydawteam@users.sf.net>
2013-12-08 23:48:29 -0500

    Remove some really obsolete comments...

--- src/pydaw/src/synth.h
+++ src/pydaw/src/synth.h
@@ -19,35 +19,31 @@ extern "C" {
 #endif
 
 #include "../include/pydaw_plugin.h"
-  
-#define PYDAW_INPUT_COUNT 0 //10
-    
+
+#define PYDAW_INPUT_COUNT 0
+
 #define PYDAW_INPUT_MIN 0
 #define PYDAW_INPUT_MAX  (PYDAW_INPUT_MIN + PYDAW_INPUT_COUNT)
-    
+
 #define PYDAW_OUTPUT0  (PYDAW_INPUT_COUNT)
 #define PYDAW_OUTPUT1  (PYDAW_OUTPUT0 + 1)
-/*GUI Step 11:  Add ports to the main synthesizer file that the GUI can talk to */
-    
-/*LMS_FIRST_CONTROL_PORT is the first port used for controls such as knobs.  All control ports must be numbered continuously,
- as they are iterated through*/
+
 #define PYDAW_FIRST_CONTROL_PORT (PYDAW_OUTPUT1 + 1)
-/*This is the last control port*/
+
 #define PYDAW_LAST_CONTROL_PORT (PYDAW_FIRST_CONTROL_PORT)
-#define PYDAW_COUNT  (PYDAW_LAST_CONTROL_PORT) /* must be 1 + highest value above CHANGE THIS IF YOU ADD OR TAKE AWAY ANYTHING*/
-  
-/*GUI Step 13:  Add a variable for each control in the LMS type*/
+#define PYDAW_COUNT  (PYDAW_LAST_CONTROL_PORT)
+
 typedef struct {
     PYFX_Data **input_arr;
-    
+
     PYFX_Data *output0;
     PYFX_Data *output1;
-    
+
     float fs;
-    
+
     int i_mono_out;
-    int i_buffer_clear;    
-    
+    int i_buffer_clear;
+
 } t_pydaw_engine;
 
 #ifdef	__cplusplus
