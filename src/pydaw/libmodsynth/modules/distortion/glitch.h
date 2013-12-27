faac05f13b7359dcfad8f495cb381ec06ce0a275
PyDAW Team <pydawteam@users.sf.net>
2013-12-27 13:14:02 -0500

    Fix a memory error found with Valgrind...

--- src/pydaw/libmodsynth/modules/distortion/glitch.h
+++ src/pydaw/libmodsynth/modules/distortion/glitch.h
@@ -20,8 +20,8 @@ extern "C" {
 
 #include "../signal_routing/audio_xfade.h"
 #include "../../lib/pitch_core.h"
-    
-typedef struct 
+
+typedef struct
 {
     float * buffer;
     int buffer_size, buffer_ptr;
@@ -51,49 +51,60 @@ void v_glc_glitch_free(t_glc_glitch * a_glc)
 t_glc_glitch * g_glc_glitch_get(float a_sr)
 {
     t_glc_glitch * f_result;
-    
+
     if(posix_memalign((void**)&f_result, 16, (sizeof(t_glc_glitch))) != 0)
     {
         return 0;
     }
-    
+
     f_result->buffer_size = (int)(a_sr * (1.0f/19.0f));
-    
-    if(posix_memalign((void**)&f_result->buffer, 16, (sizeof(float) * f_result->buffer_size)) != 0)
+
+    if(posix_memalign((void**)&f_result->buffer, 16, (sizeof(float) *
+            f_result->buffer_size)) != 0)
     {
         return 0;
     }
-    
+
+    int f_i = 0;
+
+    while(f_i < f_result->buffer_size)
+    {
+        f_result->buffer[f_i] = 0.0f;
+        f_i++;
+    }
+
     f_result->buffer_ptr = 0;
     f_result->current_repeat_count = 0;
     f_result->is_repeating = 0;
-    f_result->last_pitch = 55.5555f;    
+    f_result->last_pitch = 55.5555f;
     f_result->last_repeat = 99.99f;
     f_result->last_wet = -1.111f;
     f_result->pitch = g_pit_get();
-    f_result->repeat_count = 42;    
+    f_result->repeat_count = 42;
     f_result->sample_count = 99.99f;
     f_result->sample_tmp = 0.0f;
     f_result->sr = a_sr;
     f_result->xfade = g_axf_get_audio_xfade(-3.0f);
-    
+
     return f_result;
 }
 
-void v_glc_glitch_set(t_glc_glitch* a_glc, float a_pitch, float a_repeat, float a_wet)
+void v_glc_glitch_set(t_glc_glitch* a_glc, float a_pitch, float a_repeat,
+        float a_wet)
 {
     if(a_glc->last_pitch != a_pitch)
     {
         a_glc->last_pitch = a_pitch;
-        a_glc->sample_count = (int)((a_glc->sr)/(f_pit_midi_note_to_hz_fast(a_pitch, a_glc->pitch)));
+        a_glc->sample_count = (int)((a_glc->sr) /
+                (f_pit_midi_note_to_hz_fast(a_pitch, a_glc->pitch)));
     }
-    
+
     if(a_glc->last_repeat != a_repeat)
     {
         a_glc->last_repeat = a_repeat;
         a_glc->repeat_count = (int)(a_repeat);
     }
-    
+
     if(a_glc->last_wet != a_wet)
     {
         a_glc->last_wet = a_wet;
@@ -103,23 +114,25 @@ void v_glc_glitch_set(t_glc_glitch* a_glc, float a_pitch, float a_repeat, float 
 
 void v_glc_glitch_run(t_glc_glitch* a_glc, float a_input0, float a_input1)
 {
-    a_glc->output0 = f_axf_run_xfade(a_glc->xfade, a_input0, a_glc->buffer[a_glc->buffer_ptr]);
-    a_glc->output1 = f_axf_run_xfade(a_glc->xfade, a_input1, a_glc->buffer[a_glc->buffer_ptr]);
-    
+    a_glc->output0 = f_axf_run_xfade(a_glc->xfade, a_input0,
+            a_glc->buffer[a_glc->buffer_ptr]);
+    a_glc->output1 = f_axf_run_xfade(a_glc->xfade, a_input1,
+            a_glc->buffer[a_glc->buffer_ptr]);
+
     if(!a_glc->is_repeating)
     {
         a_glc->buffer[a_glc->buffer_ptr] = (a_input0 + a_input1) * 0.5f;
     }
-    
+
     a_glc->buffer_ptr++;
-    
+
     if(a_glc->buffer_ptr >= a_glc->sample_count)
     {
         a_glc->buffer_ptr = 0;
         if(a_glc->is_repeating)
         {
             a_glc->current_repeat_count++;
-            
+
             if(a_glc->current_repeat_count >= a_glc->repeat_count)
             {
                 a_glc->current_repeat_count = 0;
@@ -128,7 +141,7 @@ void v_glc_glitch_run(t_glc_glitch* a_glc, float a_input0, float a_input1)
         }
         else
         {
-            a_glc->is_repeating = 1;            
+            a_glc->is_repeating = 1;
         }
     }
 }
