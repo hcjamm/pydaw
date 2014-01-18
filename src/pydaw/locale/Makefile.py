38f5931fbb71b0ddd834aa3dbb60b960265b0452
PyDAW Team <pydawteam@users.sf.net>
2014-01-18 18:14:31 -0500

    Start building/packaging translations with PyDAW, starting with the Portugese/Portugal translation provided by stormpack and pc999 from kvraudio.com.
    Thanks guys, excellent work.

--- src/pydaw/locale/Makefile.py
+++ src/pydaw/locale/Makefile.py
@@ -19,10 +19,11 @@ for f_file in os.listdir(f_src_dir):
     f_result = os.system(f_cmd)
     if f_result != 0:
         print("\n\nERROR: {}/pydaw4.po returned non-zero \n\n".format(f_file))
+        exit(9999)
 
 if len(sys.argv) > 1:
     f_install_dir = sys.argv[1]
     f_share_dir = "{}/share".format(f_install_dir)
     if not os.path.isdir(f_share_dir):
         os.system("mkdir -p '{}'".format(f_share_dir))
-    os.system("cp -r '{}' '{}'".format(f_bin_dir, f_install_dir))
+    os.system("cp -r '{}' '{}'".format(f_bin_dir, f_share_dir))
