d07e820068ccf7d15800618efbc8f227444ec3d1
PyDAW Team <pydawteam@users.sf.net>
2014-01-24 18:04:42 -0500

    Make the version number part of the file name...

--- src/pydaw-build/pydaw_os/ova.sh
+++ src/pydaw-build/pydaw_os/ova.sh
@@ -1,8 +1,9 @@
 PYDAW_VERSION=pydaw4
+PYDAW_OVA_NAME=$PYDAW_VERSION-$(cat ../../pydaw4-version.txt).ova
 
-if [ -f $PYDAW_VERSION.ova ]; then
-	rm -f $PYDAW_VERSION.ova
+if [ -f $PYDAW_OVA_NAME ]; then
+	rm -f $PYDAW_OVA_NAME
 fi
 
-vboxmanage export $PYDAW_VERSION -o $PYDAW_VERSION.ova
+vboxmanage export $PYDAW_VERSION -o $PYDAW_OVA_NAME
 
