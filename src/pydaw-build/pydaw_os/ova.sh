54318511b40b9d82bb23af3b8a53cb26ca3857d6
PyDAW Team <pydawteam@users.sf.net>
2014-02-08 19:36:23 -0500

    Minor improvements to the .ova script.

--- src/pydaw-build/pydaw_os/ova.sh
+++ src/pydaw-build/pydaw_os/ova.sh
@@ -8,6 +8,8 @@ if [ -f $PYDAW_OVA_NAME ]; then
 	rm -f $PYDAW_OVA_NAME
 fi
 
+rm *.zip
+
 vboxmanage export $PYDAW_VERSION -o $PYDAW_OVA_NAME
 zip $PYDAW_MAC_NAME $PYDAW_OVA_NAME Virtual*dmg readme_windows_and_mac.txt
 zip $PYDAW_WIN_NAME $PYDAW_OVA_NAME Virtual*exe readme_windows_and_mac.txt
