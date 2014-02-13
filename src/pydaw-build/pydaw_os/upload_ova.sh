4cb3867d8de79e244f3705d40df74d1db80a14a5
PyDAW Team <pydawteam@users.sf.net>
2014-02-13 17:58:24 -0500

    Moving the windows and mac ports to separate folders later...

--- src/pydaw-build/pydaw_os/upload_ova.sh
+++ src/pydaw-build/pydaw_os/upload_ova.sh
@@ -3,7 +3,8 @@ PYDAW_NAME=$PYDAW_VERSION-$(cat ../../pydaw4-version.txt)
 PYDAW_WIN_NAME=$PYDAW_NAME-windows.zip
 PYDAW_MAC_NAME=$PYDAW_NAME-mac.zip
 
-PYDAW_DIR=pydawteam@frs.sourceforge.net:/home/frs/project/libmodsynth/$PYDAW_VERSION/windows_and_mac
+PYDAW_DIR=pydawteam@frs.sourceforge.net:/home/frs/project/libmodsynth/$PYDAW_VERSION
 
-scp $PYDAW_WIN_NAME $PYDAW_MAC_NAME $PYDAW_DIR
+scp $PYDAW_WIN_NAME $PYDAW_DIR/windows
+scp $PYDAW_MAC_NAME $PYDAW_DIR/mac
 
