a35c45d13db0d23081dde567eb9d28c85b9733ad
pydaw <pydaw@localhost.localdomain>
2014-02-14 08:33:52 -0500

    Fix upload script...

--- upload_rpm_and_src.sh
+++ upload_rpm_and_src.sh
@@ -1,6 +1,6 @@
 PYDAW_VERSION=pydaw4
-PYDAW_VERSION=$(cat src/pydaw4-version.txt)
+PYDAW_RELEASE=$(cat src/pydaw4-version.txt)
 PYDAW_DIR=pydawteam@frs.sourceforge.net:/home/frs/project/libmodsynth/$PYDAW_VERSION/linux
 
-scp ~/rpmbuild/RPMS/*/*$PYDAW_VERSION*rpm ~/rpmbuild/SRPMS/*$PYDAW_VERSION*rpm $PYDAW_DIR
+scp ~/rpmbuild/RPMS/*/*$PYDAW_RELEASE*rpm ~/rpmbuild/SRPMS/*$PYDAW_RELEASE*rpm *$PYDAW_VERSION-$PYDAW_RELEASE-source-code.tar.gz $PYDAW_DIR
 
