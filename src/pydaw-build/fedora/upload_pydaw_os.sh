d11c006b7b32c82c43e973b1a89be6af544b1624
pydaw <pydaw@localhost.localdomain>
2014-02-14 08:12:27 -0500

    Fix upload script...

--- src/pydaw-build/fedora/upload_pydaw_os.sh
+++ src/pydaw-build/fedora/upload_pydaw_os.sh
@@ -1,6 +1,5 @@
-PYDAW_VERSION=pydaw4
 PYDAW_RELEASE=$(cat ../../pydaw4-version.txt)
 PYDAW_DIR=pydawteam@frs.sourceforge.net:/home/frs/project/libmodsynth/$PYDAW_VERSION/pydaw_os
 
-scp $PYDAW_VERSION*$PYDAW_RELEASE*iso $PYDAW_DIR
+scp PyDAW-OS-Fedora-LXDE-$PYDAW_RELEASE.iso $PYDAW_DIR
 
