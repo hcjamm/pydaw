104e59c5e585dd61b9d093aaf70a8a239d85c79a
PyDAW Team <pydawteam@users.sf.net>
2014-02-22 14:12:19 -0500

    Even more major-version-bumping fixes...

--- src/pydaw/locale/Makefile.py
+++ src/pydaw/locale/Makefile.py
@@ -3,6 +3,8 @@
 import os
 import sys
 
+global_pydaw_version_string = open("../../major-version.txt").read().strip()
+
 f_base_dir = os.path.dirname(__file__)
 f_src_dir = "{}/src".format(f_base_dir)
 f_bin_dir = "{}/bin/locale".format(f_base_dir)
@@ -16,10 +18,13 @@ os.system("chmod 777 '{}'".format(f_bin_dir))
 for f_file in os.listdir(f_src_dir):
     f_dest_dir = "{}/{}/LC_MESSAGES".format(f_bin_dir, f_file)
     os.system("mkdir -p '{}'".format(f_dest_dir))
-    f_cmd = "msgfmt -o '{}/pydaw4.mo' '{}/{}/pydaw4.po'".format(f_dest_dir, f_src_dir, f_file)
+    f_cmd = "msgfmt -o '{}/{}.mo' '{}/{}/{}.po'".format(f_dest_dir, 
+        global_pydaw_version_string, f_src_dir, f_file,
+        global_pydaw_version_string)
     f_result = os.system(f_cmd)
     if f_result != 0:
-        print("\n\nERROR: {}/pydaw4.po returned non-zero \n\n".format(f_file))
+        print("\n\nERROR: {}/{}.po returned non-zero \n\n".format(f_file,
+            global_pydaw_version_string))
         exit(9999)
 
 if len(sys.argv) > 1:
