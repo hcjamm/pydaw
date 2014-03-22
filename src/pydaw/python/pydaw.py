cdeb2f7a03eeb9304ec144cb541210414422c3f2
PyDAW Team <pydawteam@users.sf.net>
2014-03-21 20:49:51 -0400

    Add Phrygian Dominant to the list of scales...

--- src/pydaw/python/pydaw.py
+++ src/pydaw/python/pydaw.py
@@ -4591,17 +4591,22 @@ class piano_roll_editor(QtGui.QGraphicsView):
             f_white_key_brush, f_black_key_brush, f_white_key_brush, f_black_key_brush,
             f_white_key_brush, f_white_key_brush, f_black_key_brush, f_white_key_brush,
             f_black_key_brush]
-        elif this_piano_roll_editor_widget.scale_combobox.currentIndex() == 8: #Lydian
+        elif this_piano_roll_editor_widget.scale_combobox.currentIndex() == 8: #Phrygian Dominant
+            f_octave_brushes = [f_base_brush, f_white_key_brush, f_black_key_brush,
+            f_black_key_brush, f_white_key_brush, f_white_key_brush, f_black_key_brush,
+            f_white_key_brush, f_white_key_brush, f_black_key_brush, f_white_key_brush,
+            f_black_key_brush]
+        elif this_piano_roll_editor_widget.scale_combobox.currentIndex() == 9: #Lydian
             f_octave_brushes = [f_base_brush, f_black_key_brush, f_white_key_brush,
             f_black_key_brush, f_white_key_brush, f_black_key_brush, f_white_key_brush,
             f_white_key_brush, f_black_key_brush, f_white_key_brush, f_black_key_brush,
             f_white_key_brush]
-        elif this_piano_roll_editor_widget.scale_combobox.currentIndex() == 9: #Mixolydian
+        elif this_piano_roll_editor_widget.scale_combobox.currentIndex() == 10: #Mixolydian
             f_octave_brushes = [f_base_brush, f_black_key_brush, f_white_key_brush,
             f_black_key_brush, f_white_key_brush, f_white_key_brush, f_black_key_brush,
             f_white_key_brush, f_black_key_brush, f_white_key_brush, f_white_key_brush,
             f_black_key_brush]
-        elif this_piano_roll_editor_widget.scale_combobox.currentIndex() == 10: #Locrian
+        elif this_piano_roll_editor_widget.scale_combobox.currentIndex() == 11: #Locrian
             f_octave_brushes = [f_base_brush, f_white_key_brush, f_black_key_brush,
             f_white_key_brush, f_black_key_brush, f_white_key_brush, f_white_key_brush,
             f_black_key_brush, f_white_key_brush, f_black_key_brush, f_white_key_brush,
@@ -4790,7 +4795,8 @@ class piano_roll_editor_widget:
         self.scale_combobox.setMinimumWidth(172)
         self.scale_combobox.addItems(["Major", "Melodic Minor", "Harmonic Minor",
                                       "Natural Minor", "Pentatonic Major", "Pentatonic Minor",
-                                      "Dorian", "Phrygian", "Lydian", "Mixolydian", "Locrian"])
+                                      "Dorian", "Phrygian", "Phrygian Dominant", "Lydian",
+                                      "Mixolydian", "Locrian"])
         self.scale_combobox.currentIndexChanged.connect(self.reload_handler)
         self.controls_grid_layout.addWidget(QtGui.QLabel(_("Scale:")), 0, 5)
         self.controls_grid_layout.addWidget(self.scale_combobox, 0, 6)
