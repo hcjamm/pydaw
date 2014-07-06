55fa5fff6247b4011c7fe582525ba148fc2db197
pydawteam <pydaw@pydaw.org>
2014-07-06 15:08:31 -0400

    Update the tarball name

--- src.sh
+++ src.sh
@@ -1,6 +1,6 @@
 git pull
 PYDAW_VERSION=$(cat src/major-version.txt)-$(cat src/minor-version.txt)
 cp -r src $PYDAW_VERSION
-tar czf $PYDAW_VERSION-source-code.tar.gz $PYDAW_VERSION
+tar czf ${PYDAW_VERSION}.tar.gz $PYDAW_VERSION
 rm -rf $PYDAW_VERSION
 
