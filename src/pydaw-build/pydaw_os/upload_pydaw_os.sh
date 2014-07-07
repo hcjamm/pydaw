ea1c1b56e49483bab5014d0903a993278b9e0f87
pydawteam <pydaw@pydaw.org>
2014-07-06 21:37:52 -0400

    Remove script that upload to SourceForge since the project is entirely migrated to Github now...

--- src/pydaw-build/pydaw_os/upload_pydaw_os.sh
+++
@@ -1,8 +0,0 @@
-PYDAW_VERSION=$(cat ../../major-version.txt)
-PYDAW_ISO_NAME=pydaw-os-$(cat ../../minor-version.txt).iso
-PYDAW_DIR=pydawteam@frs.sourceforge.net:/home/frs/project/libmodsynth/$PYDAW_VERSION/pydaw_os
-
-echo "scp'ing $PYDAW_ISO_NAME $PYDAW_DIR"
-
-scp -l 3000 $PYDAW_ISO_NAME $PYDAW_DIR
-
--- src/upload_deb.sh
+++
@@ -1,9 +0,0 @@
-git pull
-./install_deb.sh
-PYDAW_VERSION=$(cat major-version.txt)
-PYDAW_RELEASE=$(cat minor-version.txt)
-
-PYDAW_DIR=pydawteam@frs.sourceforge.net:/home/frs/project/libmodsynth/$PYDAW_VERSION/linux
-
-scp pydaw-build/$PYDAW_VERSION-$PYDAW_RELEASE*deb $PYDAW_DIR
-
